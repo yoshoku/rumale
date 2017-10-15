e155114545919399828c151ab8c45811f3587ed6
yoshoku <yoshoku@outlook.com>
2017-10-15 20:34:43 +0900

    :sparkles: add the method calculating an RBF kernel matrix

--- lib/svmkit/pairwise_metric.rb
+++ lib/svmkit/pairwise_metric.rb
@@ -2,7 +2,7 @@ module SVMKit
   # Module for calculating pairwise distances, similarities, and kernels.
   module PairwiseMetric
     class << self
-      # Calculating pairwise euclidean distances.
+      # Calculate the pairwise euclidean distances between x and y.
       #
       # @param x [NMatrix] (shape: [n_samples_x, n_features])
       # @param y [NMatrix] (shape: [n_samples_y, n_features])
@@ -16,6 +16,19 @@ module SVMKit
                           sum_x_vec.repeat(y.shape[0], 1) +
                           sum_y_vec.transpose.repeat(x.shape[0], 0)
         distance_matrix.abs.sqrt
+      end
+
+      # Calculate the rbf kernel between x and y.
+      #
+      # @param x [NMatrix] (shape: [n_samples_x, n_features])
+      # @param y [NMatrix] (shape: [n_samples_y, n_features])
+      # @param gamma [Float] The parameter of rbf kernel, if nil it is 1 / n_features.
+      # @return [NMatrix] (shape: [n_samples_x, n_samples_x] or [n_samples_x, n_samples_y] if y is given)
+      def rbf_kernel(x, y = nil, gamma = nil)
+        y = x if y.nil?
+        gamma ||= 1.0 / x.shape[1]
+        distance_matrix = euclidean_distance(x, y)
+        ((distance_matrix**2) * -gamma).exp
       end
     end
   end
--- spec/pairwise_metric_spec.rb
+++ spec/pairwise_metric_spec.rb
@@ -6,8 +6,9 @@ RSpec.describe SVMKit::PairwiseMetric do
   let(:n_samples_b) { 5 }
   let(:samples_a) { NMatrix.rand([n_samples_a, n_features]) }
   let(:samples_b) { NMatrix.rand([n_samples_b, n_features]) }
+  let(:gamma) { 0.5 }
 
-  it 'calculates pairwise euclidean distances.' do
+  it 'calculates the euclidean distance matrix between different datasets.' do
     dist_mat_bf = NMatrix.new(
       [n_samples_a, n_samples_b],
       [*0...n_samples_a].product([*0...n_samples_b]).map { |m, n| (samples_a.row(m) - samples_b.row(n)).norm2 }
@@ -16,5 +17,44 @@ RSpec.describe SVMKit::PairwiseMetric do
     expect(dist_mat.shape[0]).to eq(n_samples_a)
     expect(dist_mat.shape[1]).to eq(n_samples_b)
     expect(dist_mat).to be_within(1.0e-5).of(dist_mat_bf)
+  end
+
+  it 'calculates the euclidean distance matrix of dataset.' do
+    dist_mat_bf = NMatrix.new(
+      [n_samples_a, n_samples_a],
+      [*0...n_samples_a].product([*0...n_samples_b]).map { |m, n| (samples_a.row(m) - samples_a.row(n)).norm2 }
+    )
+    dist_mat = described_class.euclidean_distance(samples_a)
+    expect(dist_mat.shape[0]).to eq(n_samples_a)
+    expect(dist_mat.shape[1]).to eq(n_samples_a)
+    expect(dist_mat).to be_within(1.0e-5).of(dist_mat_bf)
+  end
+
+  it 'calculates the RBF kernel matrix of dataset.' do
+    kernel_mat_bf = NMatrix.new(
+      [n_samples_a, n_samples_a],
+      [*0...n_samples_a].product([*0...n_samples_b]).map do |m, n|
+        dist = (samples_a.row(m) - samples_a.row(n)).norm2
+        Math.exp(-gamma * (dist**2))
+      end
+    )
+    kernel_mat = described_class.rbf_kernel(samples_a, nil, gamma)
+    expect(kernel_mat.shape[0]).to eq(n_samples_a)
+    expect(kernel_mat.shape[1]).to eq(n_samples_a)
+    expect(kernel_mat).to be_within(1.0e-8).of(kernel_mat_bf)
+  end
+
+  it 'calculates the RBF kernel matrix between different datasets.' do
+    kernel_mat_bf = NMatrix.new(
+      [n_samples_a, n_samples_b],
+      [*0...n_samples_a].product([*0...n_samples_b]).map do |m, n|
+        dist = (samples_a.row(m) - samples_b.row(n)).norm2
+        Math.exp(-gamma * (dist**2))
+      end
+    )
+    kernel_mat = described_class.rbf_kernel(samples_a, samples_b, gamma)
+    expect(kernel_mat.shape[0]).to eq(n_samples_a)
+    expect(kernel_mat.shape[1]).to eq(n_samples_b)
+    expect(kernel_mat).to be_within(1.0e-8).of(kernel_mat_bf)
   end
 end
