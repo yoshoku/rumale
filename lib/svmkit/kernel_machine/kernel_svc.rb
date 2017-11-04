c2b8537fb397b95c7729ff260072e3728288a9cf
yoshoku <yoshoku@outlook.com>
2017-11-05 01:51:05 +0900

    :rocket: fix the kernel svc class to use Numo::NArray

--- lib/svmkit/kernel_machine/kernel_svc.rb
+++ lib/svmkit/kernel_machine/kernel_svc.rb
@@ -28,7 +28,7 @@ module SVMKit
       }.freeze
 
       # Return the weight vector for Kernel SVC.
-      # @return [NMatrix] (shape: [1, n_trainig_sample])
+      # @return [Numo::DFloat] (shape: [n_trainig_sample])
       attr_reader :weight_vec
 
       # Return the random generator for performing random sampling in the Pegasos algorithm.
@@ -52,74 +52,74 @@ module SVMKit
 
       # Fit the model with given training data.
       #
-      # @param x [NMatrix] (shape: [n_training_samples, n_training_samples])
+      # @param x [Numo::DFloat] (shape: [n_training_samples, n_training_samples])
       #   The kernel matrix of the training data to be used for fitting the model.
-      # @param y [NMatrix] (shape: [1, n_training_samples]) The labels to be used for fitting the model.
+      # @param y [Numo::Int32] (shape: [n_training_samples]) The labels to be used for fitting the model.
       # @return [KernelSVC] The learned classifier itself.
       def fit(x, y)
         # Generate binary labels
-        negative_label = y.uniq.sort.shift
-        bin_y = y.to_flat_a.map { |l| l != negative_label ? 1 : -1 }
+        negative_label = y.to_a.uniq.sort.shift
+        bin_y = y.to_a.map { |l| l != negative_label ? 1 : -1 }
         # Initialize some variables.
         n_training_samples = x.shape[0]
         rand_ids = []
-        weight_vec = NMatrix.zeros([1, n_training_samples])
+        weight_vec = Numo::DFloat.zeros(n_training_samples)
         # Start optimization.
         params[:max_iter].times do |t|
           # random sampling
           rand_ids = [*0...n_training_samples].shuffle(random: @rng) if rand_ids.empty?
           target_id = rand_ids.shift
           # update the weight vector
-          func = (weight_vec * bin_y[target_id]).dot(x.row(target_id).transpose).to_f
+          func = (weight_vec * bin_y[target_id]).dot(x[target_id, true].transpose).to_f
           func *= bin_y[target_id] / (params[:reg_param] * (t + 1))
           weight_vec[target_id] += 1.0 if func < 1.0
         end
         # Store the learned model.
-        @weight_vec = weight_vec * NMatrix.new([1, n_training_samples], bin_y)
+        @weight_vec = weight_vec * Numo::DFloat.asarray(bin_y)
         self
       end
 
       # Calculate confidence scores for samples.
       #
-      # @param x [NMatrix] (shape: [n_testing_samples, n_training_samples])
+      # @param x [Numo::DFloat] (shape: [n_testing_samples, n_training_samples])
       #     The kernel matrix between testing samples and training samples to compute the scores.
-      # @return [NMatrix] (shape: [1, n_testing_samples]) Confidence score per sample.
+      # @return [Numo::DFloat] (shape: [n_testing_samples]) Confidence score per sample.
       def decision_function(x)
         @weight_vec.dot(x.transpose)
       end
 
       # Predict class labels for samples.
       #
-      # @param x [NMatrix] (shape: [n_testing_samples, n_training_samples])
+      # @param x [Numo::DFloat] (shape: [n_testing_samples, n_training_samples])
       #     The kernel matrix between testing samples and training samples to predict the labels.
-      # @return [NMatrix] (shape: [1, n_testing_samples]) Predicted class label per sample.
+      # @return [Numo::Int32] (shape: [n_testing_samples]) Predicted class label per sample.
       def predict(x)
-        decision_function(x).map { |v| v >= 0 ? 1 : -1 }
+        Numo::Int32.cast(decision_function(x).map { |v| v >= 0 ? 1 : -1 })
       end
 
       # Claculate the mean accuracy of the given testing data.
       #
-      # @param x [NMatrix] (shape: [n_testing_samples, n_training_samples])
+      # @param x [Numo::DFloat] (shape: [n_testing_samples, n_training_samples])
       #     The kernel matrix between testing samples and training samples.
-      # @param y [NMatrix] (shape: [1, n_testing_samples]) True labels for testing data.
+      # @param y [Numo::Int32] (shape: [n_testing_samples]) True labels for testing data.
       # @return [Float] Mean accuracy
       def score(x, y)
         p = predict(x)
-        n_hits = (y.to_flat_a.map.with_index { |l, n| l == p[n] ? 1 : 0 }).inject(:+)
+        n_hits = (y.to_a.map.with_index { |l, n| l == p[n] ? 1 : 0 }).inject(:+)
         n_hits / y.size.to_f
       end
 
       # Dump marshal data.
       # @return [Hash] The marshal data about KernelSVC.
       def marshal_dump
-        { params: params, weight_vec: Utils.dump_nmatrix(@weight_vec), rng: @rng }
+        { params: params, weight_vec: @weight_vec, rng: @rng }
       end
 
       # Load marshal data.
       # @return [nil]
       def marshal_load(obj)
         self.params = obj[:params]
-        @weight_vec = Utils.restore_nmatrix(obj[:weight_vec])
+        @weight_vec = obj[:weight_vec]
         @rng = obj[:rng]
         nil
       end
--- spec/kernel_machine/kernel_svc_spec.rb
+++ spec/kernel_machine/kernel_svc_spec.rb
@@ -1,15 +1,29 @@
 require 'spec_helper'
 
 RSpec.describe SVMKit::KernelMachine::KernelSVC do
-  let(:samples) { SVMKit::Utils.restore_nmatrix(Marshal.load(File.read(__dir__ + '/../test_samples_xor.dat'))) }
-  let(:labels) { SVMKit::Utils.restore_nmatrix(Marshal.load(File.read(__dir__ + '/../test_labels_xor.dat'))) }
+  let(:samples) { Marshal.load(File.read(__dir__ + '/../test_samples_xor.dat')) }
+  let(:labels) { Marshal.load(File.read(__dir__ + '/../test_labels_xor.dat')) }
   let(:kernel_matrix) { SVMKit::PairwiseMetric::rbf_kernel(samples, nil, 1.0) }
   let(:estimator) { described_class.new(penalty: 1.0, max_iter: 1000, random_seed: 1) }
 
   it 'classifies xor data.' do
     n_samples, = samples.shape[0]
     estimator.fit(kernel_matrix, labels)
-    expect(estimator.weight_vec.size).to eq(n_samples)
+    expect(estimator.weight_vec.class).to eq(Numo::DFloat)
+    expect(estimator.weight_vec.shape[0]).to eq(n_samples)
+    expect(estimator.weight_vec.shape[1]).to be_nil
+
+    func_vals = estimator.decision_function(kernel_matrix)
+    expect(func_vals.class).to eq(Numo::DFloat)
+    expect(func_vals.shape[0]).to eq(n_samples)
+    expect(func_vals.shape[1]).to be_nil
+
+    predicted = estimator.predict(kernel_matrix)
+    expect(predicted.class).to eq(Numo::Int32)
+    expect(predicted.shape[0]).to eq(n_samples)
+    expect(predicted.shape[1]).to be_nil
+    expect(predicted).to eq(labels)
+
     score = estimator.score(kernel_matrix, labels)
     expect(score).to eq(1.0)
   end
