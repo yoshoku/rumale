c53ac076ec3723e31786ac4f1e04ab7037233adf
yoshoku <yoshoku@outlook.com>
2017-11-05 01:23:20 +0900

    :rocket: fix the methods calculating distance and kernel matrix to use Numo::NArray

--- lib/svmkit/pairwise_metric.rb
+++ lib/svmkit/pairwise_metric.rb
@@ -4,38 +4,38 @@ module SVMKit
     class << self
       # Calculate the pairwise euclidean distances between x and y.
       #
-      # @param x [NMatrix] (shape: [n_samples_x, n_features])
-      # @param y [NMatrix] (shape: [n_samples_y, n_features])
-      # @return [NMatrix] (shape: [n_samples_x, n_samples_x] or [n_samples_x, n_samples_y] if y is given)
+      # @param x [Numo::DFloat] (shape: [n_samples_x, n_features])
+      # @param y [Numo::DFloat] (shape: [n_samples_y, n_features])
+      # @return [Numo::DFloat] (shape: [n_samples_x, n_samples_x] or [n_samples_x, n_samples_y] if y is given)
       def euclidean_distance(x, y = nil)
         y = x if y.nil?
         sum_x_vec = (x**2).sum(1)
         sum_y_vec = (y**2).sum(1)
         dot_xy_mat = x.dot(y.transpose)
         distance_matrix = dot_xy_mat * -2.0 +
-                          sum_x_vec.repeat(y.shape[0], 1) +
-                          sum_y_vec.transpose.repeat(x.shape[0], 0)
-        distance_matrix.abs.sqrt
+                          sum_x_vec.tile(y.shape[0], 1).transpose +
+                          sum_y_vec.tile(x.shape[0], 1)
+        Numo::NMath.sqrt(distance_matrix.abs)
       end
 
       # Calculate the rbf kernel between x and y.
       #
-      # @param x [NMatrix] (shape: [n_samples_x, n_features])
-      # @param y [NMatrix] (shape: [n_samples_y, n_features])
+      # @param x [Numo::DFloat] (shape: [n_samples_x, n_features])
+      # @param y [Numo::DFloat] (shape: [n_samples_y, n_features])
       # @param gamma [Float] The parameter of rbf kernel, if nil it is 1 / n_features.
-      # @return [NMatrix] (shape: [n_samples_x, n_samples_x] or [n_samples_x, n_samples_y] if y is given)
+      # @return [Numo::DFloat] (shape: [n_samples_x, n_samples_x] or [n_samples_x, n_samples_y] if y is given)
       def rbf_kernel(x, y = nil, gamma = nil)
         y = x if y.nil?
         gamma ||= 1.0 / x.shape[1]
         distance_matrix = euclidean_distance(x, y)
-        ((distance_matrix**2) * -gamma).exp
+        Numo::NMath.exp((distance_matrix**2) * -gamma)
       end
 
       # Calculate the linear kernel between x and y.
       #
-      # @param x [NMatrix] (shape: [n_samples_x, n_features])
-      # @param y [NMatrix] (shape: [n_samples_y, n_features])
-      # @return [NMatrix] (shape: [n_samples_x, n_samples_x] or [n_samples_x, n_samples_y] if y is given)
+      # @param x [Numo::DFloat] (shape: [n_samples_x, n_features])
+      # @param y [Numo::DFloat] (shape: [n_samples_y, n_features])
+      # @return [Numo::DFloat] (shape: [n_samples_x, n_samples_x] or [n_samples_x, n_samples_y] if y is given)
       def linear_kernel(x, y = nil)
         y = x if y.nil?
         x.dot(y.transpose)
@@ -43,12 +43,12 @@ module SVMKit
 
       # Calculate the polynomial kernel between x and y.
       #
-      # @param x [NMatrix] (shape: [n_samples_x, n_features])
-      # @param y [NMatrix] (shape: [n_samples_y, n_features])
+      # @param x [Numo::DFloat] (shape: [n_samples_x, n_features])
+      # @param y [Numo::DFloat] (shape: [n_samples_y, n_features])
       # @param degree [Integer] The parameter of polynomial kernel.
       # @param gamma [Float] The parameter of polynomial kernel, if nil it is 1 / n_features.
       # @param coef [Integer] The parameter of polynomial kernel.
-      # @return [NMatrix] (shape: [n_samples_x, n_samples_x] or [n_samples_x, n_samples_y] if y is given)
+      # @return [Numo::DFloat] (shape: [n_samples_x, n_samples_x] or [n_samples_x, n_samples_y] if y is given)
       def polynomial_kernel(x, y = nil, degree = 3, gamma = nil, coef = 1)
         y = x if y.nil?
         gamma ||= 1.0 / x.shape[1]
@@ -57,15 +57,15 @@ module SVMKit
 
       # Calculate the sigmoid kernel between x and y.
       #
-      # @param x [NMatrix] (shape: [n_samples_x, n_features])
-      # @param y [NMatrix] (shape: [n_samples_y, n_features])
+      # @param x [Numo::DFloat] (shape: [n_samples_x, n_features])
+      # @param y [Numo::DFloat] (shape: [n_samples_y, n_features])
       # @param gamma [Float] The parameter of polynomial kernel, if nil it is 1 / n_features.
       # @param coef [Integer] The parameter of polynomial kernel.
-      # @return [NMatrix] (shape: [n_samples_x, n_samples_x] or [n_samples_x, n_samples_y] if y is given)
+      # @return [Numo::DFloat] (shape: [n_samples_x, n_samples_x] or [n_samples_x, n_samples_y] if y is given)
       def sigmoid_kernel(x, y = nil, gamma = nil, coef = 1)
         y = x if y.nil?
         gamma ||= 1.0 / x.shape[1]
-        (x.dot(y.transpose) * gamma + coef).tanh
+        Numo::NMath.tanh(x.dot(y.transpose) * gamma + coef)
       end
     end
   end
--- spec/pairwise_metric_spec.rb
+++ spec/pairwise_metric_spec.rb
@@ -4,135 +4,141 @@ RSpec.describe SVMKit::PairwiseMetric do
   let(:n_features) { 3 }
   let(:n_samples_a) { 10 }
   let(:n_samples_b) { 5 }
-  let(:samples_a) { NMatrix.rand([n_samples_a, n_features]) }
-  let(:samples_b) { NMatrix.rand([n_samples_b, n_features]) }
+  let(:samples_a) { Numo::DFloat.new(n_samples_a, n_features).rand }
+  let(:samples_b) { Numo::DFloat.new(n_samples_b, n_features).rand }
   let(:degree) { 3 }
   let(:gamma) { 0.5 }
   let(:coef) { 1 }
 
   it 'calculates the euclidean distance matrix between different datasets.' do
-    dist_mat_bf = NMatrix.new(
-      [n_samples_a, n_samples_b],
-      [*0...n_samples_a].product([*0...n_samples_b]).map { |m, n| (samples_a.row(m) - samples_b.row(n)).norm2 }
-    )
+    dist_mat_bf = Numo::DFloat.zeros(n_samples_a, n_samples_b)
+    n_samples_a.times do |m|
+      n_samples_b.times do |n|
+        dist_mat_bf[m, n] = Math.sqrt(((samples_a[m, true] - samples_b[n, true])**2).sum)
+      end
+    end
     dist_mat = described_class.euclidean_distance(samples_a, samples_b)
+    expect(dist_mat.class).to eq(Numo::DFloat)
     expect(dist_mat.shape[0]).to eq(n_samples_a)
     expect(dist_mat.shape[1]).to eq(n_samples_b)
     expect(dist_mat).to be_within(1.0e-5).of(dist_mat_bf)
   end
 
   it 'calculates the euclidean distance matrix of dataset.' do
-    dist_mat_bf = NMatrix.new(
-      [n_samples_a, n_samples_a],
-      [*0...n_samples_a].product([*0...n_samples_b]).map { |m, n| (samples_a.row(m) - samples_a.row(n)).norm2 }
-    )
+    dist_mat_bf = Numo::DFloat.zeros(n_samples_a, n_samples_a)
+    n_samples_a.times do |m|
+      n_samples_a.times do |n|
+        dist_mat_bf[m, n] = Math.sqrt(((samples_a[m, true] - samples_a[n, true])**2).sum)
+      end
+    end
     dist_mat = described_class.euclidean_distance(samples_a)
+    expect(dist_mat.class).to eq(Numo::DFloat)
     expect(dist_mat.shape[0]).to eq(n_samples_a)
     expect(dist_mat.shape[1]).to eq(n_samples_a)
     expect(dist_mat).to be_within(1.0e-5).of(dist_mat_bf)
   end
 
   it 'calculates the RBF kernel matrix of dataset.' do
-    kernel_mat_bf = NMatrix.new(
-      [n_samples_a, n_samples_a],
-      [*0...n_samples_a].product([*0...n_samples_b]).map do |m, n|
-        dist = (samples_a.row(m) - samples_a.row(n)).norm2
+    kernel_mat_bf = Numo::DFloat.asarray(
+      [*0...n_samples_a].product([*0...n_samples_a]).map do |m, n|
+        dist = Math.sqrt(((samples_a[m, true] - samples_a[n, true])**2).sum)
         Math.exp(-gamma * (dist**2))
       end
-    )
+    ).reshape(n_samples_a, n_samples_a)
     kernel_mat = described_class.rbf_kernel(samples_a, nil, gamma)
+    expect(kernel_mat.class).to eq(Numo::DFloat)
     expect(kernel_mat.shape[0]).to eq(n_samples_a)
     expect(kernel_mat.shape[1]).to eq(n_samples_a)
     expect(kernel_mat).to be_within(1.0e-8).of(kernel_mat_bf)
   end
 
   it 'calculates the RBF kernel matrix between different datasets.' do
-    kernel_mat_bf = NMatrix.new(
-      [n_samples_a, n_samples_b],
+    kernel_mat_bf = Numo::DFloat.asarray(
       [*0...n_samples_a].product([*0...n_samples_b]).map do |m, n|
-        dist = (samples_a.row(m) - samples_b.row(n)).norm2
+        dist = Math.sqrt(((samples_a[m, true] - samples_b[n, true])**2).sum)
         Math.exp(-gamma * (dist**2))
       end
-    )
+    ).reshape(n_samples_a, n_samples_b)
     kernel_mat = described_class.rbf_kernel(samples_a, samples_b, gamma)
+    expect(kernel_mat.class).to eq(Numo::DFloat)
     expect(kernel_mat.shape[0]).to eq(n_samples_a)
     expect(kernel_mat.shape[1]).to eq(n_samples_b)
     expect(kernel_mat).to be_within(1.0e-8).of(kernel_mat_bf)
   end
 
   it 'calculates the linear kernel matrix of dataset.' do
-    kernel_mat_bf = NMatrix.new(
-      [n_samples_a, n_samples_a],
-      [*0...n_samples_a].product([*0...n_samples_b]).map do |m, n|
-        (samples_a.row(m).dot(samples_a.row(n).transpose)).to_f
+    kernel_mat_bf = Numo::DFloat.asarray(
+      [*0...n_samples_a].product([*0...n_samples_a]).map do |m, n|
+        samples_a[m, true].dot(samples_a[n, true].transpose).to_f
       end
-    )
+    ).reshape(n_samples_a, n_samples_a)
     kernel_mat = described_class.linear_kernel(samples_a, nil)
+    expect(kernel_mat.class).to eq(Numo::DFloat)
     expect(kernel_mat.shape[0]).to eq(n_samples_a)
     expect(kernel_mat.shape[1]).to eq(n_samples_a)
     expect(kernel_mat).to be_within(1.0e-8).of(kernel_mat_bf)
   end
 
   it 'calculates the linear kernel matrix between different datasets.' do
-    kernel_mat_bf = NMatrix.new(
-      [n_samples_a, n_samples_b],
+    kernel_mat_bf = Numo::DFloat.asarray(
       [*0...n_samples_a].product([*0...n_samples_b]).map do |m, n|
-        (samples_a.row(m).dot(samples_b.row(n).transpose)).to_f
+        samples_a[m, true].dot(samples_b[n, true].transpose).to_f
       end
-    )
+    ).reshape(n_samples_a, n_samples_b)
     kernel_mat = described_class.linear_kernel(samples_a, samples_b)
+    expect(kernel_mat.class).to eq(Numo::DFloat)
     expect(kernel_mat.shape[0]).to eq(n_samples_a)
     expect(kernel_mat.shape[1]).to eq(n_samples_b)
     expect(kernel_mat).to be_within(1.0e-8).of(kernel_mat_bf)
   end
 
   it 'calculates the polynomial kernel matrix of dataset.' do
-    kernel_mat_bf = NMatrix.new(
-      [n_samples_a, n_samples_a],
-      [*0...n_samples_a].product([*0...n_samples_b]).map do |m, n|
-        ((samples_a.row(m).dot(samples_a.row(n).transpose)).to_f * gamma + coef)**degree
+    kernel_mat_bf = Numo::DFloat.asarray(
+      [*0...n_samples_a].product([*0...n_samples_a]).map do |m, n|
+        (samples_a[m, true].dot(samples_a[n, true].transpose).to_f * gamma + coef)**degree
       end
-    )
+    ).reshape(n_samples_a, n_samples_a)
     kernel_mat = described_class.polynomial_kernel(samples_a, nil, degree, gamma, coef)
+    expect(kernel_mat.class).to eq(Numo::DFloat)
     expect(kernel_mat.shape[0]).to eq(n_samples_a)
     expect(kernel_mat.shape[1]).to eq(n_samples_a)
     expect(kernel_mat).to be_within(1.0e-8).of(kernel_mat_bf)
   end
 
   it 'calculates the polynomial kernel matrix between different datasets.' do
-    kernel_mat_bf = NMatrix.new(
-      [n_samples_a, n_samples_b],
+    kernel_mat_bf = Numo::DFloat.asarray(
       [*0...n_samples_a].product([*0...n_samples_b]).map do |m, n|
-        ((samples_a.row(m).dot(samples_b.row(n).transpose)).to_f * gamma + coef)**degree
+        (samples_a[m, true].dot(samples_b[n, true].transpose).to_f * gamma + coef)**degree
       end
-    )
+    ).reshape(n_samples_a, n_samples_b)
     kernel_mat = described_class.polynomial_kernel(samples_a, samples_b, degree, gamma, coef)
+    expect(kernel_mat.class).to eq(Numo::DFloat)
     expect(kernel_mat.shape[0]).to eq(n_samples_a)
     expect(kernel_mat.shape[1]).to eq(n_samples_b)
     expect(kernel_mat).to be_within(1.0e-8).of(kernel_mat_bf)
   end
 
   it 'calculates the sigmoid kernel matrix of dataset.' do
-    kernel_mat_bf = NMatrix.new(
-      [n_samples_a, n_samples_a],
-      [*0...n_samples_a].product([*0...n_samples_b]).map do |m, n|
-        Math.tanh((samples_a.row(m).dot(samples_a.row(n).transpose)).to_f * gamma + coef)
+    kernel_mat_bf = Numo::DFloat.asarray(
+      [*0...n_samples_a].product([*0...n_samples_a]).map do |m, n|
+        Math.tanh(samples_a[m, true].dot(samples_a[n, true].transpose).to_f * gamma + coef)
       end
-    )
+    ).reshape(n_samples_a, n_samples_a)
     kernel_mat = described_class.sigmoid_kernel(samples_a, nil, gamma, coef)
+    expect(kernel_mat.class).to eq(Numo::DFloat)
     expect(kernel_mat.shape[0]).to eq(n_samples_a)
     expect(kernel_mat.shape[1]).to eq(n_samples_a)
     expect(kernel_mat).to be_within(1.0e-8).of(kernel_mat_bf)
   end
 
   it 'calculates the sigmoid kernel matrix between different datasets.' do
-    kernel_mat_bf = NMatrix.new(
-      [n_samples_a, n_samples_b],
+    kernel_mat_bf = Numo::DFloat.asarray(
       [*0...n_samples_a].product([*0...n_samples_b]).map do |m, n|
-        Math.tanh((samples_a.row(m).dot(samples_b.row(n).transpose)).to_f * gamma + coef)
+        Math.tanh(samples_a[m, true].dot(samples_b[n, true].transpose).to_f * gamma + coef)
       end
-    )
+    ).reshape(n_samples_a, n_samples_b)
     kernel_mat = described_class.sigmoid_kernel(samples_a, samples_b, gamma, coef)
+    expect(kernel_mat.class).to eq(Numo::DFloat)
     expect(kernel_mat.shape[0]).to eq(n_samples_a)
     expect(kernel_mat.shape[1]).to eq(n_samples_b)
     expect(kernel_mat).to be_within(1.0e-8).of(kernel_mat_bf)
