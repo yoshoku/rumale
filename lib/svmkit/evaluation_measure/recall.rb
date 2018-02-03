66c8d59319e97a8faab66735d944043a287b6a30
yoshoku <yoshoku@outlook.com>
2018-02-03 22:05:02 +0900

    :sparkles: add classes for calculation of precision and recall

---
+++ lib/svmkit/evaluation_measure/recall.rb
@@ -0,0 +1,44 @@
+require 'svmkit/base/evaluator'
+require 'svmkit/evaluation_measure/precision_recall'
+
+module SVMKit
+  # This module consists of the classes for model evaluation.
+  module EvaluationMeasure
+    # Recall is a class that calculates the recall of the predicted labels.
+    #
+    # @example
+    #   evaluator = SVMKit::EvaluationMeasure::Recall.new
+    #   puts evaluator.score(ground_truth, predicted)
+    class Recall
+      include Base::Evaluator
+      include EvaluationMeasure::PrecisionRecall
+
+      # Return the average type for calculation of recall.
+      # @return [String] ('binary', 'micro', 'macro')
+      attr_reader :average
+
+      # Create a new evaluation measure calculater for recall score.
+      #
+      # @param average [String] The average type ('binary', 'micro', 'macro')
+      def initialize(average: 'binary')
+        @average = average
+      end
+
+      # Claculate average recall
+      #
+      # @param y_true [Numo::Int32] (shape: [n_samples]) Ground truth labels.
+      # @param y_pred [Numo::Int32] (shape: [n_samples]) Predicted labels.
+      # @return [Float] Average recall
+      def score(y_true, y_pred)
+        case @average
+        when 'binary'
+          recall_each_class(y_true, y_pred).last
+        when 'micro'
+          micro_average_recall(y_true, y_pred)
+        when 'macro'
+          macro_average_recall(y_true, y_pred)
+        end
+      end
+    end
+  end
+end
---
+++ spec/evaluation_measure/recall_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+RSpec.describe SVMKit::EvaluationMeasure::Recall do
+  let(:bin_ground_truth) { Numo::Int32[1, 1, 1, 1, 1, -1, -1, -1, -1, -1] }
+  let(:bin_predicted) { Numo::Int32[-1, -1, 1, 1, 1, -1, -1, 1, 1, 1] }
+  let(:mult_ground_truth) { Numo::Int32[0, 1, 2, 0, 1, 2, 3, 3, 0, 0] }
+  let(:mult_predicted) { Numo::Int32[0, 2, 1, 2, 1, 0, 3, 3, 0, 0] }
+
+  it 'calculates average recall for binary classification task.' do
+    evaluator = described_class.new(average: 'binary')
+    recall = evaluator.score(bin_ground_truth, bin_predicted)
+    expect(recall.class).to eq(Float)
+    expect(recall).to eq(0.6)
+  end
+
+  it 'calculates macro-average recall for multilabel classification task.' do
+    evaluator = described_class.new(average: 'macro')
+    recall = evaluator.score(mult_ground_truth, mult_predicted)
+    expect(recall.class).to eq(Float)
+    expect(recall).to eq(0.5625)
+  end
+
+  it 'calculates micro-average recall for multilabel classification task.' do
+    evaluator = described_class.new(average: 'micro')
+    recall = evaluator.score(mult_ground_truth, mult_predicted)
+    expect(recall.class).to eq(Float)
+    expect(recall).to eq(0.6)
+  end
+
+  it 'returns nil given an invalid average parameter.' do
+    evaluator = described_class.new(average: 'foo')
+    recall = evaluator.score(mult_ground_truth, mult_predicted)
+    expect(recall.class).to eq(NilClass)
+    expect(recall).to be_nil
+  end
+end
