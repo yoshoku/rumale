bf08a2181bc02d83a1559b6ee831b22e0effa7a5
yoshoku <yoshoku@outlook.com>
2018-02-23 22:38:46 +0900

    :sparkles: add loss function parameter to Factorization Machine classifier

--- spec/polynomial_model/factorization_machine_spec.rb
+++ spec/polynomial_model/factorization_machine_spec.rb
@@ -7,6 +7,11 @@ RSpec.describe SVMKit::PolynomialModel::FactorizationMachineClassifier do
     described_class.new(n_factors: 2, reg_param_bias: 0.001, reg_param_weight: 0.001, reg_param_factor: 0.001,
                         init_std: 0.01, max_iter: 1000, batch_size: 10, random_seed: 1)
   end
+  let(:estimator_logit) do
+    described_class.new(n_factors: 2, loss: 'logistic',
+                        reg_param_bias: 0.001, reg_param_weight: 0.001, reg_param_factor: 0.001,
+                        init_std: 0.01, max_iter: 1000, batch_size: 10, random_seed: 1)
+  end
 
   it 'classifies two clusters.' do
     n_samples, n_features = samples.shape
@@ -30,6 +35,18 @@ RSpec.describe SVMKit::PolynomialModel::FactorizationMachineClassifier do
     expect(score).to eq(1.0)
   end
 
+  it 'estimates class probabilities with two clusters dataset.' do
+    n_samples, _n_features = samples.shape
+    estimator_logit.fit(samples, labels)
+    probs = estimator_logit.predict_proba(samples)
+    expect(probs.class).to eq(Numo::DFloat)
+    expect(probs.shape[0]).to eq(n_samples)
+    expect(probs.shape[1]).to eq(2)
+    expect(probs.sum(1).eq(1).count).to eq(n_samples)
+    predicted = Numo::Int32.cast(probs[true, 0] < probs[true, 1]) * 2 - 1
+    expect(predicted).to eq(labels)
+  end
+
   it 'dumps and restores itself using Marshal module.' do
     estimator.fit(samples, labels)
     copied = Marshal.load(Marshal.dump(estimator))
@@ -39,6 +56,7 @@ RSpec.describe SVMKit::PolynomialModel::FactorizationMachineClassifier do
     expect(estimator.bias_term).to eq(copied.bias_term)
     expect(estimator.rng).to eq(copied.rng)
     expect(estimator.params[:n_factors]).to eq(copied.params[:n_factors])
+    expect(estimator.params[:loss]).to eq(copied.params[:loss])
     expect(estimator.params[:reg_param_bias]).to eq(copied.params[:reg_param_bias])
     expect(estimator.params[:reg_param_weight]).to eq(copied.params[:reg_param_weight])
     expect(estimator.params[:reg_param_factor]).to eq(copied.params[:reg_param_factor])
