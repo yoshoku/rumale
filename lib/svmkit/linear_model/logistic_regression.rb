f6d396372a48025e96786db97e932b0549c235e6
yoshoku <yoshoku@outlook.com>
2018-03-29 12:12:40 +0900

    :sparkles: incorporate multi-class classification with one-vs-rest strategy into Logistic Regression

--- lib/svmkit/linear_model/logistic_regression.rb
+++ lib/svmkit/linear_model/logistic_regression.rb
@@ -8,7 +8,6 @@ module SVMKit
   module LinearModel
     # LogisticRegression is a class that implements Logistic Regression
     # with stochastic gradient descent (SGD) optimization.
-    # Note that the class performs as a binary classifier.
     #
     # @example
     #   estimator =
@@ -23,14 +22,18 @@ module SVMKit
       include Base::Classifier
 
       # Return the weight vector for Logistic Regression.
-      # @return [Numo::DFloat] (shape: [n_features])
+      # @return [Numo::DFloat] (shape: [n_classes, n_features])
       attr_reader :weight_vec
 
       # Return the bias term (a.k.a. intercept) for Logistic Regression.
-      # @return [Float]
+      # @return [Numo::DFloat] (shape: [n_classes])
       attr_reader :bias_term
 
-      # Return the random generator for transformation.
+      # Return the class labels.
+      # @return [Numo::Int32] (shape: [n_classes])
+      attr_reader :classes
+
+      # Return the random generator for performing random sampling.
       # @return [Random]
       attr_reader :rng
 
@@ -42,38 +45,114 @@ module SVMKit
       #   If fit_bias is true, the feature vector v becoms [v; bias_scale].
       # @param max_iter [Integer] The maximum number of iterations.
       # @param batch_size [Integer] The size of the mini batches.
+      # @param normalize [Boolean] The flag indicating whether to normalize the weight vector.
       # @param random_seed [Integer] The seed value using to initialize the random generator.
-      def initialize(reg_param: 1.0, fit_bias: false, bias_scale: 1.0, max_iter: 100, batch_size: 50, random_seed: nil)
+      def initialize(reg_param: 1.0, fit_bias: false, bias_scale: 1.0,
+                     max_iter: 100, batch_size: 50, normalize: true, random_seed: nil)
         @params = {}
         @params[:reg_param] = reg_param
         @params[:fit_bias] = fit_bias
         @params[:bias_scale] = bias_scale
         @params[:max_iter] = max_iter
         @params[:batch_size] = batch_size
+        @params[:normalize] = normalize
         @params[:random_seed] = random_seed
         @params[:random_seed] ||= srand
         @weight_vec = nil
-        @bias_term = 0.0
+        @bias_term = nil
+        @classes = nil
         @rng = Random.new(@params[:random_seed])
       end
 
       # Fit the model with given training data.
       #
       # @param x [Numo::DFloat] (shape: [n_samples, n_features]) The training data to be used for fitting the model.
-      # @param y [Numo::Int32] (shape: [n_samples]) The categorical variables (e.g. labels)
-      #   to be used for fitting the model.
-      # @return [LogisticRegression] The learned classifier itself.
+      # @param y [Numo::Int32] (shape: [n_samples]) The labels to be used for fitting the model.
+      # @return [FactorizationMachineClassifier] The learned classifier itself.
       def fit(x, y)
-        # Generate binary labels.
-        negative_label = y.to_a.uniq.sort.shift
-        bin_y = y.to_a.map { |l| l != negative_label ? 1 : 0 }
-        # Expand feature vectors for bias term.
-        samples = x
-        if @params[:fit_bias]
-          samples = Numo::NArray.hstack(
-            [samples, Numo::DFloat.ones([x.shape[0], 1]) * @params[:bias_scale]]
-          )
+        @classes = Numo::Int32[*y.to_a.uniq.sort]
+        n_classes = @classes.size
+        _n_samples, n_features = x.shape
+
+        if n_classes > 2
+          @weight_vec = Numo::DFloat.zeros(n_classes, n_features)
+          @bias_term = Numo::DFloat.zeros(n_classes)
+          n_classes.times do |n|
+            bin_y = Numo::Int32.cast(y.eq(@classes[n]))
+            weight, bias = binary_fit(x, bin_y)
+            @weight_vec[n, true] = weight
+            @bias_term[n] = bias
+          end
+        else
+          negative_label = y.to_a.uniq.sort.first
+          bin_y = Numo::Int32.cast(y.ne(negative_label))
+          @weight_vec, @bias_term = binary_fit(x, bin_y)
         end
+
+        self
+      end
+
+      # Calculate confidence scores for samples.
+      #
+      # @param x [Numo::DFloat] (shape: [n_samples, n_features]) The samples to compute the scores.
+      # @return [Numo::DFloat] (shape: [n_samples, n_classes]) Confidence score per sample.
+      def decision_function(x)
+        x.dot(@weight_vec.transpose) + @bias_term
+      end
+
+      # Predict class labels for samples.
+      #
+      # @param x [Numo::DFloat] (shape: [n_samples, n_features]) The samples to predict the labels.
+      # @return [Numo::Int32] (shape: [n_samples]) Predicted class label per sample.
+      def predict(x)
+        return Numo::Int32.cast(decision_function(x).ge(0.5)) * 2 - 1 if @classes.size <= 2
+
+        n_samples, = x.shape
+        decision_values = decision_function(x)
+        Numo::Int32.asarray(Array.new(n_samples) { |n| @classes[decision_values[n, true].max_index] })
+      end
+
+      # Predict probability for samples.
+      #
+      # @param x [Numo::DFloat] (shape: [n_samples, n_features]) The samples to predict the probailities.
+      # @return [Numo::DFloat] (shape: [n_samples, n_classes]) Predicted probability of each class per sample.
+      def predict_proba(x)
+        proba = 1.0 / (Numo::NMath.exp(-decision_function(x)) + 1.0)
+        return (proba.transpose / proba.sum(axis: 1)).transpose if @classes.size > 2
+
+        n_samples, = x.shape
+        probs = Numo::DFloat.zeros(n_samples, 2)
+        probs[true, 1] = proba
+        probs[true, 0] = 1.0 - proba
+        probs
+      end
+
+      # Dump marshal data.
+      # @return [Hash] The marshal data about LogisticRegression.
+      def marshal_dump
+        { params: @params,
+          weight_vec: @weight_vec,
+          bias_term: @bias_term,
+          classes: @classes,
+          rng: @rng }
+      end
+
+      # Load marshal data.
+      # @return [nil]
+      def marshal_load(obj)
+        @params = obj[:params]
+        @weight_vec = obj[:weight_vec]
+        @bias_term = obj[:bias_term]
+        @classes = obj[:classes]
+        @rng = obj[:rng]
+        nil
+      end
+
+      private
+
+      def binary_fit(x, bin_y)
+        # Expand feature vectors for bias term.
+        samples = @params[:fit_bias] ? expand_feature(x) : x
         # Initialize some variables.
         n_samples, n_features = samples.shape
         rand_ids = [*0...n_samples].shuffle(random: @rng)
@@ -84,79 +163,33 @@ module SVMKit
           subset_ids = rand_ids.shift(@params[:batch_size])
           rand_ids.concat(subset_ids)
           # update the weight vector.
-          eta = 1.0 / (@params[:reg_param] * (t + 1))
-          mean_vec = Numo::DFloat.zeros(n_features)
-          subset_ids.each do |n|
-            z = weight_vec.dot(samples[n, true])
-            coef = bin_y[n] / (1.0 + Math.exp(bin_y[n] * z))
-            mean_vec += samples[n, true] * coef
-          end
-          mean_vec *= eta / @params[:batch_size]
-          weight_vec = weight_vec * (1.0 - eta * @params[:reg_param]) + mean_vec
+          df = samples[subset_ids, true].dot(weight_vec.transpose)
+          coef = bin_y[subset_ids] / (Numo::NMath.exp(-bin_y[subset_ids] * df) + 1.0)
+          mean_vec = coef.dot(samples[subset_ids, true]) / @params[:batch_size]
+          weight_vec -= learning_rate(t) * (@params[:reg_param] * weight_vec - mean_vec)
           # scale the weight vector.
-          norm = Math.sqrt(weight_vec.dot(weight_vec))
-          scaler = (1.0 / @params[:reg_param]**0.5) / (norm + 1.0e-12)
-          weight_vec *= [1.0, scaler].min
+          normalize_weight_vec(weight_vec) if @params[:normalize]
         end
-        # Store the learned model.
-        if @params[:fit_bias]
-          @weight_vec = weight_vec[0...n_features - 1]
-          @bias_term = weight_vec[n_features - 1]
-        else
-          @weight_vec = weight_vec[0...n_features]
-          @bias_term = 0.0
-        end
-        self
+        split_weight_vec_bias(weight_vec)
       end
 
-      # Calculate confidence scores for samples.
-      #
-      # @param x [Numo::DFloat] (shape: [n_samples, n_features]) The samples to compute the scores.
-      # @return [Numo::DFloat] (shape: [n_samples]) Confidence score per sample.
-      def decision_function(x)
-        @weight_vec.dot(x.transpose) + @bias_term
+      def expand_feature(x)
+        Numo::NArray.hstack([x, Numo::DFloat.ones([x.shape[0], 1]) * @params[:bias_scale]])
       end
 
-      # Predict class labels for samples.
-      #
-      # @param x [Numo::DFloat] (shape: [n_samples, n_features]) The samples to predict the labels.
-      # @return [Numo::Int32] (shape: [n_samples]) Predicted class label per sample.
-      def predict(x)
-        Numo::Int32.cast(sigmoid(decision_function(x)).map { |v| v >= 0.5 ? 1 : -1 })
+      def learning_rate(iter)
+        1.0 / (@params[:reg_param] * (iter + 1))
       end
 
-      # Predict probability for samples.
-      #
-      # @param x [Numo::DFloat] (shape: [n_samples, n_features]) The samples to predict the probailities.
-      # @return [Numo::DFloat] (shape: [n_samples, n_classes]) Predicted probability of each class per sample.
-      def predict_proba(x)
-        n_samples, = x.shape
-        proba = Numo::DFloat.zeros(n_samples, 2)
-        proba[true, 1] = sigmoid(decision_function(x))
-        proba[true, 0] = 1.0 - proba[true, 1]
-        proba
+      def normalize_weight_vec(weight_vec)
+        norm = Math.sqrt(weight_vec.dot(weight_vec))
+        weight_vec * [1.0, (1.0 / @params[:reg_param]**0.5) / (norm + 1.0e-12)].min
       end
 
-      # Dump marshal data.
-      # @return [Hash] The marshal data about LogisticRegression.
-      def marshal_dump
-        { params: @params, weight_vec: @weight_vec, bias_term: @bias_term, rng: @rng }
-      end
-
-      # Load marshal data.
-      # @return [nil]
-      def marshal_load(obj)
-        @params = obj[:params]
-        @weight_vec = obj[:weight_vec]
-        @bias_term = obj[:bias_term]
-        @rng = obj[:rng]
-        nil
-      end
-
-      private
-
-      def sigmoid(x)
-        1.0 / (Numo::NMath.exp(-x) + 1.0)
+      def split_weight_vec_bias(weight_vec)
+        weights = @params[:fit_bias] ? weight_vec[0...-1] : weight_vec
+        bias = @params[:fit_bias] ? weight_vec[-1] : 0.0
+        [weights, bias]
       end
     end
   end
--- spec/linear_model/logistic_regression_spec.rb
+++ spec/linear_model/logistic_regression_spec.rb
@@ -3,70 +3,120 @@
 require 'spec_helper'
 
 RSpec.describe SVMKit::LinearModel::LogisticRegression do
-  let(:samples) { Marshal.load(File.read(__dir__ + '/../test_samples.dat')) }
-  let(:labels) { Marshal.load(File.read(__dir__ + '/../test_labels.dat')) }
-  let(:estimator) { described_class.new(reg_param: 1.0, max_iter: 100, batch_size: 20, random_seed: 1) }
-  let(:estimator_bias) do
-    described_class.new(reg_param: 1.0, fit_bias: true, max_iter: 100, batch_size: 20, random_seed: 1)
-  end
+  let(:x_bin) { Marshal.load(File.read(__dir__ + '/../test_samples.dat')) }
+  let(:y_bin) { Marshal.load(File.read(__dir__ + '/../test_labels.dat')) }
+  let(:x_mlt) { Marshal.load(File.read(__dir__ + '/../test_samples_three_clusters.dat')) }
+  let(:y_mlt) { Marshal.load(File.read(__dir__ + '/../test_labels_three_clusters.dat')) }
+  let(:estimator) { described_class.new(reg_param: 1.0, random_seed: 1) }
+  let(:estimator_bias) { described_class.new(reg_param: 1.0, fit_bias: true, random_seed: 1) }
 
   it 'classifies two clusters.' do
-    n_samples, n_features = samples.shape
-    estimator.fit(samples, labels)
+    n_samples, n_features = x_bin.shape
+    estimator.fit(x_bin, y_bin)
+
+    expect(estimator.classes.class).to eq(Numo::Int32)
+    expect(estimator.classes.size).to eq(2)
+    expect(estimator.classes.shape[0]).to eq(2)
+    expect(estimator.classes.shape[1]).to be_nil
+
+    expect(estimator.weight_vec.class).to eq(Numo::DFloat)
     expect(estimator.weight_vec.size).to eq(n_features)
     expect(estimator.weight_vec.shape[0]).to eq(n_features)
     expect(estimator.weight_vec.shape[1]).to be_nil
     expect(estimator.bias_term).to be_zero
 
-    func_vals = estimator.decision_function(samples)
+    func_vals = estimator.decision_function(x_bin)
     expect(func_vals.class).to eq(Numo::DFloat)
     expect(func_vals.shape[0]).to eq(n_samples)
     expect(func_vals.shape[1]).to be_nil
 
-    predicted = estimator.predict(samples)
+    predicted = estimator.predict(x_bin)
     expect(predicted.class).to eq(Numo::Int32)
     expect(predicted.shape[0]).to eq(n_samples)
     expect(predicted.shape[1]).to be_nil
-    expect(predicted).to eq(labels)
 
-    expect(estimator.score(samples, labels)).to eq(1.0)
+    expect(predicted).to eq(y_bin)
+    expect(estimator.score(x_bin, y_bin)).to eq(1.0)
   end
 
   it 'learns the model of two clusters dataset with bias term.' do
-    _n_samples, n_features = samples.shape
-    estimator_bias.fit(samples, labels)
+    _n_samples, n_features = x_bin.shape
+    estimator_bias.fit(x_bin, y_bin)
     expect(estimator_bias.weight_vec.size).to eq(n_features)
     expect(estimator_bias.weight_vec.shape[0]).to eq(n_features)
     expect(estimator_bias.weight_vec.shape[1]).to be_nil
     expect(estimator_bias.bias_term).to_not be_zero
-    expect(estimator_bias.score(samples, labels)).to eq(1.0)
+    expect(estimator_bias.score(x_bin, y_bin)).to eq(1.0)
   end
 
   it 'estimates class probabilities with two clusters dataset.' do
-    n_samples, _n_features = samples.shape
-    estimator.fit(samples, labels)
-    probs = estimator.predict_proba(samples)
+    n_samples, _n_features = x_bin.shape
+    estimator.fit(x_bin, y_bin)
+    probs = estimator.predict_proba(x_bin)
     expect(probs.class).to eq(Numo::DFloat)
     expect(probs.shape[0]).to eq(n_samples)
     expect(probs.shape[1]).to eq(2)
     expect(probs.sum(1).eq(1).count).to eq(n_samples)
     predicted = Numo::Int32.cast(probs[true, 0] < probs[true, 1]) * 2 - 1
-    expect(predicted).to eq(labels)
+    expect(predicted).to eq(y_bin)
   end
 
   it 'dumps and restores itself using Marshal module.' do
-    estimator.fit(samples, labels)
-    copied = Marshal.load(Marshal.dump(estimator))
-    expect(estimator.class).to eq(copied.class)
-    expect(estimator.params[:reg_param]).to eq(copied.params[:reg_param])
-    expect(estimator.params[:fit_bias]).to eq(copied.params[:fit_bias])
-    expect(estimator.params[:bias_scale]).to eq(copied.params[:bias_scale])
-    expect(estimator.params[:max_iter]).to eq(copied.params[:max_iter])
-    expect(estimator.params[:batch_size]).to eq(copied.params[:batch_size])
-    expect(estimator.params[:random_seed]).to eq(copied.params[:random_seed])
-    expect(estimator.weight_vec).to eq(copied.weight_vec)
-    expect(estimator.bias_term).to eq(copied.bias_term)
-    expect(estimator.rng).to eq(copied.rng)
-    expect(copied.score(samples, labels)).to eq(1.0)
+    estimator_bias.fit(x_mlt, y_mlt)
+    copied = Marshal.load(Marshal.dump(estimator_bias))
+    expect(estimator_bias.class).to eq(copied.class)
+    expect(estimator_bias.params[:reg_param]).to eq(copied.params[:reg_param])
+    expect(estimator_bias.params[:fit_bias]).to eq(copied.params[:fit_bias])
+    expect(estimator_bias.params[:bias_scale]).to eq(copied.params[:bias_scale])
+    expect(estimator_bias.params[:max_iter]).to eq(copied.params[:max_iter])
+    expect(estimator_bias.params[:batch_size]).to eq(copied.params[:batch_size])
+    expect(estimator_bias.params[:random_seed]).to eq(copied.params[:random_seed])
+    expect(estimator_bias.weight_vec).to eq(copied.weight_vec)
+    expect(estimator_bias.bias_term).to eq(copied.bias_term)
+    expect(estimator_bias.rng).to eq(copied.rng)
+    expect(copied.score(x_mlt, y_mlt)).to eq(1.0)
+  end
+
+  it 'classifies three clusters.' do
+    n_classes = y_mlt.to_a.uniq.size
+    n_samples, n_features = x_mlt.shape
+
+    estimator_bias.fit(x_mlt, y_mlt)
+
+    expect(estimator_bias.classes.class).to eq(Numo::Int32)
+    expect(estimator_bias.classes.size).to eq(n_classes)
+    expect(estimator_bias.classes.shape[0]).to eq(n_classes)
+    expect(estimator_bias.classes.shape[1]).to be_nil
+
+    expect(estimator_bias.weight_vec.class).to eq(Numo::DFloat)
+    expect(estimator_bias.weight_vec.size).to eq(n_classes * n_features)
+    expect(estimator_bias.weight_vec.shape[0]).to eq(n_classes)
+    expect(estimator_bias.weight_vec.shape[1]).to eq(n_features)
+
+    expect(estimator_bias.bias_term.class).to eq(Numo::DFloat)
+    expect(estimator_bias.bias_term.size).to eq(n_classes)
+    expect(estimator_bias.bias_term.shape[0]).to eq(n_classes)
+    expect(estimator_bias.bias_term.shape[1]).to be_nil
+
+    predicted = estimator_bias.predict(x_mlt)
+    expect(predicted.class).to eq(Numo::Int32)
+    expect(predicted.shape[0]).to eq(n_samples)
+    expect(predicted.shape[1]).to be_nil
+
+    expect(predicted).to eq(y_mlt)
+    expect(estimator_bias.score(x_mlt, y_mlt)).to eq(1.0)
+  end
+
+  it 'estimates class probabilities with three clusters dataset.' do
+    classes = y_mlt.to_a.uniq.sort
+    n_classes = classes.size
+    n_samples, _n_features = x_mlt.shape
+    estimator.fit(x_mlt, y_mlt)
+    probs = estimator.predict_proba(x_mlt)
+    expect(probs.class).to eq(Numo::DFloat)
+    expect(probs.shape[0]).to eq(n_samples)
+    expect(probs.shape[1]).to eq(n_classes)
+    predicted = Numo::Int32[*Array.new(n_samples) { |n| classes[probs[n, true].max_index] }]
+    expect(predicted).to eq(y_mlt)
   end
 end
