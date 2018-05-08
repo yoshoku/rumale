b1b75f60dd4cdfccb8f96e89a03381c7328e878d
yoshoku <yoshoku@outlook.com>
2018-05-08 09:08:23 +0900

    :sparkles: Add abstract class for regression

---
+++ lib/svmkit/base/regressor.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+module SVMKit
+  module Base
+    # Module for all regressors in SVMKit.
+    module Regressor
+      # An abstract method for fitting a model.
+      def fit
+        raise NotImplementedError, "#{__method__} has to be implemented in #{self.class}."
+      end
+
+      # An abstract method for predicting labels.
+      def predict
+        raise NotImplementedError, "#{__method__} has to be implemented in #{self.class}."
+      end
+
+      # Calculate the coefficient of determination for the given testing data.
+      #
+      # @param x [Numo::DFloat] (shape: [n_samples, n_features]) Testing data.
+      # @param y [Numo::DFloat] (shape: [n_samples, n_outputs]) Target values for testing data.
+      # @return [Float] Coefficient of determination
+      def score(x, y)
+        SVMKit::Validation.check_sample_array(x)
+        SVMKit::Validation.check_tvalue_array(y)
+        SVMKit::Validation.check_sample_tvalue_size(x, y)
+        evaluator = SVMKit::EvaluationMeasure::R2Score.new
+        evaluator.score(y, predict(x))
+      end
+    end
+  end
+end
---
+++ spec/base/regressor_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe SVMKit::Base::Regressor do
+  let(:dummy_class) do
+    class Dummy
+      include SVMKit::Base::Regressor
+    end
+    Dummy.new
+  end
+
+  it 'raises NotImplementedError when the fit method is not implemented.' do
+    expect { dummy_class.fit }.to raise_error(NotImplementedError)
+  end
+
+  it 'raises NotImplementedError when the predict method is not implemented.' do
+    expect { dummy_class.predict }.to raise_error(NotImplementedError)
+  end
+end
