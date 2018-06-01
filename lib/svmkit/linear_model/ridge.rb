4f2db56c808df1426a28d8deb502dcc65ad714f5
yoshoku <yoshoku@outlook.com>
2018-06-01 12:49:44 +0900

    :art: Fix to use Nadam for optimization of Ridge

--- lib/svmkit/linear_model/ridge.rb
+++ lib/svmkit/linear_model/ridge.rb
@@ -3,22 +3,19 @@
 require 'svmkit/validation'
 require 'svmkit/base/base_estimator'
 require 'svmkit/base/regressor'
+require 'svmkit/optimizer/nadam'
 
 module SVMKit
   module LinearModel
     # Ridge is a class that implements Ridge Regression
-    # with stochastic gradient descent (SGD) optimization.
+    # with mini-batch stochastic gradient descent optimization.
     #
     # @example
     #   estimator =
-    #     SVMKit::LinearModel::Ridge.new(reg_param: 0.1, max_iter: 5000, batch_size: 50, random_seed: 1)
+    #     SVMKit::LinearModel::Ridge.new(reg_param: 0.1, max_iter: 1000, batch_size: 20, random_seed: 1)
     #   estimator.fit(training_samples, traininig_values)
     #   results = estimator.predict(testing_samples)
     #
-    # *Reference*
-    # - S. Shalev-Shwartz and Y. Singer, "Pegasos: Primal Estimated sub-GrAdient SOlver for SVM," Proc. ICML'07, pp. 807--814, 2007.
-    # - I. Sutskever, J. Martens, G. Dahl, and G. Hinton, "On the importance of initialization and momentum in deep learning," Proc. ICML'13, pp. 1139--1147, 2013.
-    # - G. Hinton, N. Srivastava, and K. Swersky, "Lecture 6e rmsprop," Neural Networks for Machine Learning, 2012.
     class Ridge
       include Base::BaseEstimator
       include Base::Regressor
@@ -40,30 +37,21 @@ module SVMKit
       #
       # @param reg_param [Float] The regularization parameter.
       # @param fit_bias [Boolean] The flag indicating whether to fit the bias term.
-      # @param learning_rate [Float] The learning rate for optimization.
-      # @param decay [Float] The discounting factor for RMS prop optimization.
-      # @param momentum [Float] The Nesterov momentum for optimization.
       # @param max_iter [Integer] The maximum number of iterations.
       # @param batch_size [Integer] The size of the mini batches.
       # @param random_seed [Integer] The seed value using to initialize the random generator.
-      def initialize(reg_param: 1.0, fit_bias: false, learning_rate: 0.01, decay: 0.9, momentum: 0.9,
-                     max_iter: 1000, batch_size: 10, random_seed: nil)
-        check_params_float(reg_param: reg_param,
-                           learning_rate: learning_rate, decay: decay, momentum: momentum)
+      def initialize(reg_param: 1.0, fit_bias: false, max_iter: 1000, batch_size: 10, optimizer: nil, random_seed: nil)
+        check_params_float(reg_param: reg_param)
         check_params_integer(max_iter: max_iter, batch_size: batch_size)
         check_params_boolean(fit_bias: fit_bias)
         check_params_type_or_nil(Integer, random_seed: random_seed)
-        check_params_positive(reg_param: reg_param,
-                              learning_rate: learning_rate, decay: decay, momentum: momentum,
-                              max_iter: max_iter, batch_size: batch_size)
+        check_params_positive(reg_param: reg_param, max_iter: max_iter, batch_size: batch_size)
         @params = {}
         @params[:reg_param] = reg_param
         @params[:fit_bias] = fit_bias
-        @params[:learning_rate] = learning_rate
-        @params[:decay] = decay
-        @params[:momentum] = momentum
         @params[:max_iter] = max_iter
         @params[:batch_size] = batch_size
+        @params[:optimizer] = optimizer
         @params[:random_seed] = random_seed
         @params[:random_seed] ||= srand
         @weight_vec = nil
@@ -136,8 +124,7 @@ module SVMKit
         n_samples, n_features = samples.shape
         rand_ids = [*0...n_samples].shuffle(random: @rng)
         weight_vec = Numo::DFloat.zeros(n_features)
-        weight_sqrsum = Numo::DFloat.zeros(n_features)
-        weight_update = Numo::DFloat.zeros(n_features)
+        optimizer = Optimizer::Nadam.new
         # Start optimization.
         @params[:max_iter].times do |_t|
           # Random sampling.
@@ -146,12 +133,10 @@ module SVMKit
           data = samples[subset_ids, true]
           values = y[subset_ids]
           # Calculate gradients for loss function.
-          loss_grad = loss_gradient(data, values, weight_vec - @params[:momentum] * weight_update)
+          loss_grad = loss_gradient(data, values, weight_vec)
           next if loss_grad.ne(0.0).count.zero?
           # Update weight.
-          weight_vec, weight_sqrsum, weight_update =
-            update_weight(weight_vec, weight_sqrsum, weight_update,
-                          weight_gradient(loss_grad, data, weight_vec - @params[:momentum] * weight_update))
+          weight_vec = optimizer.call(weight_vec, weight_gradient(loss_grad, data, weight_vec))
         end
         split_weight_vec_bias(weight_vec)
       end
@@ -162,13 +147,6 @@ module SVMKit
 
       def weight_gradient(loss_grad, data, weight)
         (loss_grad.expand_dims(1) * data).mean(0) + @params[:reg_param] * weight
-      end
-
-      def update_weight(weight, sqrsum, update, gr)
-        new_sqrsum = @params[:decay] * sqrsum + (1.0 - @params[:decay]) * gr**2
-        new_update = (@params[:learning_rate] / ((new_sqrsum + 1.0e-8)**0.5)) * gr
-        new_weight = weight - (new_update + @params[:momentum] * update)
-        [new_weight, new_sqrsum, new_update]
       end
 
       def expand_feature(x)
