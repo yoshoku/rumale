99d85b23eb82e81f48a0f6e0615d64f393b9d004
yoshoku <yoshoku@outlook.com>
2018-06-05 09:13:32 +0900

    :sparkles: Add optimizer class with SGD

---
+++ lib/svmkit/optimizer/sgd.rb
@@ -0,0 +1,65 @@
+# frozen_string_literal: true
+
+require 'svmkit/validation'
+require 'svmkit/base/base_estimator'
+
+module SVMKit
+  module Optimizer
+    # SGD is a class that implements SGD optimizer.
+    #
+    # @example
+    #   optimizer = SVMKit::Optimizer::SGD.new(learning_rate: 0.01, momentum: 0.9, decay: 0.9)
+    #   estimator = SVMKit::LinearModel::LinearRegression.new(optimizer: optimizer, random_seed: 1)
+    #   estimator.fit(samples, values)
+    class SGD
+      include Base::BaseEstimator
+      include Validation
+
+      # Create a new optimizer with SGD.
+      #
+      # @param learning_rate [Float] The initial value of learning rate.
+      # @param momentum [Float] The initial value of momentum.
+      # @param decay [Float] The smooting parameter.
+      def initialize(learning_rate: 0.01, momentum: 0.0, decay: 0.0)
+        check_params_float(learning_rate: learning_rate, momentum: momentum, decay: decay)
+        check_params_positive(learning_rate: learning_rate, momentum: momentum, decay: decay)
+        @params = {}
+        @params[:learning_rate] = learning_rate
+        @params[:momentum] = momentum
+        @params[:decay] = decay
+        @iter = 0
+        @update = nil
+      end
+
+      # Calculate the updated weight with SGD.
+      #
+      # @param weight [Numo::DFloat] (shape: [n_features]) The weight to be updated.
+      # @param gradient [Numo::DFloat] (shape: [n_features]) The gradient for updating the weight.
+      # @return [Numo::DFloat] (shape: [n_feautres]) The updated weight.
+      def call(weight, gradient)
+        @update ||= Numo::DFloat.zeros(weight.shape[0])
+        current_learning_rate = @params[:learning_rate] / (1.0 + @params[:decay] * @iter)
+        @iter += 1
+        @update = @params[:momentum] * @update - current_learning_rate * gradient
+        weight + @update
+      end
+
+      # Dump marshal data.
+      # @return [Hash] The marshal data.
+      def marshal_dump
+        { params: @params,
+          iter: @iter,
+          update: @update }
+      end
+
+      # Load marshal data.
+      # @return [nil]
+      def marshal_load(obj)
+        @params = obj[:params]
+        @iter = obj[:iter]
+        @update = obj[:update]
+        nil
+      end
+    end
+  end
+end
---
+++ spec/optimizer/sgd_spec.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe SVMKit::Optimizer::SGD do
+  let(:x) { Marshal.load(File.read(__dir__ + '/../test_samples.dat')) }
+  let(:y) { x.dot(Numo::DFloat[1.0, 2.0]) }
+  let(:y_mult) { x.dot(Numo::DFloat[[1.0, 2.0], [2.0, 1.0]]) }
+  let(:optimizer) { described_class.new(learning_rate: 0.1, momentum: 0.9, decay: 0.1) }
+  let(:estimator) { SVMKit::LinearModel::LinearRegression.new(optimizer: optimizer, max_iter: 100, random_seed: 1) }
+
+  it 'learns the model for single regression problem with optimizer.' do
+    estimator.fit(x, y)
+    expect(estimator.score(x, y)).to be_within(0.01).of(1.0)
+  end
+
+  it 'learns the model for multiple-regression problems with optimizer.' do
+    estimator.fit(x, y_mult)
+    expect(estimator.score(x, y_mult)).to be_within(0.01).of(1.0)
+  end
+
+  it 'dumps and restores itself using Marshal module.' do
+    optimizer.call(Numo::DFloat.new(3).rand, Numo::DFloat.new(3).rand)
+    copied = Marshal.load(Marshal.dump(optimizer))
+    expect(optimizer.class).to eq(copied.class)
+    expect(optimizer.params).to eq(copied.params)
+    expect(optimizer.instance_variable_get(:@update)).to eq(copied.instance_variable_get(:@update))
+    expect(optimizer.instance_variable_get(:@iter)).to eq(copied.instance_variable_get(:@iter))
+  end
+end
