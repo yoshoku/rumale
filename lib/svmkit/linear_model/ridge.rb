bf0945a106bb190226429e28e97700dca6d65d29
yoshoku <yoshoku@outlook.com>
2018-06-05 21:08:14 +0900

    :art: Fix optimizer to be selectable on Ridge

--- lib/svmkit/linear_model/ridge.rb
+++ lib/svmkit/linear_model/ridge.rb
@@ -39,6 +39,8 @@ module SVMKit
       # @param fit_bias [Boolean] The flag indicating whether to fit the bias term.
       # @param max_iter [Integer] The maximum number of iterations.
       # @param batch_size [Integer] The size of the mini batches.
+      # @param optimizer [Optimizer] The optimizer to calculate adaptive learning rate.
+      #   If nil is given, Nadam is used.
       # @param random_seed [Integer] The seed value using to initialize the random generator.
       def initialize(reg_param: 1.0, fit_bias: false, max_iter: 1000, batch_size: 10, optimizer: nil, random_seed: nil)
         check_params_float(reg_param: reg_param)
@@ -52,6 +54,7 @@ module SVMKit
         @params[:max_iter] = max_iter
         @params[:batch_size] = batch_size
         @params[:optimizer] = optimizer
+        @params[:optimizer] ||= Optimizer::Nadam.new
         @params[:random_seed] = random_seed
         @params[:random_seed] ||= srand
         @weight_vec = nil
@@ -75,11 +78,7 @@ module SVMKit
         if n_outputs > 1
           @weight_vec = Numo::DFloat.zeros(n_outputs, n_features)
           @bias_term = Numo::DFloat.zeros(n_outputs)
-          n_outputs.times do |n|
-            weight, bias = single_fit(x, y[true, n])
-            @weight_vec[n, true] = weight
-            @bias_term[n] = bias
-          end
+          n_outputs.times { |n| @weight_vec[n, true], @bias_term[n] = single_fit(x, y[true, n]) }
         else
           @weight_vec, @bias_term = single_fit(x, y)
         end
@@ -124,7 +123,7 @@ module SVMKit
         n_samples, n_features = samples.shape
         rand_ids = [*0...n_samples].shuffle(random: @rng)
         weight_vec = Numo::DFloat.zeros(n_features)
-        optimizer = Optimizer::Nadam.new
+        optimizer = @params[:optimizer].dup
         # Start optimization.
         @params[:max_iter].times do |_t|
           # Random sampling.
