b99bc4bc0425934424d331dd08d8970f87eb02c1
yoshoku <yoshoku@outlook.com>
2018-06-05 09:09:06 +0900

    :sparkles: Add regressor class with ordinary least square linear regression

---
+++ lib/svmkit/linear_model/linear_regression.rb
@@ -0,0 +1,159 @@
+# frozen_string_literal: true
+
+require 'svmkit/validation'
+require 'svmkit/base/base_estimator'
+require 'svmkit/base/regressor'
+require 'svmkit/optimizer/nadam'
+
+module SVMKit
+  module LinearModel
+    # LinearRegression is a class that implements ordinary least square linear regression
+    # with mini-batch stochastic gradient descent optimization.
+    #
+    # @example
+    #   estimator =
+    #     SVMKit::LinearModel::LinearRegression.new(max_iter: 1000, batch_size: 20, random_seed: 1)
+    #   estimator.fit(training_samples, traininig_values)
+    #   results = estimator.predict(testing_samples)
+    #
+    class LinearRegression
+      include Base::BaseEstimator
+      include Base::Regressor
+      include Validation
+
+      # Return the weight vector.
+      # @return [Numo::DFloat] (shape: [n_outputs, n_features])
+      attr_reader :weight_vec
+
+      # Return the bias term (a.k.a. intercept).
+      # @return [Numo::DFloat] (shape: [n_outputs])
+      attr_reader :bias_term
+
+      # Return the random generator for random sampling.
+      # @return [Random]
+      attr_reader :rng
+
+      # Create a new ordinary least square linear regressor.
+      #
+      # @param fit_bias [Boolean] The flag indicating whether to fit the bias term.
+      # @param max_iter [Integer] The maximum number of iterations.
+      # @param batch_size [Integer] The size of the mini batches.
+      # @param optimizer [Optimizer] The optimizer to calculate adaptive learning rate.
+      #   If nil is given, Nadam is used.
+      # @param random_seed [Integer] The seed value using to initialize the random generator.
+      def initialize(fit_bias: false, max_iter: 1000, batch_size: 10, optimizer: nil, random_seed: nil)
+        check_params_integer(max_iter: max_iter, batch_size: batch_size)
+        check_params_boolean(fit_bias: fit_bias)
+        check_params_type_or_nil(Integer, random_seed: random_seed)
+        check_params_positive(max_iter: max_iter, batch_size: batch_size)
+        @params = {}
+        @params[:fit_bias] = fit_bias
+        @params[:max_iter] = max_iter
+        @params[:batch_size] = batch_size
+        @params[:optimizer] = optimizer
+        @params[:optimizer] ||= Optimizer::Nadam.new
+        @params[:random_seed] = random_seed
+        @params[:random_seed] ||= srand
+        @weight_vec = nil
+        @bias_term = nil
+        @rng = Random.new(@params[:random_seed])
+      end
+
+      # Fit the model with given training data.
+      #
+      # @param x [Numo::DFloat] (shape: [n_samples, n_features]) The training data to be used for fitting the model.
+      # @param y [Numo::Int32] (shape: [n_samples, n_outputs]) The target values to be used for fitting the model.
+      # @return [LinearRegression] The learned regressor itself.
+      def fit(x, y)
+        check_sample_array(x)
+        check_tvalue_array(y)
+        check_sample_tvalue_size(x, y)
+
+        n_outputs = y.shape[1].nil? ? 1 : y.shape[1]
+        n_features = x.shape[1]
+
+        if n_outputs > 1
+          @weight_vec = Numo::DFloat.zeros(n_outputs, n_features)
+          @bias_term = Numo::DFloat.zeros(n_outputs)
+          n_outputs.times { |n| @weight_vec[n, true], @bias_term[n] = single_fit(x, y[true, n]) }
+        else
+          @weight_vec, @bias_term = single_fit(x, y)
+        end
+
+        self
+      end
+
+      # Predict values for samples.
+      #
+      # @param x [Numo::DFloat] (shape: [n_samples, n_features]) The samples to predict the values.
+      # @return [Numo::DFloat] (shape: [n_samples, n_outputs]) Predicted values per sample.
+      def predict(x)
+        check_sample_array(x)
+        x.dot(@weight_vec.transpose) + @bias_term
+      end
+
+      # Dump marshal data.
+      # @return [Hash] The marshal data about LinearRegression.
+      def marshal_dump
+        { params: @params,
+          weight_vec: @weight_vec,
+          bias_term: @bias_term,
+          rng: @rng }
+      end
+
+      # Load marshal data.
+      # @return [nil]
+      def marshal_load(obj)
+        @params = obj[:params]
+        @weight_vec = obj[:weight_vec]
+        @bias_term = obj[:bias_term]
+        @rng = obj[:rng]
+        nil
+      end
+
+      private
+
+      def single_fit(x, y)
+        # Expand feature vectors for bias term.
+        samples = @params[:fit_bias] ? expand_feature(x) : x
+        # Initialize some variables.
+        n_samples, n_features = samples.shape
+        rand_ids = [*0...n_samples].shuffle(random: @rng)
+        weight_vec = Numo::DFloat.zeros(n_features)
+        optimizer = @params[:optimizer].dup
+        # Start optimization.
+        @params[:max_iter].times do |_t|
+          # Random sampling.
+          subset_ids = rand_ids.shift(@params[:batch_size])
+          rand_ids.concat(subset_ids)
+          data = samples[subset_ids, true]
+          values = y[subset_ids]
+          # Calculate gradients for loss function.
+          loss_grad = loss_gradient(data, values, weight_vec)
+          next if loss_grad.ne(0.0).count.zero?
+          # Update weight.
+          weight_vec = optimizer.call(weight_vec, weight_gradient(loss_grad, data, weight_vec))
+        end
+        split_weight_vec_bias(weight_vec)
+      end
+
+      def loss_gradient(x, y, weight)
+        2.0 * (x.dot(weight) - y)
+      end
+
+      def weight_gradient(loss_grad, data, _weight)
+        (loss_grad.expand_dims(1) * data).mean(0)
+      end
+
+      def expand_feature(x)
+        Numo::NArray.hstack([x, Numo::DFloat.ones([x.shape[0], 1])])
+      end
+
+      def split_weight_vec_bias(weight_vec)
+        weights = @params[:fit_bias] ? weight_vec[0...-1] : weight_vec
+        bias = @params[:fit_bias] ? weight_vec[-1] : 0.0
+        [weights, bias]
+      end
+    end
+  end
+end
