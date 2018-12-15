5d3c9e6d2e692a1b7ccd7918d29e92db39092894
yoshoku <yoshoku@outlook.com>
2018-12-16 01:27:52 +0900

    :sparkles: Add utils module

---
+++ lib/svmkit/utils.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+module SVMKit
+  # @!visibility private
+  module Utils
+    module_function
+
+    # @!visibility private
+    def choice_ids(size, probs, rng=nil)
+      rng ||= Random.new
+      Array.new(size) do
+        target = rng.rand
+        chosen = 0
+        probs.each_with_index do |p, idx|
+          break (chosen = idx) if target <= p
+          target -= p
+        end
+        chosen
+      end
+    end
+  end
+end
