6e7ddad505a7eafb8cfc9ae4c7e9355acfac6e8c
yoshoku <yoshoku@outlook.com>
2019-01-20 02:12:19 +0900

    :sparkles: Add pipeline class

---
+++ lib/svmkit/pipeline/pipeline.rb
@@ -0,0 +1,187 @@
+# frozen_string_literal: true
+
+require 'svmkit/validation'
+require 'svmkit/base/base_estimator'
+
+module SVMKit
+  # Module implements utilities of pipeline that cosists of a chain of transfomers and estimators.
+  module Pipeline
+    # Pipeline is a class that implements the function to perform the transformers and estimators sequencially.
+    #
+    # @example
+    #   rbf = SVMKit::KernelApproximation::RBF.new(gamma: 1.0, n_coponents: 128, random_seed: 1)
+    #   svc = SVMKit::LinearModel::SVC.new(reg_param: 1.0, fit_bias: true, max_iter: 5000, random_seed: 1)
+    #   pipeline = SVMKit::Pipeline::Pipeline.new(steps: { trs: rbf, est: svc })
+    #   pipeline.fit(training_samples, traininig_labels)
+    #   results = pipeline.predict(testing_samples)
+    #
+    class Pipeline
+      include Base::BaseEstimator
+      include Validation
+
+      # Return the steps.
+      # @return [Hash]
+      attr_reader :steps
+
+      # Create a new pipeline.
+      #
+      # @param steps [Hash] List of transformers and estimators. The order of transforms follows the insertion order of hash keys.
+      #   The last entry is considered an estimator.
+      def initialize(steps:)
+        check_params_type(Hash, steps: steps)
+        validate_steps(steps)
+        @params = {}
+        @steps = steps
+      end
+
+      # Fit the model with given training data.
+      #
+      # @param x [Numo::DFloat] (shape: [n_samples, n_features]) The training data to be transformed and used for fitting the model.
+      # @param y [Numo::NArray] (shape: [n_samples, n_outputs]) The target values or labels to be used for fitting the model.
+      # @return [Pipeline] The learned pipeline itself.
+      def fit(x, y)
+        trans_x = apply_transforms(x, y, fit: true)
+        last_estimator.fit(trans_x, y) unless last_estimator.nil?
+        self
+      end
+
+      # Call the fit_predict method of last estimator after applying all transforms.
+      #
+      # @param x [Numo::DFloat] (shape: [n_samples, n_features]) The training data to be transformed and used for fitting the model.
+      # @param y [Numo::NArray] (shape: [n_samples, n_outputs], default: nil) The target values or labels to be used for fitting the model.
+      # @return [Numo::NArray] The predicted results by last estimator.
+      def fit_predict(x, y = nil)
+        trans_x = apply_transforms(x, y, fit: true)
+        last_estimator.fit_predict(trans_x)
+      end
+
+      # Call the fit_transform method of last estimator after applying all transforms.
+      #
+      # @param x [Numo::DFloat] (shape: [n_samples, n_features]) The training data to be transformed and used for fitting the model.
+      # @param y [Numo::NArray] (shape: [n_samples, n_outputs], default: nil) The target values or labels to be used for fitting the model.
+      # @return [Numo::NArray] The predicted results by last estimator.
+      def fit_transform(x, y = nil)
+        trans_x = apply_transforms(x, y, fit: true)
+        last_estimator.fit_transform(trans_x, y)
+      end
+
+      # Call the decision_function method of last estimator after applying all transforms.
+      #
+      # @param x [Numo::DFloat] (shape: [n_samples, n_features]) The samples to compute the scores.
+      # @return [Numo::DFloat] (shape: [n_samples]) Confidence score per sample.
+      def decision_function(x)
+        trans_x = apply_transforms(x)
+        last_estimator.decision_function(trans_x)
+      end
+
+      # Call the predict method of last estimator after applying all transforms.
+      #
+      # @param x [Numo::DFloat] (shape: [n_samples, n_features]) The samples to obtain prediction result.
+      # @return [Numo::NArray] The predicted results by last estimator.
+      def predict(x)
+        trans_x = apply_transforms(x)
+        last_estimator.predict(trans_x)
+      end
+
+      # Call the predict_log_proba method of last estimator after applying all transforms.
+      #
+      # @param x [Numo::DFloat] (shape: [n_samples, n_features]) The samples to predict the log-probailities.
+      # @return [Numo::DFloat] (shape: [n_samples, n_classes]) Predicted log-probability of each class per sample.
+      def predict_log_proba(x)
+        trans_x = apply_transforms(x)
+        last_estimator.predict_log_proba(trans_x)
+      end
+
+      # Call the predict_proba method of last estimator after applying all transforms.
+      #
+      # @param x [Numo::DFloat] (shape: [n_samples, n_features]) The samples to predict the probailities.
+      # @return [Numo::DFloat] (shape: [n_samples, n_classes]) Predicted probability of each class per sample.
+      def predict_proba(x)
+        trans_x = apply_transforms(x)
+        last_estimator.predict_proba(trans_x)
+      end
+
+      # Call the transform method of last estimator after applying all transforms.
+      #
+      # @param x [Numo::DFloat] (shape: [n_samples, n_features]) The samples to be transformed.
+      # @return [Numo::DFloat] (shape: [n_samples, n_components]) The transformed samples.
+      def transform(x)
+        trans_x = apply_transforms(x)
+        last_estimator.nil? ? trans_x : last_estimator.transform(trans_x)
+      end
+
+      # Call the inverse_transform method in reverse order.
+      #
+      # @param z [Numo::DFloat] (shape: [n_samples, n_components]) The transformed samples to be restored into original space.
+      # @return [Numo::DFloat] (shape: [n_samples, n_featuress]) The restored samples.
+      def inverse_transform(z)
+        itrans_z = z
+        @steps.keys.reverse.each do |name|
+          transformer = @steps[name]
+          next if transformer.nil?
+          itrans_z = transformer.inverse_transform(itrans_z)
+        end
+        itrans_z
+      end
+
+      # Call the score method of last estimator after applying all transforms.
+      #
+      # @param x [Numo::DFloat] (shape: [n_samples, n_features]) Testing data.
+      # @param y [Numo::NArray] (shape: [n_samples, n_outputs]) True target values or labels for testing data.
+      # @return [Float] The score of last estimator
+      def score(x, y)
+        trans_x = apply_transforms(x)
+        last_estimator.score(trans_x, y)
+      end
+
+      # Dump marshal data.
+      # @return [Hash] The marshal data about Pipeline.
+      def marshal_dump
+        { params: @params,
+          steps: @steps }
+      end
+
+      # Load marshal data.
+      # @return [nil]
+      def marshal_load(obj)
+        @params = obj[:params]
+        @steps = obj[:steps]
+        nil
+      end
+
+      private
+
+      def validate_steps(steps)
+        steps.keys[0...-1].each do |name|
+          transformer = steps[name]
+          next if transformer.nil? || %i[fit transform].all? { |m| transformer.class.method_defined?(m) }
+          raise TypeError,
+                'Class of intermediate step in pipeline should be implemented fit and transform methods: ' \
+                "#{name} => #{transformer.class}"
+        end
+
+        estimator = steps[steps.keys.last]
+        unless estimator.nil? || estimator.class.method_defined?(:fit)
+          raise TypeError,
+                'Class of last step in pipeline should be implemented fit method: ' \
+                "#{steps.keys.last} => #{estimator.class}"
+        end
+      end
+
+      def apply_transforms(x, y = nil, fit: false)
+        trans_x = x
+        @steps.keys[0...-1].each do |name|
+          transformer = @steps[name]
+          next if transformer.nil?
+          transformer.fit(trans_x, y) if fit
+          trans_x = transformer.transform(trans_x)
+        end
+        trans_x
+      end
+
+      def last_estimator
+        @steps[@steps.keys.last]
+      end
+    end
+  end
+end
---
+++ spec/pipeline/pipeline_spec.rb
@@ -0,0 +1,98 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe SVMKit::Pipeline::Pipeline do
+  let(:x) { Marshal.load(File.read(__dir__ + '/../test_samples_xor.dat')) }
+  let(:y) { Marshal.load(File.read(__dir__ + '/../test_labels_xor.dat')) }
+  let(:xc) { Marshal.load(File.read(__dir__ + '/../test_samples_three_clusters.dat')) }
+  let(:yc) { Marshal.load(File.read(__dir__ + '/../test_labels_three_clusters.dat')) }
+  let(:n_rbf_comps) { 64 }
+  let(:n_nmf_comps) { 8 }
+  let(:n_pca_comps) { 4 }
+  let(:n_clusters) { 3 }
+  let(:rbf) { SVMKit::KernelApproximation::RBF.new(gamma: 0.1, n_components: n_rbf_comps, random_seed: 1) }
+  let(:pca) { SVMKit::Decomposition::PCA.new(n_components: n_pca_comps, tol: 1.0e-8, random_seed: 1) }
+  let(:nmf) { SVMKit::Decomposition::NMF.new(n_components: n_nmf_comps, random_seed: 1) }
+  let(:svc) { SVMKit::LinearModel::SVC.new(random_seed: 1) }
+  let(:nrm) { SVMKit::Preprocessing::L2Normalizer.new }
+  let(:nbs) { SVMKit::NaiveBayes::GaussianNB.new }
+  let(:kms) { SVMKit::Clustering::KMeans.new(n_clusters: n_clusters, random_seed: 1) }
+
+  it 'classifies xor data with Kernel approximation, PCA, and SVC.' do
+    n_samples, n_features = x.shape
+
+    pipe = described_class.new(steps: { rbf: rbf, pca: pca, skip: nil, nrm: nrm, svc: svc })
+    pipe.fit(x, y)
+
+    expect(pipe.steps[:rbf].random_mat.shape).to match([n_features, n_rbf_comps])
+    expect(pipe.steps[:pca].components.shape).to match([n_pca_comps, n_rbf_comps])
+    expect(pipe.steps[:svc].weight_vec.shape).to match([n_pca_comps])
+    expect(pipe.predict(x).shape).to match([n_samples])
+    expect(pipe.decision_function(x).shape).to match([n_samples])
+    expect(pipe.score(x, y)).to eq(1.0)
+  end
+
+  it 'analyzes clusters with Kernel approximation and K-Means.' do
+    n_samples, = xc.shape
+
+    pipe = described_class.new(steps: { rbf: rbf, kms: kms })
+    predicted = pipe.fit_predict(xc)
+
+    expect(predicted.shape).to match([n_samples])
+    expect(pipe.steps[:kms].cluster_centers.shape).to match([n_clusters, n_rbf_comps])
+    expect(pipe.score(xc, yc)).to eq(1.0)
+  end
+
+  it 'transforms high-dimensional data with NMF and PCA.' do
+    n_samples, n_features = x.shape
+    n_high_features = 16
+    projected_x = x.abs.dot(Numo::DFloat.new(n_features, n_high_features).rand)
+
+    pipe = described_class.new(steps: { nmf: nmf, pca: pca })
+    trans_x = pipe.fit_transform(projected_x, y)
+    expect(trans_x.shape).to match([n_samples, n_pca_comps])
+    expect(pipe.steps[:nmf].components.shape).to match([n_nmf_comps, n_high_features])
+    expect(pipe.steps[:pca].components.shape).to match([n_pca_comps, n_nmf_comps])
+
+    trans_x = pipe.transform(projected_x)
+    expect(trans_x.shape).to match([n_samples, n_pca_comps])
+
+    reconst_x = pipe.inverse_transform(trans_x)
+    expect(reconst_x.shape).to match([n_samples, n_high_features])
+  end
+
+  it 'estimates class probabilities with Kernel approximation, PCA, and Naive bayes.'  do
+    n_samples, n_features = x.shape
+    classes = y.to_a.uniq.sort
+
+    pipe = described_class.new(steps: { rbf: rbf, pca: pca, nbs: nbs })
+    pipe.fit(x, y)
+
+    probs = pipe.predict_proba(x)
+    predicted = Numo::Int32[*(Array.new(n_samples) { |n| classes[probs[n, true].max_index] })]
+    expect(probs.shape).to match([n_samples, 2])
+    expect(predicted).to eq(y)
+
+    log_probs = pipe.predict_log_proba(x)
+    predicted = Numo::Int32[*(Array.new(n_samples) { |n| classes[log_probs[n, true].max_index] })]
+    expect(log_probs.shape).to match([n_samples, 2])
+    expect(predicted).to eq(y)
+  end
+
+  it 'dumps and restores itself using Marshal module.' do
+    pipe = described_class.new(steps: { rbf: rbf, pca: pca, svc: svc })
+    pipe.fit(x, y)
+    copied = Marshal.load(Marshal.dump(pipe))
+    expect(copied.class).to eq(pipe.class)
+    expect(copied.steps).to include(*pipe.steps.keys)
+    expect(copied.steps[:rbf].random_mat).to eq(pipe.steps[:rbf].random_mat)
+    expect(copied.steps[:pca].components).to eq(pipe.steps[:pca].components)
+    expect(copied.steps[:svc].weight_vec).to eq(pipe.steps[:svc].weight_vec)
+  end
+
+  it 'raises TypeError when given steps including a non-transformer and non-estimator.' do
+    expect { described_class.new(steps: { rbf: rbf, bad: 'skip',  svc: svc }) }.to raise_error(TypeError)
+    expect { described_class.new(steps: { rbf: rbf, bad: 'skip' }) }.to raise_error(TypeError)
+  end
+end
