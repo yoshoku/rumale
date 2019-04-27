e8b46f32ef3e6eb516617779d3cb4a278ce35615
yoshoku <yoshoku@outlook.com>
2019-04-27 15:44:32 +0900

    :sparkles: Add estimator classes with extremely randomized tree

---
+++ lib/rumale/tree/extra_tree_classifier.rb
@@ -0,0 +1,119 @@
+# frozen_string_literal: true
+
+require 'rumale/tree/decision_tree_classifier'
+
+module Rumale
+  module Tree
+    # ExtraTreeClassifier is a class that implements extra randomized tree for classification.
+    #
+    # @example
+    #   estimator =
+    #     Rumale::Tree::ExtraTreeClassifier.new(
+    #       criterion: 'gini', max_depth: 3, max_leaf_nodes: 10, min_samples_leaf: 5, random_seed: 1)
+    #   estimator.fit(training_samples, traininig_labels)
+    #   results = estimator.predict(testing_samples)
+    #
+    # *Reference*
+    # - P. Geurts, D. Ernst, and L. Wehenkel, "Extremely randomized trees," Machine Learning, vol. 63 (1), pp. 3--42, 2006.
+    class ExtraTreeClassifier < DecisionTreeClassifier
+      # Return the class labels.
+      # @return [Numo::Int32] (size: n_classes)
+      attr_reader :classes
+
+      # Return the importance for each feature.
+      # @return [Numo::DFloat] (size: n_features)
+      attr_reader :feature_importances
+
+      # Return the learned tree.
+      # @return [Node]
+      attr_reader :tree
+
+      # Return the random generator for random selection of feature index.
+      # @return [Random]
+      attr_reader :rng
+
+      # Return the labels assigned each leaf.
+      # @return [Numo::Int32] (size: n_leafs)
+      attr_reader :leaf_labels
+
+      # Create a new classifier with extra randomized tree algorithm.
+      #
+      # @param criterion [String] The function to evaluate spliting point. Supported criteria are 'gini' and 'entropy'.
+      # @param max_depth [Integer] The maximum depth of the tree.
+      #   If nil is given, extra tree grows without concern for depth.
+      # @param max_leaf_nodes [Integer] The maximum number of leaves on extra tree.
+      #   If nil is given, number of leaves is not limited.
+      # @param min_samples_leaf [Integer] The minimum number of samples at a leaf node.
+      # @param max_features [Integer] The number of features to consider when searching optimal split point.
+      #   If nil is given, split process considers all features.
+      # @param random_seed [Integer] The seed value using to initialize the random generator.
+      #   It is used to randomly determine the order of features when deciding spliting point.
+      def initialize(criterion: 'gini', max_depth: nil, max_leaf_nodes: nil, min_samples_leaf: 1, max_features: nil,
+                     random_seed: nil)
+        check_params_type_or_nil(Integer, max_depth: max_depth, max_leaf_nodes: max_leaf_nodes,
+                                          max_features: max_features, random_seed: random_seed)
+        check_params_integer(min_samples_leaf: min_samples_leaf)
+        check_params_string(criterion: criterion)
+        check_params_positive(max_depth: max_depth, max_leaf_nodes: max_leaf_nodes,
+                              min_samples_leaf: min_samples_leaf, max_features: max_features)
+        super
+      end
+
+      # Fit the model with given training data.
+      #
+      # @param x [Numo::DFloat] (shape: [n_samples, n_features]) The training data to be used for fitting the model.
+      # @param y [Numo::Int32] (shape: [n_samples]) The labels to be used for fitting the model.
+      # @return [ExtraTreeClassifier] The learned classifier itself.
+      def fit(x, y)
+        check_sample_array(x)
+        check_label_array(y)
+        check_sample_label_size(x, y)
+        super
+      end
+
+      # Predict class labels for samples.
+      #
+      # @param x [Numo::DFloat] (shape: [n_samples, n_features]) The samples to predict the labels.
+      # @return [Numo::Int32] (shape: [n_samples]) Predicted class label per sample.
+      def predict(x)
+        check_sample_array(x)
+        super
+      end
+
+      # Predict probability for samples.
+      #
+      # @param x [Numo::DFloat] (shape: [n_samples, n_features]) The samples to predict the probailities.
+      # @return [Numo::DFloat] (shape: [n_samples, n_classes]) Predicted probability of each class per sample.
+      def predict_proba(x)
+        check_sample_array(x)
+        super
+      end
+
+      # Dump marshal data.
+      # @return [Hash] The marshal data about ExtraTreeClassifier
+      def marshal_dump
+        super
+      end
+
+      # Load marshal data.
+      # @return [nil]
+      def marshal_load(obj)
+        super
+      end
+
+      private
+
+      def best_split(features, y, whole_impurity)
+        threshold = @rng.rand(features.min..features.max)
+        l_ids = features.le(threshold).where
+        r_ids = features.gt(threshold).where
+        l_impurity = l_ids.size > 0 ? impurity(y[l_ids, true]) : 0.0
+        r_impurity = r_ids.size > 0 ? impurity(y[r_ids, true]) : 0.0
+        gain = whole_impurity -
+               l_impurity * l_ids.size.fdiv(y.shape[0]) -
+               r_impurity * r_ids.size.fdiv(y.shape[0])
+        [l_impurity, r_impurity, threshold, gain]
+      end
+    end
+  end
+end
---
+++ spec/tree/extra_tree_classifier_spec.rb
@@ -0,0 +1,125 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Rumale::Tree::ExtraTreeClassifier do
+  let(:x_bin) { Marshal.load(File.read(__dir__ + '/../test_samples.dat')) }
+  let(:y_bin) { Marshal.load(File.read(__dir__ + '/../test_labels.dat')) }
+  let(:x_mlt) { Marshal.load(File.read(__dir__ + '/../test_samples_three_clusters.dat')) }
+  let(:y_mlt) { Marshal.load(File.read(__dir__ + '/../test_labels_three_clusters.dat')) }
+  let(:estimator_entropy) { described_class.new(criterion: 'entropy', random_seed: 1) }
+  let(:max_depth) { nil }
+  let(:max_leaf_nodes) { nil }
+  let(:min_samples_leaf) { 1 }
+  let(:max_features) { nil }
+  let(:estimator) do
+    described_class.new(max_depth: max_depth, max_leaf_nodes: max_leaf_nodes,
+                        min_samples_leaf: min_samples_leaf, max_features: max_features, random_seed: 1)
+  end
+
+  it 'classifies two clusters data.' do
+    _n_samples, n_features = x_bin.shape
+    estimator.fit(x_bin, y_bin)
+    expect(estimator.tree.class).to eq(Rumale::Tree::Node)
+    expect(estimator.classes.class).to eq(Numo::Int32)
+    expect(estimator.classes.size).to eq(2)
+    expect(estimator.feature_importances.class).to eq(Numo::DFloat)
+    expect(estimator.feature_importances.shape[0]).to eq(n_features)
+    expect(estimator.feature_importances.shape[1]).to be_nil
+    expect(estimator.score(x_bin, y_bin)).to eq(1.0)
+  end
+
+  it 'classifies three clusters data.' do
+    _n_samples, n_features = x_mlt.shape
+    estimator.fit(x_mlt, y_mlt)
+    expect(estimator.tree.class).to eq(Rumale::Tree::Node)
+    expect(estimator.classes.class).to eq(Numo::Int32)
+    expect(estimator.classes.size).to eq(3)
+    expect(estimator.feature_importances.class).to eq(Numo::DFloat)
+    expect(estimator.feature_importances.shape[0]).to eq(n_features)
+    expect(estimator.feature_importances.shape[1]).to be_nil
+    expect(estimator.score(x_mlt, y_mlt)).to eq(1.0)
+  end
+
+  it 'estimates class probabilities with three clusters dataset.' do
+    n_samples, _n_features = x_mlt.shape
+    estimator_entropy.fit(x_mlt, y_mlt)
+    probs = estimator_entropy.predict_proba(x_mlt)
+    expect(probs.class).to eq(Numo::DFloat)
+    expect(probs.shape[0]).to eq(n_samples)
+    expect(probs.shape[1]).to eq(3)
+    classes = y_mlt.to_a.uniq.sort
+    predicted = Numo::Int32.asarray(Array.new(n_samples) { |n| classes[probs[n, true].max_index] })
+    expect(predicted).to eq(y_mlt)
+  end
+
+  it 'dumps and restores itself using Marshal module.' do
+    estimator.fit(x_mlt, y_mlt)
+    copied = Marshal.load(Marshal.dump(estimator))
+    expect(estimator.class).to eq(copied.class)
+    expect(estimator.classes).to eq(copied.classes)
+    expect(estimator.feature_importances).to eq(copied.feature_importances)
+    expect(estimator.rng).to eq(copied.rng)
+    # FIXME: A slight error on the value of the threhold parameter occurs.
+    #        It seems to be caused by rounding error of Float.
+    # expect(estimator.tree).to eq(copied.tree)
+    expect(copied.score(x_mlt, y_mlt)).to eq(1.0)
+  end
+
+  context 'when max_depth parameter is given' do
+    let(:max_depth) { 1 }
+    it 'learns model with given parameters.' do
+      estimator.fit(x_mlt, y_mlt)
+      expect(estimator.params[:max_depth]).to eq(max_depth)
+      expect(estimator.tree.left.left).to be_nil
+      expect(estimator.tree.left.right).to be_nil
+      expect(estimator.tree.right.left).to be_nil
+      expect(estimator.tree.right.right).to be_nil
+    end
+  end
+
+  context 'when max_leaf_nodes parameter is given' do
+    let(:max_leaf_nodes) { 2 }
+    it 'learns model with given parameters.' do
+      estimator.fit(x_mlt, y_mlt)
+      expect(estimator.params[:max_leaf_nodes]).to eq(max_leaf_nodes)
+      expect(estimator.leaf_labels.size).to eq(max_leaf_nodes)
+    end
+  end
+
+  context 'when min_samples_leaf parameter is given' do
+    let(:min_samples_leaf) { 290 }
+    it 'learns model with given parameters.' do
+      estimator.fit(x_mlt, y_mlt)
+      expect(estimator.params[:min_samples_leaf]).to eq(min_samples_leaf)
+      expect(estimator.tree.left.leaf).to be_truthy
+      expect(estimator.tree.left.n_samples).to be >= min_samples_leaf
+      expect(estimator.tree.right).to be_nil
+    end
+  end
+
+  context 'when max_features parameter is given' do
+    context 'negative value' do
+      let(:max_features) { -10 }
+      it 'raises ArgumentError by validation' do
+        expect { estimator }.to raise_error(ArgumentError)
+      end
+    end
+
+    context 'value larger than number of features' do
+      let(:max_features) { 10 }
+      it 'value of max_features is equal to the number of features' do
+        estimator.fit(x_mlt, y_mlt)
+        expect(estimator.params[:max_features]).to eq(x_mlt.shape[1])
+      end
+    end
+
+    context 'valid value' do
+      let(:max_features) { 2 }
+      it 'learns model with given parameters.' do
+        estimator.fit(x_mlt, y_mlt)
+        expect(estimator.params[:max_features]).to eq(2)
+      end
+    end
+  end
+end
