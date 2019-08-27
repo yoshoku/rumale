81326dc49a4da655f217a1b63803cc8acb0e0820
yoshoku <yoshoku@outlook.com>
2019-08-27 23:19:58 +0900

    :art: Fix spec for principal componen analysis

--- spec/decomposition/pca_spec.rb
+++ spec/decomposition/pca_spec.rb
@@ -8,72 +8,61 @@ RSpec.describe Rumale::Decomposition::PCA do
   let(:solver) { 'fpt' }
   let(:decomposer) { described_class.new(n_components: n_components, solver: solver, tol: 1.0e-8, random_seed: 1) }
   let(:transformer) { Rumale::KernelApproximation::RBF.new(gamma: 1.0, n_components: 32, random_seed: 1) }
+  let(:samples) { transformer.fit_transform(x) }
+  let(:sub_samples) { decomposer.fit_transform(samples) }
+  let(:rec_samples) { decomposer.inverse_transform(sub_samples) }
+  let(:mse) { Numo::NMath.sqrt(((samples - rec_samples)**2).sum(1)).mean }
+  let(:n_samples) { samples.shape[0] }
+  let(:n_features) { samples.shape[1] }
 
-  context 'when solver is fix point algorithm' do
+  shared_examples 'projection into subspace' do
     it 'projects high-dimensinal data into subspace.' do
-      samples = transformer.fit_transform(x)
-      sub_samples = decomposer.fit_transform(samples)
-      n_samples, n_features = samples.shape
       expect(sub_samples.class).to eq(Numo::DFloat)
       expect(sub_samples.shape[0]).to eq(n_samples)
       expect(sub_samples.shape[1]).to eq(n_components)
+      expect(rec_samples.shape[0]).to eq(n_samples)
+      expect(rec_samples.shape[1]).to eq(n_features)
       expect(decomposer.components.class).to eq(Numo::DFloat)
       expect(decomposer.components.shape[0]).to eq(n_components)
       expect(decomposer.components.shape[1]).to eq(n_features)
       expect(decomposer.mean.class).to eq(Numo::DFloat)
       expect(decomposer.mean.shape[0]).to eq(n_features)
       expect(decomposer.mean.shape[1]).to be_nil
-      rec_samples = decomposer.inverse_transform(sub_samples)
-      mse = Numo::NMath.sqrt(((samples - rec_samples)**2).sum(1)).mean
       expect(mse).to be <= 0.1
     end
+  end
+
+  shared_examples 'projection into one-dimensional subspace' do
+    let(:n_components) { 1 }
+    let(:samples) { x }
+    let(:sub_samples) { decomposer.fit_transform(samples).expand_dims(1) }
 
     it 'projects data into one-dimensional subspace.' do
-      liner = described_class.new(n_components: 1, random_seed: 1)
-      sub_x = liner.fit_transform(x)
-      rec_x = liner.inverse_transform(sub_x.expand_dims(1))
-      expect(rec_x.shape[0]).to eq(x.shape[0])
-      expect(rec_x.shape[1]).to eq(x.shape[1])
-      mse = Numo::NMath.sqrt(((x - rec_x)**2).sum(1)).mean
+      expect(sub_samples.shape[0]).to eq(n_samples)
+      expect(sub_samples.shape[1]).to eq(n_components)
+      expect(rec_samples.shape[0]).to eq(n_samples)
+      expect(rec_samples.shape[1]).to eq(n_features)
+      expect(decomposer.components.shape[0]).to eq(n_features)
+      expect(decomposer.components.shape[1]).to be_nil
+      expect(decomposer.mean.shape[0]).to eq(n_features)
+      expect(decomposer.mean.shape[1]).to be_nil
       expect(mse).to be <= 0.5
     end
+  end
+
+  context 'when solver is fix point algorithm' do
+    it_behaves_like 'projection into subspace'
+    it_behaves_like 'projection into one-dimensional subspace'
   end
 
   context 'when solver is eigen value decomposition' do
     let(:solver) { 'evd' }
 
-    it 'projects high-dimensinal data into subspace.' do
-      samples = transformer.fit_transform(x)
-      sub_samples = decomposer.fit_transform(samples)
-      n_samples, n_features = samples.shape
-      expect(sub_samples.class).to eq(Numo::DFloat)
-      expect(sub_samples.shape[0]).to eq(n_samples)
-      expect(sub_samples.shape[1]).to eq(n_components)
-      expect(decomposer.components.class).to eq(Numo::DFloat)
-      expect(decomposer.components.shape[0]).to eq(n_components)
-      expect(decomposer.components.shape[1]).to eq(n_features)
-      expect(decomposer.mean.class).to eq(Numo::DFloat)
-      expect(decomposer.mean.shape[0]).to eq(n_features)
-      expect(decomposer.mean.shape[1]).to be_nil
-      rec_samples = decomposer.inverse_transform(sub_samples)
-      mse = Numo::NMath.sqrt(((samples - rec_samples)**2).sum(1)).mean
-      expect(mse).to be <= 0.1
-    end
-
-    it 'projects data into one-dimensional subspace.' do
-      liner = described_class.new(n_components: 1, random_seed: 1)
-      sub_x = liner.fit_transform(x)
-      rec_x = liner.inverse_transform(sub_x.expand_dims(1))
-      expect(rec_x.shape[0]).to eq(x.shape[0])
-      expect(rec_x.shape[1]).to eq(x.shape[1])
-      mse = Numo::NMath.sqrt(((x - rec_x)**2).sum(1)).mean
-      expect(mse).to be <= 0.5
-    end
+    it_behaves_like 'projection into subspace'
+    it_behaves_like 'projection into one-dimensional subspace'
   end
 
   it 'dumps and restores itself using Marshal module.' do
-    samples = transformer.fit_transform(x)
-    decomposer.fit(samples)
     copied = Marshal.load(Marshal.dump(decomposer))
     expect(decomposer.class).to eq(copied.class)
     expect(decomposer.params[:n_components]).to eq(copied.params[:n_components])
