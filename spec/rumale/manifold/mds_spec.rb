76d848acbe611ce90ea028a0f0d8670cfb403203
yoshoku <yoshoku@outlook.com>
2019-11-19 22:53:18 +0900

    :art: Refactor manifold transformer specs

--- spec/rumale/manifold/mds_spec.rb
+++ spec/rumale/manifold/mds_spec.rb
@@ -3,82 +3,104 @@
 require 'spec_helper'
 
 RSpec.describe Rumale::Manifold::MDS do
-  let(:x) { two_clusters_dataset[0] }
-  let(:n_samples) { x.shape[0] }
-  let(:n_features) { 32 }
+  let(:samples) do
+    Rumale::KernelApproximation::RBF.new(n_components: 32, random_seed: 1).fit_transform(two_clusters_dataset[0])
+  end
+  let(:n_samples) { samples.shape[0] }
+  let(:n_features) { samples.shape[1] }
   let(:n_components) { 2 }
-  let(:samples) { Rumale::KernelApproximation::RBF.new(gamma: 1.0, n_components: n_features, random_seed: 1).fit_transform(x) }
-  let(:distance_matrix) { Rumale::PairwiseMetric.euclidean_distance(samples) }
-  let(:init_stress) { described_class.new(n_components: n_components, max_iter: 0, random_seed: 1).fit(samples).stress }
-  let(:mds) { described_class.new(n_components: n_components, max_iter: 50, random_seed: 1) }
-  let(:mds_metric) { described_class.new(n_components: n_components, metric: 'precomputed', max_iter: 50, random_seed: 1) }
-  let(:mds_tol) { described_class.new(n_components: n_components, max_iter: 50, init: 'pca', tol: 100.0, random_seed: 1) }
-  let(:mds_params) do
-    described_class.new(n_components: n_components, max_iter: 100, init: 'pca', verbose: true, tol: nil, random_seed: 1)
+  let(:metric) { 'euclidean' }
+  let(:tol) { nil }
+  let(:verbose) { false }
+  let(:max_iter) { 50 }
+  let(:mds) do
+    described_class.new(n_components: n_components, metric: metric, max_iter: max_iter, tol: tol, init: 'pca',
+                        verbose: verbose, random_seed: 1)
+  end
+  let(:init_stress) do
+    described_class.new(n_components: n_components, metric: metric, max_iter: 0, tol: tol, init: 'pca',
+                        verbose: verbose, random_seed: 1).fit(x).stress
+  end
+  let(:low_samples) { mds.fit_transform(x) }
+  let(:copied) { Marshal.load(Marshal.dump(mds)) }
+
+  context 'when metric is "euclidean"' do
+    let(:metric) { 'euclidean' }
+    let(:x) { samples }
+
+    it 'maps high-dimensional data into low-dimensional data.', :aggregate_failures do
+      expect(low_samples.class).to eq(Numo::DFloat)
+      expect(low_samples.shape[0]).to eq(n_samples)
+      expect(low_samples.shape[1]).to eq(n_components)
+      expect(mds.embedding.class).to eq(Numo::DFloat)
+      expect(mds.embedding.shape[0]).to eq(n_samples)
+      expect(mds.embedding.shape[1]).to eq(n_components)
+      expect(mds.n_iter).to eq(max_iter)
+      expect(mds.stress.class).to eq(Float)
+      expect(mds.stress).not_to be_nil
+      expect(mds.stress).to be < init_stress
+    end
+
+    it 'dumps and restores itself using Marshal module.', :aggregate_failures do
+      expect(mds.class).to eq(copied.class)
+      expect(mds.params[:n_components]).to match(copied.params[:n_components])
+      expect(mds.params[:max_iter]).to match(copied.params[:max_iter])
+      expect(mds.params[:metric]).to match(copied.params[:metric])
+      expect(mds.params[:init]).to match(copied.params[:init])
+      expect(mds.params[:verbose]).to match(copied.params[:verbose])
+      expect(mds.params[:random_seed]).to match(copied.params[:random_seed])
+      expect(mds.embedding).to eq(copied.embedding)
+      expect(mds.stress).to eq(copied.stress)
+      expect(mds.n_iter).to eq(copied.n_iter)
+      expect(mds.rng).to eq(copied.rng)
+    end
+
+    context 'when tol parameter is given' do
+      let(:tol) { 1000 }
+
+      it 'terminates optimization based on the tol parameter.', :aggregate_failures do
+        expect(low_samples.class).to eq(Numo::DFloat)
+        expect(low_samples.shape[0]).to eq(n_samples)
+        expect(low_samples.shape[1]).to eq(n_components)
+        expect(mds.embedding.class).to eq(Numo::DFloat)
+        expect(mds.embedding.shape[0]).to eq(n_samples)
+        expect(mds.embedding.shape[1]).to eq(n_components)
+        expect(mds.n_iter).to be < max_iter
+        expect(mds.stress.class).to eq(Float)
+        expect(mds.stress).not_to be_nil
+        expect(mds.stress).to be < init_stress
+      end
+    end
+
+    context 'when verbose is "true"' do
+      let(:verbose) { true }
+      let(:max_iter) { 100 }
+
+      it 'outputs debug messages.', :aggregate_failures do
+        expect { mds.fit(x) }.to output(/MDS/).to_stdout
+      end
+    end
   end
 
-  it 'maps high-dimensional data into low-dimensional data.' do
-    low_samples = mds.fit_transform(samples)
-    expect(low_samples.class).to eq(Numo::DFloat)
-    expect(low_samples.shape[0]).to eq(n_samples)
-    expect(low_samples.shape[1]).to eq(n_components)
-    expect(mds.embedding.class).to eq(Numo::DFloat)
-    expect(mds.embedding.shape[0]).to eq(n_samples)
-    expect(mds.embedding.shape[1]).to eq(n_components)
-    expect(mds.n_iter).to eq(50)
-    expect(mds.stress.class).to eq(Float)
-    expect(mds.stress).not_to be_nil
-    expect(mds.stress).to be < init_stress
-  end
+  context 'when metric is "precomputed"' do
+    let(:metric) { 'precomputed' }
+    let(:x) { Rumale::PairwiseMetric.euclidean_distance(samples) }
 
-  it 'maps high-dimensional data represented by distance matrix.' do
-    low_samples = mds_metric.fit_transform(distance_matrix)
-    expect(low_samples.class).to eq(Numo::DFloat)
-    expect(low_samples.shape[0]).to eq(n_samples)
-    expect(low_samples.shape[1]).to eq(n_components)
-    expect(mds_metric.embedding.class).to eq(Numo::DFloat)
-    expect(mds_metric.embedding.shape[0]).to eq(n_samples)
-    expect(mds_metric.embedding.shape[1]).to eq(n_components)
-    expect(mds_metric.n_iter).to eq(50)
-    expect(mds_metric.stress.class).to eq(Float)
-    expect(mds_metric.stress).not_to be_nil
-    expect(mds_metric.stress).to be < init_stress
-  end
+    it 'maps high-dimensional data represented by distance matrix.', :aggregate_failures do
+      expect(low_samples.class).to eq(Numo::DFloat)
+      expect(low_samples.shape[0]).to eq(n_samples)
+      expect(low_samples.shape[1]).to eq(n_components)
+      expect(mds.embedding.class).to eq(Numo::DFloat)
+      expect(mds.embedding.shape[0]).to eq(n_samples)
+      expect(mds.embedding.shape[1]).to eq(n_components)
+      expect(mds.n_iter).to eq(50)
+      expect(mds.stress.class).to eq(Float)
+      expect(mds.stress).not_to be_nil
+      expect(mds.stress).to be < init_stress
+    end
 
-  it 'terminates optimization based on the tol parameter.' do
-    low_samples = mds_tol.fit_transform(samples)
-    expect(low_samples.class).to eq(Numo::DFloat)
-    expect(low_samples.shape[0]).to eq(n_samples)
-    expect(low_samples.shape[1]).to eq(n_components)
-    expect(mds_tol.embedding.class).to eq(Numo::DFloat)
-    expect(mds_tol.embedding.shape[0]).to eq(n_samples)
-    expect(mds_tol.embedding.shape[1]).to eq(n_components)
-    expect(mds_tol.n_iter).to be < 50
-    expect(mds_tol.stress.class).to eq(Float)
-    expect(mds_tol.stress).not_to be_nil
-    expect(mds_tol.stress).to be < init_stress
-  end
-
-  # it 'raises ArgumentError when given a non-square matrix.' do
-  #   expect { mds_metric.fit(Numo::DFloat.new(5, 3).rand) }.to raise_error(ArgumentError)
-  # end
-
-  it 'outputs debug messages.' do
-    expect { mds_params.fit(samples) }.to output(/MDS/).to_stdout
-  end
-
-  it 'dumps and restores itself using Marshal module.' do
-    copied = Marshal.load(Marshal.dump(mds_params))
-    expect(mds_params.class).to eq(copied.class)
-    expect(mds_params.params[:n_components]).to match(copied.params[:n_components])
-    expect(mds_params.params[:max_iter]).to match(copied.params[:max_iter])
-    expect(mds_params.params[:metric]).to match(copied.params[:metric])
-    expect(mds_params.params[:init]).to match(copied.params[:init])
-    expect(mds_params.params[:verbose]).to match(copied.params[:verbose])
-    expect(mds_params.params[:random_seed]).to match(copied.params[:random_seed])
-    expect(mds_params.embedding).to eq(copied.embedding)
-    expect(mds_params.stress).to eq(copied.stress)
-    expect(mds_params.n_iter).to eq(copied.n_iter)
-    expect(mds_params.rng).to eq(copied.rng)
+    it 'raises ArgumentError when given a non-square matrix.', :aggregate_failures do
+      expect { mds.fit(Numo::DFloat.new(5, 3).rand) }.to raise_error(ArgumentError)
+    end
   end
 end
--- spec/rumale/manifold/tsne_spec.rb
+++ spec/rumale/manifold/tsne_spec.rb
@@ -3,84 +3,106 @@
 require 'spec_helper'
 
 RSpec.describe Rumale::Manifold::TSNE do
-  let(:x) { two_clusters_dataset[0] }
-  let(:n_samples) { x.shape[0] }
-  let(:n_features) { 32 }
+  let(:samples) do
+    Rumale::KernelApproximation::RBF.new(n_components: 32, random_seed: 1).fit_transform(two_clusters_dataset[0])
+  end
+  let(:n_samples) { samples.shape[0] }
+  let(:n_features) { samples.shape[1] }
   let(:n_components) { 2 }
-  let(:samples) { Rumale::KernelApproximation::RBF.new(gamma: 1.0, n_components: n_features, random_seed: 1).fit_transform(x) }
-  let(:distance_matrix) { Rumale::PairwiseMetric.euclidean_distance(samples) }
-  let(:init_kl) { described_class.new(n_components: n_components, max_iter: 0, random_seed: 1).fit(samples).kl_divergence }
-  let(:tsne) { described_class.new(n_components: n_components, max_iter: 50, random_seed: 1) }
-  let(:tsne_metric) { described_class.new(n_components: n_components, metric: 'precomputed', max_iter: 50, random_seed: 1) }
-  let(:tsne_tol) { described_class.new(n_components: n_components, max_iter: 50, tol: 1.0, random_seed: 1) }
-  let(:tsne_params) do
-    described_class.new(n_components: n_components, perplexity: 200.0, max_iter: 200,
-                        init: 'pca', verbose: true, random_seed: 1)
+  let(:metric) { 'euclidean' }
+  let(:perplexity) { 30 }
+  let(:tol) { nil }
+  let(:verbose) { false }
+  let(:max_iter) { 50 }
+  let(:tsne) do
+    described_class.new(n_components: n_components, metric: metric, max_iter: max_iter, tol: tol, init: 'pca',
+                        perplexity: perplexity, verbose: verbose, random_seed: 1)
+  end
+  let(:init_kl) do
+    described_class.new(n_components: n_components, metric: metric, max_iter: 0, tol: tol, init: 'pca',
+                        perplexity: perplexity, verbose: verbose, random_seed: 1).fit(x).kl_divergence
+  end
+  let(:low_samples) { tsne.fit_transform(x) }
+  let(:copied) { Marshal.load(Marshal.dump(tsne)) }
+
+  context 'when metric is "euclidean"' do
+    let(:metric) { 'euclidean' }
+    let(:x) { samples }
+
+    it 'maps high-dimensional data into low-dimensional data.', :aggregate_failures do
+      expect(low_samples.class).to eq(Numo::DFloat)
+      expect(low_samples.shape[0]).to eq(n_samples)
+      expect(low_samples.shape[1]).to eq(n_components)
+      expect(tsne.embedding.class).to eq(Numo::DFloat)
+      expect(tsne.embedding.shape[0]).to eq(n_samples)
+      expect(tsne.embedding.shape[1]).to eq(n_components)
+      expect(tsne.n_iter).to eq(max_iter)
+      expect(tsne.kl_divergence.class).to eq(Float)
+      expect(tsne.kl_divergence).not_to be_nil
+      expect(tsne.kl_divergence).to be < init_kl
+    end
+
+    it 'dumps and restores itself using Marshal module.', :aggregate_failures do
+      expect(tsne.params[:n_components]).to match(copied.params[:n_components])
+      expect(tsne.params[:perplexity]).to match(copied.params[:perplexity])
+      expect(tsne.params[:max_iter]).to match(copied.params[:max_iter])
+      expect(tsne.params[:metric]).to match(copied.params[:metric])
+      expect(tsne.params[:init]).to match(copied.params[:init])
+      expect(tsne.params[:verbose]).to match(copied.params[:verbose])
+      expect(tsne.params[:random_seed]).to match(copied.params[:random_seed])
+      expect(tsne.embedding).to eq(copied.embedding)
+      expect(tsne.kl_divergence).to eq(copied.kl_divergence)
+      expect(tsne.n_iter).to eq(copied.n_iter)
+      expect(tsne.rng).to eq(copied.rng)
+    end
+
+    context 'when tol parameter is given' do
+      let(:tol) { 1 }
+
+      it 'terminates optimization based on the tol parameter.', :aggregate_failures do
+        expect(low_samples.class).to eq(Numo::DFloat)
+        expect(low_samples.shape[0]).to eq(n_samples)
+        expect(low_samples.shape[1]).to eq(n_components)
+        expect(tsne.embedding.class).to eq(Numo::DFloat)
+        expect(tsne.embedding.shape[0]).to eq(n_samples)
+        expect(tsne.embedding.shape[1]).to eq(n_components)
+        expect(tsne.n_iter).to be < max_iter
+        expect(tsne.kl_divergence.class).to eq(Float)
+        expect(tsne.kl_divergence).not_to be_nil
+        expect(tsne.kl_divergence).to be < init_kl
+      end
+    end
+
+    context 'when verbose is "true"' do
+      let(:verbose) { true }
+      let(:perplexity) { 200 }
+      let(:max_iter) { 100 }
+
+      it 'outputs debug messages.', :aggregate_failures do
+        expect { tsne.fit(x) }.to output(/t-SNE/).to_stdout
+      end
+    end
   end
 
-  it 'maps high-dimensional data into low-dimensional data.' do
-    low_samples = tsne.fit_transform(samples)
-    expect(low_samples.class).to eq(Numo::DFloat)
-    expect(low_samples.shape[0]).to eq(n_samples)
-    expect(low_samples.shape[1]).to eq(n_components)
-    expect(tsne.embedding.class).to eq(Numo::DFloat)
-    expect(tsne.embedding.shape[0]).to eq(n_samples)
-    expect(tsne.embedding.shape[1]).to eq(n_components)
-    expect(tsne.n_iter).to eq(50)
-    expect(tsne.kl_divergence.class).to eq(Float)
-    expect(tsne.kl_divergence).not_to be_nil
-    expect(tsne.kl_divergence).to be < init_kl
-  end
+  context 'when metric is "precomputed"' do
+    let(:metric) { 'precomputed' }
+    let(:x) { Rumale::PairwiseMetric.euclidean_distance(samples) }
 
-  it 'maps high-dimensional data represented by distance matrix.' do
-    low_samples = tsne_metric.fit_transform(distance_matrix)
-    expect(low_samples.class).to eq(Numo::DFloat)
-    expect(low_samples.shape[0]).to eq(n_samples)
-    expect(low_samples.shape[1]).to eq(n_components)
-    expect(tsne_metric.embedding.class).to eq(Numo::DFloat)
-    expect(tsne_metric.embedding.shape[0]).to eq(n_samples)
-    expect(tsne_metric.embedding.shape[1]).to eq(n_components)
-    expect(tsne_metric.n_iter).to eq(50)
-    expect(tsne_metric.kl_divergence.class).to eq(Float)
-    expect(tsne_metric.kl_divergence).not_to be_nil
-    expect(tsne_metric.kl_divergence).to be < init_kl
-  end
+    it 'maps high-dimensional data represented by distance matrix.', :aggregate_failures do
+      expect(low_samples.class).to eq(Numo::DFloat)
+      expect(low_samples.shape[0]).to eq(n_samples)
+      expect(low_samples.shape[1]).to eq(n_components)
+      expect(tsne.embedding.class).to eq(Numo::DFloat)
+      expect(tsne.embedding.shape[0]).to eq(n_samples)
+      expect(tsne.embedding.shape[1]).to eq(n_components)
+      expect(tsne.n_iter).to eq(max_iter)
+      expect(tsne.kl_divergence.class).to eq(Float)
+      expect(tsne.kl_divergence).not_to be_nil
+      expect(tsne.kl_divergence).to be < init_kl
+    end
 
-  it 'terminates optimization based on the tol parameter.' do
-    low_samples = tsne_tol.fit_transform(samples)
-    expect(low_samples.class).to eq(Numo::DFloat)
-    expect(low_samples.shape[0]).to eq(n_samples)
-    expect(low_samples.shape[1]).to eq(n_components)
-    expect(tsne_tol.embedding.class).to eq(Numo::DFloat)
-    expect(tsne_tol.embedding.shape[0]).to eq(n_samples)
-    expect(tsne_tol.embedding.shape[1]).to eq(n_components)
-    expect(tsne_tol.n_iter).to be < 50
-    expect(tsne_tol.kl_divergence.class).to eq(Float)
-    expect(tsne_tol.kl_divergence).not_to be_nil
-    expect(tsne_tol.kl_divergence).to be < init_kl
-  end
-
-  it 'raises ArgumentError when given a non-square matrix.' do
-    expect { tsne_metric.fit(Numo::DFloat.new(5, 3).rand) }.to raise_error(ArgumentError)
-  end
-
-  it 'outputs debug messages.' do
-    expect { tsne_params.fit(samples) }.to output(/t-SNE/).to_stdout
-  end
-
-  it 'dumps and restores itself using Marshal module.' do
-    copied = Marshal.load(Marshal.dump(tsne_params))
-    expect(tsne_params.class).to eq(copied.class)
-    expect(tsne_params.params[:n_components]).to match(copied.params[:n_components])
-    expect(tsne_params.params[:perplexity]).to match(copied.params[:perplexity])
-    expect(tsne_params.params[:max_iter]).to match(copied.params[:max_iter])
-    expect(tsne_params.params[:metric]).to match(copied.params[:metric])
-    expect(tsne_params.params[:init]).to match(copied.params[:init])
-    expect(tsne_params.params[:verbose]).to match(copied.params[:verbose])
-    expect(tsne_params.params[:random_seed]).to match(copied.params[:random_seed])
-    expect(tsne_params.embedding).to eq(copied.embedding)
-    expect(tsne_params.kl_divergence).to eq(copied.kl_divergence)
-    expect(tsne_params.n_iter).to eq(copied.n_iter)
-    expect(tsne_params.rng).to eq(copied.rng)
+    it 'raises ArgumentError when given a non-square matrix.', :aggregate_failures do
+      expect { tsne.fit(Numo::DFloat.new(5, 3).rand) }.to raise_error(ArgumentError)
+    end
   end
 end
