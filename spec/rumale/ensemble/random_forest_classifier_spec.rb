cb018d22ad42b13a133dff57058a98c177e6c538
yoshoku <yoshoku@outlook.com>
2019-11-26 20:43:19 +0900

    :art: Refactor Random Forest estimator specs

--- spec/rumale/ensemble/random_forest_classifier_spec.rb
+++ spec/rumale/ensemble/random_forest_classifier_spec.rb
@@ -3,100 +3,104 @@
 require 'spec_helper'
 
 RSpec.describe Rumale::Ensemble::RandomForestClassifier do
-  let(:two_clusters) { two_clusters_dataset }
-  let(:x_bin) { two_clusters[0] }
-  let(:y_bin) { two_clusters[1] }
-  let(:three_clusters) { three_clusters_dataset }
-  let(:x_mlt) { three_clusters[0] }
-  let(:y_mlt) { three_clusters[1] }
+  let(:x) { dataset[0] }
+  let(:y) { dataset[1] }
+  let(:classes) { y.to_a.uniq.sort }
+  let(:n_samples) { x.shape[0] }
+  let(:n_features) { x.shape[1] }
+  let(:n_classes) { classes.size }
   let(:n_estimators) { 10 }
-  let(:estimator) { described_class.new(n_estimators: n_estimators, max_depth: 2, max_features: 2, random_seed: 1) }
-  let(:estimator_parallel) do
-    described_class.new(n_estimators: n_estimators, max_depth: 1, max_features: 1, n_jobs: -1, random_seed: 1)
+  let(:n_jobs) { nil }
+  let(:estimator) do
+    described_class.new(n_estimators: n_estimators, max_depth: 2, max_features: 2, n_jobs: n_jobs, random_seed: 1).fit(x, y)
+  end
+  let(:score) { estimator.score(x, y) }
+
+  context 'when binary classification problem' do
+    let(:dataset) { two_clusters_dataset }
+
+    it 'classifies two clusters data.', :aggregate_failures do
+      expect(estimator.params[:n_estimators]).to eq(n_estimators)
+      expect(estimator.params[:max_depth]).to eq(2)
+      expect(estimator.params[:max_features]).to eq(2)
+      expect(estimator.estimators.class).to eq(Array)
+      expect(estimator.estimators.size).to eq(n_estimators)
+      expect(estimator.estimators[0].class).to eq(Rumale::Tree::DecisionTreeClassifier)
+      expect(estimator.classes.class).to eq(Numo::Int32)
+      expect(estimator.classes.ndim).to eq(1)
+      expect(estimator.classes.shape[0]).to eq(n_classes)
+      expect(estimator.feature_importances.class).to eq(Numo::DFloat)
+      expect(estimator.feature_importances.ndim).to eq(1)
+      expect(estimator.feature_importances.shape[0]).to eq(n_features)
+      expect(score).to eq(1.0)
+    end
   end
 
-  it 'classifies two clusters data.' do
-    _n_samples, n_features = x_bin.shape
-    estimator.fit(x_bin, y_bin)
-    expect(estimator.params[:n_estimators]).to eq(n_estimators)
-    expect(estimator.params[:max_depth]).to eq(2)
-    expect(estimator.params[:max_features]).to eq(2)
-    expect(estimator.estimators.class).to eq(Array)
-    expect(estimator.estimators.size).to eq(n_estimators)
-    expect(estimator.estimators[0].class).to eq(Rumale::Tree::DecisionTreeClassifier)
-    expect(estimator.classes.class).to eq(Numo::Int32)
-    expect(estimator.classes.size).to eq(2)
-    expect(estimator.feature_importances.class).to eq(Numo::DFloat)
-    expect(estimator.feature_importances.shape[0]).to eq(n_features)
-    expect(estimator.feature_importances.shape[1]).to be_nil
-    expect(estimator.score(x_bin, y_bin)).to eq(1.0)
-  end
+  context 'when multiclass classification problem' do
+    let(:dataset) { three_clusters_dataset }
+    let(:probs) { estimator.predict_proba(x) }
+    let(:predicted_by_probs) { Numo::Int32[*(Array.new(n_samples) { |n| classes[probs[n, true].max_index] })] }
+    let(:index_mat) { estimator.apply(x) }
+    let(:copied) { Marshal.load(Marshal.dump(estimator)) }
 
-  it 'classifies three clusters data.' do
-    _n_samples, n_features = x_mlt.shape
-    estimator.fit(x_mlt, y_mlt)
-    expect(estimator.estimators.class).to eq(Array)
-    expect(estimator.estimators.size).to eq(n_estimators)
-    expect(estimator.estimators[0].class).to eq(Rumale::Tree::DecisionTreeClassifier)
-    expect(estimator.classes.class).to eq(Numo::Int32)
-    expect(estimator.classes.size).to eq(3)
-    expect(estimator.feature_importances.class).to eq(Numo::DFloat)
-    expect(estimator.feature_importances.shape[0]).to eq(n_features)
-    expect(estimator.feature_importances.shape[1]).to be_nil
-    expect(estimator.score(x_mlt, y_mlt)).to eq(1.0)
-  end
+    it 'classifies three clusters data.', :aggregate_failures do
+      expect(estimator.estimators.class).to eq(Array)
+      expect(estimator.estimators.size).to eq(n_estimators)
+      expect(estimator.estimators[0].class).to eq(Rumale::Tree::DecisionTreeClassifier)
+      expect(estimator.classes.class).to eq(Numo::Int32)
+      expect(estimator.classes.ndim).to eq(1)
+      expect(estimator.classes.shape[0]).to eq(n_classes)
+      expect(estimator.feature_importances.class).to eq(Numo::DFloat)
+      expect(estimator.feature_importances.ndim).to eq(1)
+      expect(estimator.feature_importances.shape[0]).to eq(n_features)
+      expect(score).to eq(1.0)
+    end
 
-  it 'estimates class probabilities with three clusters dataset.' do
-    n_samples, = x_mlt.shape
-    estimator.fit(x_mlt, y_mlt)
-    probs = estimator.predict_proba(x_mlt)
-    expect(probs.class).to eq(Numo::DFloat)
-    expect(probs.shape[0]).to eq(n_samples)
-    expect(probs.shape[1]).to eq(3)
-    classes = y_mlt.to_a.uniq.sort
-    predicted = Numo::Int32.asarray(Array.new(n_samples) { |n| classes[probs[n, true].max_index] })
-    expect(predicted).to eq(y_mlt)
-  end
+    it 'estimates class probabilities with three clusters dataset.', :aggregate_failures do
+      expect(probs.class).to eq(Numo::DFloat)
+      expect(probs.ndim).to eq(2)
+      expect(probs.shape[0]).to eq(n_samples)
+      expect(probs.shape[1]).to eq(n_classes)
+      expect(predicted_by_probs).to eq(y)
+    end
 
-  it 'classifies three clusters data in parallel.' do
-    n_samples, n_features = x_mlt.shape
-    estimator_parallel.fit(x_mlt, y_mlt)
-    probs = estimator_parallel.predict_proba(x_mlt)
-    classes = y_mlt.to_a.uniq.sort
-    predicted = Numo::Int32.asarray(Array.new(n_samples) { |n| classes[probs[n, true].max_index] })
-    expect(estimator_parallel.estimators.class).to eq(Array)
-    expect(estimator_parallel.estimators.size).to eq(n_estimators)
-    expect(estimator_parallel.estimators[0].class).to eq(Rumale::Tree::DecisionTreeClassifier)
-    expect(estimator_parallel.classes.class).to eq(Numo::Int32)
-    expect(estimator_parallel.classes.size).to eq(3)
-    expect(estimator_parallel.feature_importances.class).to eq(Numo::DFloat)
-    expect(estimator_parallel.feature_importances.shape[0]).to eq(n_features)
-    expect(estimator_parallel.feature_importances.shape[1]).to be_nil
-    expect(estimator_parallel.score(x_mlt, y_mlt)).to eq(1.0)
-    expect(probs.class).to eq(Numo::DFloat)
-    expect(probs.shape[0]).to eq(n_samples)
-    expect(probs.shape[1]).to eq(3)
-    expect(predicted).to eq(y_mlt)
-  end
+    it 'returns leaf index that each sample reached', :aggregate_failures do
+      expect(index_mat.ndim).to eq(2)
+      expect(index_mat.shape[0]).to eq(n_samples)
+      expect(index_mat.shape[1]).to eq(n_estimators)
+      expect(index_mat[true, 0]).to eq(estimator.estimators[0].apply(x))
+    end
 
-  it 'returns leaf index that each sample reached' do
-    n_samples, = x_mlt.shape
-    estimator.fit(x_mlt, y_mlt)
-    index_mat = estimator.apply(x_mlt)
-    expect(index_mat.shape[0]).to eq(n_samples)
-    expect(index_mat.shape[1]).to eq(n_estimators)
-    expect(index_mat[true, 0]).to eq(estimator.estimators[0].apply(x_mlt))
-  end
+    it 'dumps and restores itself using Marshal module.', :aggregate_failures do
+      expect(estimator.class).to eq(copied.class)
+      expect(estimator.params).to match(copied.params)
+      expect(estimator.estimators.size).to eq(copied.estimators.size)
+      expect(estimator.classes).to eq(copied.classes)
+      expect(estimator.feature_importances).to eq(copied.feature_importances)
+      expect(estimator.rng).to eq(copied.rng)
+      expect(score).to eq(copied.score(x, y))
+    end
 
-  it 'dumps and restores itself using Marshal module.' do
-    estimator.fit(x_mlt, y_mlt)
-    copied = Marshal.load(Marshal.dump(estimator))
-    expect(estimator.class).to eq(copied.class)
-    expect(estimator.params).to match(copied.params)
-    expect(estimator.estimators.size).to eq(copied.estimators.size)
-    expect(estimator.classes).to eq(copied.classes)
-    expect(estimator.feature_importances).to eq(copied.feature_importances)
-    expect(estimator.rng).to eq(copied.rng)
-    expect(copied.score(x_mlt, y_mlt)).to eq(1.0)
+    context 'when n_jobs parameter is not nil' do
+      let(:n_jobs) { -1 }
+
+      it 'classifies three clusters data in parallel.', :aggregate_failures do
+        expect(estimator.estimators.class).to eq(Array)
+        expect(estimator.estimators.size).to eq(n_estimators)
+        expect(estimator.estimators[0].class).to eq(Rumale::Tree::DecisionTreeClassifier)
+        expect(estimator.classes.class).to eq(Numo::Int32)
+        expect(estimator.classes.ndim).to eq(1)
+        expect(estimator.classes.shape[0]).to eq(n_classes)
+        expect(estimator.feature_importances.class).to eq(Numo::DFloat)
+        expect(estimator.feature_importances.ndim).to eq(1)
+        expect(estimator.feature_importances.shape[0]).to eq(n_features)
+        expect(score).to eq(1.0)
+        expect(probs.class).to eq(Numo::DFloat)
+        expect(probs.ndim).to eq(2)
+        expect(probs.shape[0]).to eq(n_samples)
+        expect(probs.shape[1]).to eq(n_classes)
+        expect(predicted_by_probs).to eq(y)
+      end
+    end
   end
 end
