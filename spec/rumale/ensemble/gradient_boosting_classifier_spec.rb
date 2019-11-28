d3ff49a2b6601374d362963747dedf528583d1a7
yoshoku <yoshoku@outlook.com>
2019-11-28 20:07:19 +0900

    :art: Refactor Gradient Boosting estimator specs

--- spec/rumale/ensemble/gradient_boosting_classifier_spec.rb
+++ spec/rumale/ensemble/gradient_boosting_classifier_spec.rb
@@ -3,94 +3,107 @@
 require 'spec_helper'
 
 RSpec.describe Rumale::Ensemble::GradientBoostingClassifier do
-  let(:two_clusters) { two_clusters_dataset }
-  let(:x_bin) { two_clusters[0] }
-  let(:y_bin) { two_clusters[1] }
-  let(:three_clusters) { three_clusters_dataset }
-  let(:x_mlt) { three_clusters[0] }
-  let(:y_mlt) { three_clusters[1] }
+  let(:x) { dataset[0] }
+  let(:y) { dataset[1] }
+  let(:classes) { y.to_a.uniq.sort }
+  let(:n_samples) { x.shape[0] }
+  let(:n_features) { x.shape[1] }
+  let(:n_classes) { classes.size }
   let(:n_estimators) { 10 }
-  let(:estimator) { described_class.new(n_estimators: n_estimators, learning_rate: 0.9, max_features: 1, random_seed: 1) }
-  let(:estimator_parallel) do
-    described_class.new(n_estimators: n_estimators, learning_rate: 0.9, max_features: 1, n_jobs: -1, random_seed: 1)
+  let(:n_jobs) { nil }
+  let(:estimator) do
+    described_class.new(n_estimators: n_estimators, learning_rate: 0.9, max_features: 1, n_jobs: n_jobs, random_seed: 1).fit(x, y)
+  end
+  let(:score) { estimator.score(x, y) }
+  let(:leaf_ids) { estimator.apply(x) }
+
+  context 'when binary classification problem' do
+    let(:dataset) { two_clusters_dataset }
+
+    it 'classifies two clusters data.', :aggregate_failures do
+      expect(estimator.estimators.class).to eq(Array)
+      expect(estimator.estimators[0].class).to eq(Rumale::Tree::GradientTreeRegressor)
+      expect(estimator.estimators.size).to eq(n_estimators)
+      expect(estimator.classes.class).to eq(Numo::Int32)
+      expect(estimator.classes.ndim).to eq(1)
+      expect(estimator.classes.shape[0]).to eq(n_classes)
+      expect(estimator.feature_importances.class).to eq(Numo::DFloat)
+      expect(estimator.feature_importances.ndim).to eq(1)
+      expect(estimator.feature_importances.shape[0]).to eq(n_features)
+      expect(score).to be_within(0.02).of(1.0)
+      expect(leaf_ids.class).to eq(Numo::Int32)
+      expect(leaf_ids.ndim).to eq(2)
+      expect(leaf_ids.shape[0]).to eq(n_samples)
+      expect(leaf_ids.shape[1]).to eq(n_estimators)
+    end
   end
 
-  it 'classifies two clusters data.' do
-    n_samples, n_features = x_bin.shape
-    estimator.fit(x_bin, y_bin)
-    leaf_ids = estimator.apply(x_bin)
-    expect(estimator.estimators.class).to eq(Array)
-    expect(estimator.estimators[0].class).to eq(Rumale::Tree::GradientTreeRegressor)
-    expect(estimator.estimators.size).to eq(n_estimators)
-    expect(estimator.classes.class).to eq(Numo::Int32)
-    expect(estimator.classes.size).to eq(2)
-    expect(estimator.feature_importances.class).to eq(Numo::DFloat)
-    expect(estimator.feature_importances.shape[0]).to eq(n_features)
-    expect(estimator.feature_importances.shape[1]).to be_nil
-    expect(estimator.score(x_bin, y_bin)).to be_within(0.02).of(1.0)
-    expect(leaf_ids.class).to eq(Numo::Int32)
-    expect(leaf_ids.shape).to eq([n_samples, n_estimators])
-  end
+  context 'when multiclass classification problem' do
+    let(:dataset) { three_clusters_dataset }
+    let(:probs) { estimator.predict_proba(x) }
+    let(:predicted_by_probs) { Numo::Int32[*(Array.new(n_samples) { |n| classes[probs[n, true].max_index] })] }
+    let(:copied) { Marshal.load(Marshal.dump(estimator)) }
 
-  it 'classifies three clusters data.' do
-    n_samples, n_features = x_mlt.shape
-    estimator.fit(x_mlt, y_mlt)
-    leaf_ids = estimator.apply(x_mlt)
-    expect(estimator.estimators.class).to eq(Array)
-    expect(estimator.estimators[0].class).to eq(Array)
-    expect(estimator.estimators[0][0].class).to eq(Rumale::Tree::GradientTreeRegressor)
-    expect(estimator.estimators.size).to eq(3)
-    expect(estimator.estimators[0].size).to eq(n_estimators)
-    expect(estimator.classes.class).to eq(Numo::Int32)
-    expect(estimator.classes.size).to eq(3)
-    expect(estimator.feature_importances.class).to eq(Numo::DFloat)
-    expect(estimator.feature_importances.shape[0]).to eq(n_features)
-    expect(estimator.feature_importances.shape[1]).to be_nil
-    expect(estimator.score(x_mlt, y_mlt)).to be_within(0.02).of(1.0)
-    expect(leaf_ids.class).to eq(Numo::Int32)
-    expect(leaf_ids.shape).to eq([n_samples, n_estimators, 3])
-  end
+    it 'classifies three clusters data.', :aggregate_failures do
+      expect(estimator.estimators.class).to eq(Array)
+      expect(estimator.estimators[0].class).to eq(Array)
+      expect(estimator.estimators[0][0].class).to eq(Rumale::Tree::GradientTreeRegressor)
+      expect(estimator.estimators.size).to eq(3)
+      expect(estimator.estimators[0].size).to eq(n_estimators)
+      expect(estimator.classes.class).to eq(Numo::Int32)
+      expect(estimator.classes.ndim).to eq(1)
+      expect(estimator.classes.shape[0]).to eq(n_classes)
+      expect(estimator.feature_importances.class).to eq(Numo::DFloat)
+      expect(estimator.feature_importances.ndim).to eq(1)
+      expect(estimator.feature_importances.shape[0]).to eq(n_features)
+      expect(score).to be_within(0.02).of(1.0)
+      expect(leaf_ids.class).to eq(Numo::Int32)
+      expect(leaf_ids.ndim).to eq(3)
+      expect(leaf_ids.shape[0]).to eq(n_samples)
+      expect(leaf_ids.shape[1]).to eq(n_estimators)
+      expect(leaf_ids.shape[2]).to eq(3)
+    end
 
-  it 'classifies three clusters data in parallel.' do
-    n_samples, n_features = x_mlt.shape
-    estimator_parallel.fit(x_mlt, y_mlt)
-    leaf_ids = estimator_parallel.apply(x_mlt)
-    expect(estimator_parallel.estimators.class).to eq(Array)
-    expect(estimator_parallel.estimators[0].class).to eq(Array)
-    expect(estimator_parallel.estimators[0][0].class).to eq(Rumale::Tree::GradientTreeRegressor)
-    expect(estimator_parallel.estimators.size).to eq(3)
-    expect(estimator_parallel.estimators[0].size).to eq(n_estimators)
-    expect(estimator_parallel.classes.class).to eq(Numo::Int32)
-    expect(estimator_parallel.classes.size).to eq(3)
-    expect(estimator_parallel.feature_importances.class).to eq(Numo::DFloat)
-    expect(estimator_parallel.feature_importances.shape[0]).to eq(n_features)
-    expect(estimator_parallel.feature_importances.shape[1]).to be_nil
-    expect(estimator_parallel.score(x_mlt, y_mlt)).to be_within(0.02).of(1.0)
-    expect(leaf_ids.class).to eq(Numo::Int32)
-    expect(leaf_ids.shape).to eq([n_samples, n_estimators, 3])
-  end
+    it 'estimates class probabilities with three clusters dataset.', :aggregate_failures do
+      expect(probs.class).to eq(Numo::DFloat)
+      expect(probs.ndim).to eq(2)
+      expect(probs.shape[0]).to eq(n_samples)
+      expect(probs.shape[1]).to eq(n_classes)
+      expect(predicted_by_probs).to eq(y)
+    end
 
-  it 'estimates class probabilities with three clusters dataset.' do
-    n_samples = x_mlt.shape[0]
-    estimator.fit(x_mlt, y_mlt)
-    probs = estimator.predict_proba(x_mlt)
-    classes = y_mlt.to_a.uniq.sort
-    predicted = Numo::Int32.asarray(Array.new(n_samples) { |n| classes[probs[n, true].max_index] })
-    expect(probs.class).to eq(Numo::DFloat)
-    expect(probs.shape[0]).to eq(n_samples)
-    expect(probs.shape[1]).to eq(3)
-    expect(predicted).to eq(y_mlt)
-  end
+    it 'dumps and restores itself using Marshal module.', :aggeregate_failures do
+      expect(estimator.class).to eq(copied.class)
+      expect(estimator.params).to match(copied.params)
+      expect(estimator.estimators.size).to eq(copied.estimators.size)
+      expect(estimator.classes).to eq(copied.classes)
+      expect(estimator.feature_importances).to eq(copied.feature_importances)
+      expect(estimator.rng).to eq(copied.rng)
+      expect(score).to eq(copied.score(x, y))
+    end
 
-  it 'dumps and restores itself using Marshal module.' do
-    estimator.fit(x_mlt, y_mlt)
-    copied = Marshal.load(Marshal.dump(estimator))
-    expect(estimator.class).to eq(copied.class)
-    expect(estimator.params).to match(copied.params)
-    expect(estimator.estimators.size).to eq(copied.estimators.size)
-    expect(estimator.classes).to eq(copied.classes)
-    expect(estimator.feature_importances).to eq(copied.feature_importances)
-    expect(estimator.rng).to eq(copied.rng)
-    expect(estimator.score(x_mlt, y_mlt)).to eq(copied.score(x_mlt, y_mlt))
+    context 'when n_jobs parameter is not nil' do
+      let(:n_jobs) { -1 }
+
+      it 'classifies three clusters data in parallel.', :aggregate_failures do
+        expect(estimator.estimators.class).to eq(Array)
+        expect(estimator.estimators[0].class).to eq(Array)
+        expect(estimator.estimators[0][0].class).to eq(Rumale::Tree::GradientTreeRegressor)
+        expect(estimator.estimators.size).to eq(3)
+        expect(estimator.estimators[0].size).to eq(n_estimators)
+        expect(estimator.classes.class).to eq(Numo::Int32)
+        expect(estimator.classes.ndim).to eq(1)
+        expect(estimator.classes.shape[0]).to eq(n_classes)
+        expect(estimator.feature_importances.class).to eq(Numo::DFloat)
+        expect(estimator.feature_importances.ndim).to eq(1)
+        expect(estimator.feature_importances.shape[0]).to eq(n_features)
+        expect(score).to be_within(0.02).of(1.0)
+        expect(leaf_ids.class).to eq(Numo::Int32)
+        expect(leaf_ids.ndim).to eq(3)
+        expect(leaf_ids.shape[0]).to eq(n_samples)
+        expect(leaf_ids.shape[1]).to eq(n_estimators)
+        expect(leaf_ids.shape[2]).to eq(3)
+      end
+    end
   end
 end
--- spec/rumale/ensemble/gradient_boosting_regressor_spec.rb
+++ spec/rumale/ensemble/gradient_boosting_regressor_spec.rb
@@ -4,113 +4,124 @@ require 'spec_helper'
 
 RSpec.describe Rumale::Ensemble::GradientBoostingRegressor do
   let(:x) { two_clusters_dataset[0] }
-  let(:y) { x[true, 0] + x[true, 1]**2 }
-  let(:y_mult) { Numo::DFloat[x[true, 0].to_a, (x[true, 1]**2).to_a].transpose.dot(Numo::DFloat[[0.6, 0.4], [0.0, 0.1]]) }
   let(:n_samples) { x.shape[0] }
   let(:n_features) { x.shape[1] }
-  let(:n_outputs) { y_mult.shape[1] }
   let(:n_estimators) { 10 }
-  let(:estimator) { described_class.new(n_estimators: n_estimators, learning_rate: 0.9, reg_lambda: 0.001, max_features: 1, random_seed: 9) }
-  let(:estimator_parallel) do
-    described_class.new(n_estimators: n_estimators, learning_rate: 0.9, reg_lambda: 0.001, max_features: 1, n_jobs: -1, random_seed: 9)
+  let(:n_jobs) { nil }
+  let(:estimator) do
+    described_class.new(n_estimators: n_estimators, learning_rate: 0.9, reg_lambda: 0.001, max_features: 1,
+                        n_jobs: n_jobs, random_seed: 9).fit(x, y)
+  end
+  let(:leaf_ids) { estimator.apply(x) }
+  let(:predicted) { estimator.predict(x) }
+  let(:score) { estimator.score(x, y) }
+
+  context 'when single target problem' do
+    let(:y) { x[true, 0] + x[true, 1]**2 }
+    let(:copied) { Marshal.load(Marshal.dump(estimator)) }
+
+    it 'learns the model for single regression problem.', :aggregate_failures do
+      expect(estimator.params[:n_estimators]).to eq(n_estimators)
+      expect(estimator.params[:max_features]).to eq(1)
+      expect(estimator.estimators.class).to eq(Array)
+      expect(estimator.estimators[0].class).to eq(Rumale::Tree::GradientTreeRegressor)
+      expect(estimator.estimators.size).to eq(n_estimators)
+      expect(estimator.feature_importances.class).to eq(Numo::DFloat)
+      expect(estimator.feature_importances.ndim).to eq(1)
+      expect(estimator.feature_importances.shape[0]).to eq(n_features)
+      expect(predicted.class).to eq(Numo::DFloat)
+      expect(predicted.ndim).to eq(1)
+      expect(predicted.shape[0]).to eq(n_samples)
+      expect(score).to be_within(0.02).of(1.0)
+      expect(leaf_ids.class).to eq(Numo::Int32)
+      expect(leaf_ids.ndim).to eq(2)
+      expect(leaf_ids.shape[0]).to eq(n_samples)
+      expect(leaf_ids.shape[1]).to eq(n_estimators)
+      expect(leaf_ids[true, 0]).to eq(estimator.estimators[0].apply(x))
+    end
+
+    it 'dumps and restores itself using Marshal module.', :aggregate_failures do
+      expect(estimator.class).to eq(copied.class)
+      expect(estimator.params).to match(copied.params)
+      expect(estimator.estimators.size).to eq(copied.estimators.size)
+      expect(estimator.feature_importances).to eq(copied.feature_importances)
+      expect(estimator.rng).to eq(copied.rng)
+      expect(score).to eq(copied.score(x, y))
+    end
+
+    context 'when n_jobs parameter is not nil' do
+      let(:n_jobs) { -1 }
+
+      it 'learns the model for single regression problem in parallel.', :aggregate_failures do
+        expect(estimator.params[:n_estimators]).to eq(n_estimators)
+        expect(estimator.params[:max_features]).to eq(1)
+        expect(estimator.estimators.class).to eq(Array)
+        expect(estimator.estimators[0].class).to eq(Rumale::Tree::GradientTreeRegressor)
+        expect(estimator.estimators.size).to eq(n_estimators)
+        expect(estimator.feature_importances.class).to eq(Numo::DFloat)
+        expect(estimator.feature_importances.ndim).to eq(1)
+        expect(estimator.feature_importances.shape[0]).to eq(n_features)
+        expect(predicted.class).to eq(Numo::DFloat)
+        expect(predicted.ndim).to eq(1)
+        expect(predicted.shape[0]).to eq(n_samples)
+        expect(score).to be_within(0.02).of(1.0)
+        expect(leaf_ids.class).to eq(Numo::Int32)
+        expect(leaf_ids.ndim).to eq(2)
+        expect(leaf_ids.shape[0]).to eq(n_samples)
+        expect(leaf_ids.shape[1]).to eq(n_estimators)
+        expect(leaf_ids[true, 0]).to eq(estimator.estimators[0].apply(x))
+      end
+    end
   end
 
-  it 'learns the model for single regression problem.' do
-    estimator.fit(x, y)
-    leaf_ids = estimator.apply(x)
-    predicted = estimator.predict(x)
-    expect(estimator.params[:n_estimators]).to eq(n_estimators)
-    expect(estimator.params[:max_features]).to eq(1)
-    expect(estimator.estimators.class).to eq(Array)
-    expect(estimator.estimators[0].class).to eq(Rumale::Tree::GradientTreeRegressor)
-    expect(estimator.estimators.size).to eq(n_estimators)
-    expect(estimator.feature_importances.class).to eq(Numo::DFloat)
-    expect(estimator.feature_importances.shape[0]).to eq(n_features)
-    expect(estimator.feature_importances.shape[1]).to be_nil
-    expect(predicted.class).to eq(Numo::DFloat)
-    expect(predicted.shape[0]).to eq(n_samples)
-    expect(predicted.shape[1]).to be_nil
-    expect(estimator.score(x, y)).to be_within(0.02).of(1.0)
-    expect(leaf_ids.class).to eq(Numo::Int32)
-    expect(leaf_ids.shape).to eq([n_samples, n_estimators])
-  end
+  context 'when multi-target problem' do
+    let(:y) { Numo::DFloat[x[true, 0].to_a, (x[true, 1]**2).to_a].transpose.dot(Numo::DFloat[[0.6, 0.4], [0.0, 0.1]]) }
+    let(:n_outputs) { y.shape[1] }
 
-  it 'learns the model for multiple regression problem.' do
-    estimator.fit(x, y_mult)
-    leaf_ids = estimator.apply(x)
-    predicted = estimator.predict(x)
-    expect(estimator.estimators.class).to eq(Array)
-    expect(estimator.estimators[0].class).to eq(Array)
-    expect(estimator.estimators[0][0].class).to eq(Rumale::Tree::GradientTreeRegressor)
-    expect(estimator.estimators.size).to eq(n_outputs)
-    expect(estimator.estimators[0].size).to eq(n_estimators)
-    expect(estimator.feature_importances.class).to eq(Numo::DFloat)
-    expect(estimator.feature_importances.shape[0]).to eq(n_features)
-    expect(estimator.feature_importances.shape[1]).to be_nil
-    expect(predicted.class).to eq(Numo::DFloat)
-    expect(predicted.shape[0]).to eq(n_samples)
-    expect(predicted.shape[1]).to eq(n_outputs)
-    expect(estimator.score(x, y_mult)).to be_within(0.02).of(1.0)
-    expect(leaf_ids.class).to eq(Numo::Int32)
-    expect(leaf_ids.shape).to eq([n_samples, n_estimators, n_outputs])
-  end
+    it 'learns the model for multiple regression problem.', :aggregate_failures do
+      expect(estimator.estimators.class).to eq(Array)
+      expect(estimator.estimators[0].class).to eq(Array)
+      expect(estimator.estimators[0][0].class).to eq(Rumale::Tree::GradientTreeRegressor)
+      expect(estimator.estimators.size).to eq(n_outputs)
+      expect(estimator.estimators[0].size).to eq(n_estimators)
+      expect(estimator.feature_importances.class).to eq(Numo::DFloat)
+      expect(estimator.feature_importances.ndim).to eq(1)
+      expect(estimator.feature_importances.shape[0]).to eq(n_features)
+      expect(predicted.class).to eq(Numo::DFloat)
+      expect(predicted.ndim).to eq(2)
+      expect(predicted.shape[0]).to eq(n_samples)
+      expect(predicted.shape[1]).to eq(n_outputs)
+      expect(score).to be_within(0.02).of(1.0)
+      expect(leaf_ids.class).to eq(Numo::Int32)
+      expect(leaf_ids.ndim).to eq(3)
+      expect(leaf_ids.shape[0]).to eq(n_samples)
+      expect(leaf_ids.shape[1]).to eq(n_estimators)
+      expect(leaf_ids.shape[2]).to eq(n_outputs)
+    end
 
-  it 'learns the model for single regression problem in parallel.' do
-    estimator_parallel.fit(x, y)
-    leaf_ids = estimator_parallel.apply(x)
-    predicted = estimator_parallel.predict(x)
-    expect(estimator_parallel.params[:n_estimators]).to eq(n_estimators)
-    expect(estimator_parallel.params[:max_features]).to eq(1)
-    expect(estimator_parallel.estimators.class).to eq(Array)
-    expect(estimator_parallel.estimators[0].class).to eq(Rumale::Tree::GradientTreeRegressor)
-    expect(estimator_parallel.estimators.size).to eq(n_estimators)
-    expect(estimator_parallel.feature_importances.class).to eq(Numo::DFloat)
-    expect(estimator_parallel.feature_importances.shape[0]).to eq(n_features)
-    expect(estimator_parallel.feature_importances.shape[1]).to be_nil
-    expect(predicted.class).to eq(Numo::DFloat)
-    expect(predicted.shape[0]).to eq(n_samples)
-    expect(predicted.shape[1]).to be_nil
-    expect(estimator_parallel.score(x, y)).to be_within(0.02).of(1.0)
-    expect(leaf_ids.class).to eq(Numo::Int32)
-    expect(leaf_ids.shape).to eq([n_samples, n_estimators])
-  end
+    context 'when n_jobs parameter is not nil' do
+      let(:n_jobs) { -1 }
 
-  it 'learns the model for multiple regression problem in parallel.' do
-    estimator_parallel.fit(x, y_mult)
-    leaf_ids = estimator_parallel.apply(x)
-    predicted = estimator_parallel.predict(x)
-    expect(estimator_parallel.estimators.class).to eq(Array)
-    expect(estimator_parallel.estimators[0].class).to eq(Array)
-    expect(estimator_parallel.estimators[0][0].class).to eq(Rumale::Tree::GradientTreeRegressor)
-    expect(estimator_parallel.estimators.size).to eq(n_outputs)
-    expect(estimator_parallel.estimators[0].size).to eq(n_estimators)
-    expect(estimator_parallel.feature_importances.class).to eq(Numo::DFloat)
-    expect(estimator_parallel.feature_importances.shape[0]).to eq(n_features)
-    expect(estimator_parallel.feature_importances.shape[1]).to be_nil
-    expect(predicted.class).to eq(Numo::DFloat)
-    expect(predicted.shape[0]).to eq(n_samples)
-    expect(predicted.shape[1]).to eq(n_outputs)
-    expect(estimator_parallel.score(x, y_mult)).to be_within(0.02).of(1.0)
-    expect(leaf_ids.class).to eq(Numo::Int32)
-    expect(leaf_ids.shape).to eq([n_samples, n_estimators, n_outputs])
-  end
-
-  it 'returns leaf index that each sample reached.' do
-    estimator.fit(x, y)
-    index_mat = estimator.apply(x)
-    expect(index_mat.shape[0]).to eq(n_samples)
-    expect(index_mat.shape[1]).to eq(n_estimators)
-    expect(index_mat[true, 0]).to eq(estimator.estimators[0].apply(x))
-  end
-
-  it 'dumps and restores itself using Marshal module.' do
-    estimator.fit(x, y)
-    copied = Marshal.load(Marshal.dump(estimator))
-    expect(estimator.class).to eq(copied.class)
-    expect(estimator.params).to match(copied.params)
-    expect(estimator.estimators.size).to eq(copied.estimators.size)
-    expect(estimator.feature_importances).to eq(copied.feature_importances)
-    expect(estimator.rng).to eq(copied.rng)
-    expect(estimator.score(x, y)).to eq(copied.score(x, y))
+      it 'learns the model for multiple regression problem in parallel.', :aggregate_failures do
+        expect(estimator.estimators.class).to eq(Array)
+        expect(estimator.estimators[0].class).to eq(Array)
+        expect(estimator.estimators[0][0].class).to eq(Rumale::Tree::GradientTreeRegressor)
+        expect(estimator.estimators.size).to eq(n_outputs)
+        expect(estimator.estimators[0].size).to eq(n_estimators)
+        expect(estimator.feature_importances.class).to eq(Numo::DFloat)
+        expect(estimator.feature_importances.ndim).to eq(1)
+        expect(estimator.feature_importances.shape[0]).to eq(n_features)
+        expect(predicted.class).to eq(Numo::DFloat)
+        expect(predicted.ndim).to eq(2)
+        expect(predicted.shape[0]).to eq(n_samples)
+        expect(predicted.shape[1]).to eq(n_outputs)
+        expect(score).to be_within(0.02).of(1.0)
+        expect(leaf_ids.class).to eq(Numo::Int32)
+        expect(leaf_ids.ndim).to eq(3)
+        expect(leaf_ids.shape[0]).to eq(n_samples)
+        expect(leaf_ids.shape[1]).to eq(n_estimators)
+        expect(leaf_ids.shape[2]).to eq(n_outputs)
+      end
+    end
   end
 end
