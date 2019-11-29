efc91eecc43e9b34170dcd9bd0a8af4e860cb7d5
yoshoku <yoshoku@outlook.com>
2019-11-29 23:57:51 +0900

    :art: Refactor accuracy spec

--- spec/rumale/evaluation_measure/accuracy_spec.rb
+++ spec/rumale/evaluation_measure/accuracy_spec.rb
@@ -3,22 +3,25 @@
 require 'spec_helper'
 
 RSpec.describe Rumale::EvaluationMeasure::Accuracy do
-  let(:bin_ground_truth) { Numo::Int32[1, 1, 1, 1, 1, -1, -1, -1, -1, -1] }
-  let(:bin_predicted) { Numo::Int32[-1, -1, 1, 1, 1, -1, -1, 1, 1, 1] }
-  let(:mult_ground_truth) { Numo::Int32[1, 1, 2, 2, 3, 3, 0, 0, 4, 4] }
-  let(:mult_predicted) { Numo::Int32[5, 1, 5, 2, 5, 3, 5, 0, 5, 4] }
+  let(:accuracy) { described_class.new.score(ground_truth, predicted) }
 
-  it 'calculates accuracy for binary classification task.' do
-    evaluator = described_class.new
-    accuracy = evaluator.score(bin_ground_truth, bin_predicted)
-    expect(accuracy.class).to eq(Float)
-    expect(accuracy).to eq(0.5)
+  context 'when binary classification problem' do
+    let(:ground_truth) { Numo::Int32[1, 1, 1, 1, 1, -1, -1, -1, -1, -1] }
+    let(:predicted) { Numo::Int32[-1, -1, 1, 1, 1, -1, -1, 1, 1, 1] }
+
+    it 'calculates accuracy for binary classification task.', :aggregate_failures do
+      expect(accuracy.class).to eq(Float)
+      expect(accuracy).to eq(0.5)
+    end
   end
 
-  it 'calculates accuracy for multilabel classification task.' do
-    evaluator = described_class.new
-    accuracy = evaluator.score(mult_ground_truth, mult_predicted)
-    expect(accuracy.class).to eq(Float)
-    expect(accuracy).to eq(0.5)
+  context 'when multiclass classification problem' do
+    let(:ground_truth) { Numo::Int32[1, 1, 2, 2, 3, 3, 0, 0, 4, 4] }
+    let(:predicted) { Numo::Int32[5, 1, 5, 2, 5, 3, 5, 0, 5, 4] }
+
+    it 'calculates accuracy for multilabel classification task.', :aggregate_failures do
+      expect(accuracy.class).to eq(Float)
+      expect(accuracy).to eq(0.5)
+    end
   end
 end
