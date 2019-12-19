6758a2fcab349ae7eafa3c829611b75f870c9783
yoshoku <yoshoku@outlook.com>
2019-12-19 20:18:30 +0900

    :sparkles: Fix to enable to select distance metric of KNeighborsRegressor

--- lib/rumale/nearest_neighbors/k_neighbors_regressor.rb
+++ lib/rumale/nearest_neighbors/k_neighbors_regressor.rb
@@ -19,51 +19,62 @@ module Rumale
       include Base::Regressor
 
       # Return the prototypes for the nearest neighbor regressor.
-      # @return [Numo::DFloat] (shape: [n_samples, n_features])
+      # If the metric is 'precomputed', that returns nil.
+      # @return [Numo::DFloat] (shape: [n_testing_samples, n_features])
       attr_reader :prototypes
 
       # Return the values of the prototypes
-      # @return [Numo::DFloat] (shape: [n_samples, n_outputs])
+      # @return [Numo::DFloat] (shape: [n_testing_samples, n_outputs])
       attr_reader :values
 
       # Create a new regressor with the nearest neighbor rule.
       #
       # @param n_neighbors [Integer] The number of neighbors.
-      def initialize(n_neighbors: 5)
+      # @param metric [String] The metric to calculate the distances.
+      #   If metric is 'euclidean', Euclidean distance is calculated for distance between points.
+      #   If metric is 'precomputed', the fit and predict methods expect to be given a distance matrix.
+      def initialize(n_neighbors: 5, metric: 'euclidean')
         check_params_numeric(n_neighbors: n_neighbors)
         check_params_positive(n_neighbors: n_neighbors)
         @params = {}
         @params[:n_neighbors] = n_neighbors
+        @params[:metric] = metric == 'precomputed' ? 'precomputed' : 'euclidean'
         @prototypes = nil
         @values = nil
       end
 
       # Fit the model with given training data.
       #
-      # @param x [Numo::DFloat] (shape: [n_samples, n_features]) The training data to be used for fitting the model.
-      # @param y [Numo::DFloat] (shape: [n_samples, n_outputs]) The target values to be used for fitting the model.
+      # @param x [Numo::DFloat] (shape: [n_training_samples, n_features]) The training data to be used for fitting the model.
+      #   If the metric is 'precomputed', x must be a square distance matrix (shape: [n_training_samples, n_training_samples]).
+      # @param y [Numo::DFloat] (shape: [n_training_samples, n_outputs]) The target values to be used for fitting the model.
       # @return [KNeighborsRegressor] The learned regressor itself.
       def fit(x, y)
         x = check_convert_sample_array(x)
         y = check_convert_tvalue_array(y)
         check_sample_tvalue_size(x, y)
-        @prototypes = x.dup
+        raise ArgumentError, 'Expect the input distance matrix to be square.' if @params[:metric] == 'precomputed' && x.shape[0] != x.shape[1]
+        @prototypes = x.dup unless @params[:metric] == 'precomputed'
         @values = y.dup
         self
       end
 
       # Predict values for samples.
       #
-      # @param x [Numo::DFloat] (shape: [n_samples, n_features]) The samples to predict the values.
-      # @return [Numo::DFloat] (shape: [n_samples, n_outputs]) Predicted values per sample.
+      # @param x [Numo::DFloat] (shape: [n_training_samples, n_features]) The samples to predict the values.
+      #   If the metric is 'precomputed', x must be a square distance matrix (shape: [n_testing_samples, n_training_samples]).
+      # @return [Numo::DFloat] (shape: [n_training_samples, n_outputs]) Predicted values per sample.
       def predict(x)
         x = check_convert_sample_array(x)
+        if @params[:metric] == 'precomputed' && x.shape[1] != @values.shape[0]
+          raise ArgumentError, 'Expect the size input matrix to be n_testing_samples-by-n_training_samples.'
+        end
         # Initialize some variables.
-        n_samples, = x.shape
+        n_samples = x.shape[0]
         n_prototypes, n_outputs = @values.shape
         n_neighbors = [@params[:n_neighbors], n_prototypes].min
         # Calculate distance matrix.
-        distance_matrix = PairwiseMetric.euclidean_distance(x, @prototypes)
+        distance_matrix = @params[:metrix] == 'precomputed' ? x : PairwiseMetric.euclidean_distance(x, @prototypes)
         # Predict values for the given samples.
         predicted_values = Array.new(n_samples) do |n|
           neighbor_ids = distance_matrix[n, true].to_a.each_with_index.sort.map(&:last)[0...n_neighbors]
--- spec/rumale/nearest_neighbors/k_neighbors_regressor_spec.rb
+++ spec/rumale/nearest_neighbors/k_neighbors_regressor_spec.rb
@@ -3,60 +3,120 @@
 require 'spec_helper'
 
 RSpec.describe Rumale::NearestNeighbors::KNeighborsRegressor do
-  let(:x) { two_clusters_dataset[0] }
-  let(:single_target) { x[true, 0] + x[true, 1]**2 }
-  let(:multi_target) { x.dot(Numo::DFloat[[1.0, 2.0], [2.0, 1.0]]) }
+  let(:dataset) { two_clusters_dataset }
+  let(:single_target) { dataset[0][true, 0] + dataset[0][true, 1]**2 }
+  let(:multi_target) { dataset[0].dot(Numo::DFloat[[1.0, 2.0], [2.0, 1.0]]) }
   let(:n_samples) { x.shape[0] }
-  let(:n_features) { x.shape[1] }
   let(:n_outputs) { y.shape[1] }
-  let(:estimator) { described_class.new(n_neighbors: 5).fit(x, y) }
+  let(:estimator) { described_class.new(n_neighbors: 5, metric: metric).fit(x, y) }
   let(:predicted) { estimator.predict(x) }
   let(:score) { estimator.score(x, y) }
   let(:copied) { Marshal.load(Marshal.dump(estimator)) }
 
-  context 'when single target problem' do
-    let(:y) { single_target }
+  context 'when metric is "euclidean"' do
+    let(:metric) { 'euclidean' }
+    let(:x) { dataset[0] }
+    let(:n_features) { x.shape[1] }
 
-    it 'learns the model for single regression problem.', :aggregate_failures do
-      expect(estimator.prototypes.class).to eq(Numo::DFloat)
-      expect(estimator.prototypes.ndim).to eq(2)
-      expect(estimator.prototypes.shape[0]).to eq(n_samples)
-      expect(estimator.prototypes.shape[1]).to eq(n_features)
-      expect(estimator.values.class).to eq(Numo::DFloat)
-      expect(estimator.values.ndim).to eq(1)
-      expect(estimator.values.shape[0]).to eq(n_samples)
-      expect(predicted.class).to eq(Numo::DFloat)
-      expect(predicted.ndim).to eq(1)
-      expect(predicted.shape[0]).to eq(n_samples)
-      expect(score).to be_within(0.01).of(1.0)
+    context 'when single target problem' do
+      let(:y) { single_target }
+
+      it 'learns the model for single regression problem.', :aggregate_failures do
+        expect(estimator.prototypes.class).to eq(Numo::DFloat)
+        expect(estimator.prototypes.ndim).to eq(2)
+        expect(estimator.prototypes.shape[0]).to eq(n_samples)
+        expect(estimator.prototypes.shape[1]).to eq(n_features)
+        expect(estimator.values.class).to eq(Numo::DFloat)
+        expect(estimator.values.ndim).to eq(1)
+        expect(estimator.values.shape[0]).to eq(n_samples)
+        expect(predicted.class).to eq(Numo::DFloat)
+        expect(predicted.ndim).to eq(1)
+        expect(predicted.shape[0]).to eq(n_samples)
+        expect(score).to be_within(0.01).of(1.0)
+      end
+
+      it 'dumps and restores itself using Marshal module.', :aggregate_failures do
+        expect(estimator.class).to eq(copied.class)
+        expect(estimator.params[:n_neighbors]).to eq(copied.params[:n_neighbors])
+        expect(estimator.prototypes).to eq(copied.prototypes)
+        expect(estimator.values).to eq(copied.values)
+        expect(score).to eq(copied.score(x, y))
+      end
     end
 
-    it 'dumps and restores itself using Marshal module.', :aggregate_failures do
-      expect(estimator.class).to eq(copied.class)
-      expect(estimator.params[:n_neighbors]).to eq(copied.params[:n_neighbors])
-      expect(estimator.prototypes).to eq(copied.prototypes)
-      expect(estimator.values).to eq(copied.values)
-      expect(score).to eq(copied.score(x, y))
+    context 'when multi-target problem' do
+      let(:y) { multi_target }
+
+      it 'learns the model for multiple regression problem.', :aggregate_failures do
+        expect(estimator.prototypes.class).to eq(Numo::DFloat)
+        expect(estimator.prototypes.ndim).to eq(2)
+        expect(estimator.prototypes.shape[0]).to eq(n_samples)
+        expect(estimator.prototypes.shape[1]).to eq(n_features)
+        expect(estimator.values.class).to eq(Numo::DFloat)
+        expect(estimator.values.ndim).to eq(2)
+        expect(estimator.values.shape[0]).to eq(n_samples)
+        expect(estimator.values.shape[1]).to eq(n_outputs)
+        expect(predicted.class).to eq(Numo::DFloat)
+        expect(predicted.ndim).to eq(2)
+        expect(predicted.shape[0]).to eq(n_samples)
+        expect(predicted.shape[1]).to eq(n_outputs)
+        expect(score).to be_within(0.01).of(1.0)
+      end
     end
   end
 
-  context 'when multi-target problem' do
-    let(:y) { multi_target }
+  context 'when metric is "precomputed"' do
+    let(:metric) { 'precomputed' }
+    let(:x) { Rumale::PairwiseMetric.manhattan_distance(dataset[0]) }
 
-    it 'learns the model for multiple regression problem.', :aggregate_failures do
-      expect(estimator.prototypes.class).to eq(Numo::DFloat)
-      expect(estimator.prototypes.ndim).to eq(2)
-      expect(estimator.prototypes.shape[0]).to eq(n_samples)
-      expect(estimator.prototypes.shape[1]).to eq(n_features)
-      expect(estimator.values.class).to eq(Numo::DFloat)
-      expect(estimator.values.ndim).to eq(2)
-      expect(estimator.values.shape[0]).to eq(n_samples)
-      expect(estimator.values.shape[1]).to eq(n_outputs)
-      expect(predicted.class).to eq(Numo::DFloat)
-      expect(predicted.ndim).to eq(2)
-      expect(predicted.shape[0]).to eq(n_samples)
-      expect(predicted.shape[1]).to eq(n_outputs)
-      expect(score).to be_within(0.01).of(1.0)
+    context 'when single target problem' do
+      let(:y) { single_target }
+
+      it 'learns the model for single regression problem.', :aggregate_failures do
+        expect(estimator.prototypes.class).to eq(NilClass)
+        expect(estimator.values.class).to eq(Numo::DFloat)
+        expect(estimator.values.ndim).to eq(1)
+        expect(estimator.values.shape[0]).to eq(n_samples)
+        expect(predicted.class).to eq(Numo::DFloat)
+        expect(predicted.ndim).to eq(1)
+        expect(predicted.shape[0]).to eq(n_samples)
+        expect(score).to be_within(0.01).of(1.0)
+      end
+
+      it 'dumps and restores itself using Marshal module.', :aggregate_failures do
+        expect(estimator.class).to eq(copied.class)
+        expect(estimator.params[:n_neighbors]).to eq(copied.params[:n_neighbors])
+        expect(estimator.prototypes).to eq(copied.prototypes)
+        expect(estimator.values).to eq(copied.values)
+        expect(score).to eq(copied.score(x, y))
+      end
+    end
+
+    context 'when multi-target problem' do
+      let(:y) { multi_target }
+
+      it 'learns the model for multiple regression problem.', :aggregate_failures do
+        expect(estimator.prototypes.class).to eq(NilClass)
+        expect(estimator.values.class).to eq(Numo::DFloat)
+        expect(estimator.values.ndim).to eq(2)
+        expect(estimator.values.shape[0]).to eq(n_samples)
+        expect(estimator.values.shape[1]).to eq(n_outputs)
+        expect(predicted.class).to eq(Numo::DFloat)
+        expect(predicted.ndim).to eq(2)
+        expect(predicted.shape[0]).to eq(n_samples)
+        expect(predicted.shape[1]).to eq(n_outputs)
+        expect(score).to be_within(0.01).of(1.0)
+      end
+    end
+
+    context 'when wrong size matrix is given' do
+      let(:estimator) { described_class.new(n_neighbors: 5, metric: 'precomputed') }
+      let(:y) { single_target }
+
+      it 'raises ArgumentError', :aggregate_failures do
+        expect { estimator.fit(Numo::DFloat.new(n_samples, 2).rand, y) }.to raise_error(ArgumentError)
+        expect { estimator.fit(x, y).predict(Numo::DFloat.new(2, n_samples / 2).rand) }.to raise_error(ArgumentError)
+      end
     end
   end
 end
