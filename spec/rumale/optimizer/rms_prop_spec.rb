08e4e41487ccf7b41f6cb8db44699eae7867d621
yoshoku <yoshoku@outlook.com>
2020-01-12 00:30:36 +0900

    :art: Fix not to use LineareRegression on optimizer specs

--- spec/rumale/optimizer/rms_prop_spec.rb
+++ spec/rumale/optimizer/rms_prop_spec.rb
@@ -3,21 +3,7 @@
 require 'spec_helper'
 
 RSpec.describe Rumale::Optimizer::RMSProp do
-  let(:x) { two_clusters_dataset[0] }
-  let(:y) { x.dot(Numo::DFloat[1.0, 2.0]) }
-  let(:y_mult) { x.dot(Numo::DFloat[[1.0, 2.0], [2.0, 1.0]]) }
   let(:optimizer) { described_class.new(learning_rate: 0.1, momentum: 0.9, decay: 0.1) }
-  let(:estimator) { Rumale::LinearModel::LinearRegression.new(optimizer: optimizer, max_iter: 100, random_seed: 1) }
-
-  it 'learns the model for single regression problem with optimizer.' do
-    estimator.fit(x, y)
-    expect(estimator.score(x, y)).to be_within(0.01).of(1.0)
-  end
-
-  it 'learns the model for multiple-regression problems with optimizer.' do
-    estimator.fit(x, y_mult)
-    expect(estimator.score(x, y_mult)).to be_within(0.01).of(1.0)
-  end
 
   it 'dumps and restores itself using Marshal module.' do
     optimizer.call(Numo::DFloat.new(3).rand, Numo::DFloat.new(3).rand)
