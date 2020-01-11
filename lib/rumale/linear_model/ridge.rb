edc0314bb1f791de04088132b0193d4191b78b4b
yoshoku <yoshoku@outlook.com>
2020-01-12 00:16:36 +0900

    :rocket: Fix Ridge to use new linear model abstract class

--- lib/rumale/linear_model/ridge.rb
+++ lib/rumale/linear_model/ridge.rb
@@ -1,16 +1,16 @@
 # frozen_string_literal: true
 
-require 'rumale/linear_model/base_linear_model'
+require 'rumale/linear_model/base_sgd'
 require 'rumale/base/regressor'
 
 module Rumale
   module LinearModel
     # Ridge is a class that implements Ridge Regression
-    # with mini-batch stochastic gradient descent optimization or singular value decomposition.
+    # with stochastic gradient descent (SGD) optimization or singular value decomposition (SVD).
     #
     # @example
     #   estimator =
-    #     Rumale::LinearModel::Ridge.new(reg_param: 0.1, max_iter: 1000, batch_size: 20, random_seed: 1)
+    #     Rumale::LinearModel::Ridge.new(reg_param: 0.1, max_iter: 500, batch_size: 20, random_seed: 1)
     #   estimator.fit(training_samples, traininig_values)
     #   results = estimator.predict(testing_samples)
     #
@@ -19,7 +19,10 @@ module Rumale
     #   estimator = Rumale::LinearModel::Ridge.new(reg_param: 0.1, solver: 'svd')
     #   estimator.fit(training_samples, traininig_values)
     #   results = estimator.predict(testing_samples)
-    class Ridge < BaseLinearModel
+    #
+    # *Reference*
+    # - L. Bottou, "Large-Scale Machine Learning with Stochastic Gradient Descent," Proc. COMPSTAT'10, pp. 177--186, 2010.
+    class Ridge < BaseSGD
       include Base::Regressor
 
       # Return the weight vector.
@@ -36,15 +39,23 @@ module Rumale
 
       # Create a new Ridge regressor.
       #
+      # @param learning_rate [Float] The initial value of learning rate.
+      #   The learning rate decreases as the iteration proceeds according to the equation: learning_rate / (1 + decay * t).
+      #   If solver = 'svd', this parameter is ignored.
+      # @param decay [Float] The smoothing parameter for decreasing learning rate as the iteration proceeds.
+      #   If nil is given, the decay sets to 'reg_param * learning_rate'.
+      #   If solver = 'svd', this parameter is ignored.
+      # @param momentum [Float] The momentum factor.
+      #   If solver = 'svd', this parameter is ignored.
       # @param reg_param [Float] The regularization parameter.
       # @param fit_bias [Boolean] The flag indicating whether to fit the bias term.
       # @param bias_scale [Float] The scale of the bias term.
-      # @param max_iter [Integer] The maximum number of iterations.
+      # @param max_iter [Integer] The maximum number of epochs that indicates
+      #   how many times the whole data is given to the training process.
       #   If solver = 'svd', this parameter is ignored.
       # @param batch_size [Integer] The size of the mini batches.
       #   If solver = 'svd', this parameter is ignored.
-      # @param optimizer [Optimizer] The optimizer to calculate adaptive learning rate.
-      #   If nil is given, Nadam is used.
+      # @param tol [Float] The tolerance of loss for terminating optimization.
       #   If solver = 'svd', this parameter is ignored.
       # @param solver [String] The algorithm to calculate weights. ('sgd' or 'svd').
       #   'sgd' uses the stochastic gradient descent optimization.
@@ -53,18 +64,31 @@ module Rumale
       #   If nil is given, the method does not execute in parallel.
       #   If zero or less is given, it becomes equal to the number of processors.
       #   This parameter is ignored if the Parallel gem is not loaded or the solver is 'svd'.
+      # @param verbose [Boolean] The flag indicating whether to output loss during iteration.
+      #   If solver = 'svd', this parameter is ignored.
       # @param random_seed [Integer] The seed value using to initialize the random generator.
-      def initialize(reg_param: 1.0, fit_bias: false, bias_scale: 1.0, max_iter: 1000, batch_size: 10, optimizer: nil,
-                     solver: 'sgd', n_jobs: nil, random_seed: nil)
-        check_params_numeric(reg_param: reg_param, bias_scale: bias_scale, max_iter: max_iter, batch_size: batch_size)
-        check_params_boolean(fit_bias: fit_bias)
+      def initialize(learning_rate: 0.01, decay: nil, momentum: 0.9,
+                     reg_param: 1.0, fit_bias: true, bias_scale: 1.0,
+                     max_iter: 100, batch_size: 50, tol: 1e-4,
+                     solver: 'sgd',
+                     n_jobs: nil, verbose: false, random_seed: nil)
+        check_params_numeric(learning_rate: learning_rate, momentum: momentum,
+                             reg_param: reg_param, bias_scale: bias_scale,
+                             max_iter: max_iter, batch_size: batch_size, tol: tol)
+        check_params_boolean(fit_bias: fit_bias, verbose: verbose)
         check_params_string(solver: solver)
-        check_params_numeric_or_nil(n_jobs: n_jobs, random_seed: random_seed)
-        check_params_positive(reg_param: reg_param, max_iter: max_iter, batch_size: batch_size)
-        keywd_args = method(:initialize).parameters.map { |_t, arg| [arg, binding.local_variable_get(arg)] }.to_h
-        keywd_args.delete(:solver)
-        super(**keywd_args)
+        check_params_numeric_or_nil(decay: decay, n_jobs: n_jobs, random_seed: random_seed)
+        check_params_positive(learning_rate: learning_rate, reg_param: reg_param, max_iter: max_iter, batch_size: batch_size)
+        super()
+        @params.merge!(method(:initialize).parameters.map { |_t, arg| [arg, binding.local_variable_get(arg)] }.to_h)
         @params[:solver] = solver != 'svd' ? 'sgd' : 'svd'
+        @params[:decay] ||= @params[:reg_param] * @params[:learning_rate]
+        @params[:random_seed] ||= srand
+        @rng = Random.new(@params[:random_seed])
+        @penalty_type = L2_PENALTY
+        @loss_func = LinearModel::Loss::MeanSquaredError.new
+        @weight_vec = nil
+        @bias_term = nil
       end
 
       # Fit the model with given training data.
@@ -93,25 +117,6 @@ module Rumale
       def predict(x)
         x = check_convert_sample_array(x)
         x.dot(@weight_vec.transpose) + @bias_term
-      end
-
-      # Dump marshal data.
-      # @return [Hash] The marshal data about Ridge.
-      def marshal_dump
-        { params: @params,
-          weight_vec: @weight_vec,
-          bias_term: @bias_term,
-          rng: @rng }
-      end
-
-      # Load marshal data.
-      # @return [nil]
-      def marshal_load(obj)
-        @params = obj[:params]
-        @weight_vec = obj[:weight_vec]
-        @bias_term = obj[:bias_term]
-        @rng = obj[:rng]
-        nil
       end
 
       private
@@ -149,10 +154,6 @@ module Rumale
         else
           @weight_vec, @bias_term = partial_fit(x, y)
         end
-      end
-
-      def calc_loss_gradient(x, y, weight)
-        2.0 * (x.dot(weight) - y)
       end
     end
   end
