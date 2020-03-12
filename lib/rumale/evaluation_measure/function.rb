62730c59cdac3a39cbbd5cbefdc8f264b484a8d6
yoshoku <yoshoku@outlook.com>
2020-03-12 22:43:58 +0900

    :sparkles: Add module function for summarizing classification performance

---
+++ lib/rumale/evaluation_measure/function.rb
@@ -0,0 +1,94 @@
+# frozen_string_literal: true
+
+require 'rumale/validation'
+require 'rumale/evaluation_measure/accuracy'
+require 'rumale/evaluation_measure/precision_recall'
+
+module Rumale
+  module EvaluationMeasure
+    module_function
+
+    # Output a summary of classification performance for each class.
+    #
+    # @param y_true [Numo::Int32] (shape: [n_samples]) The ground truth labels.
+    # @param y_pred [Numo::Int32] (shape: [n_samples]) The predicted labels.
+    # @param target_name [Nil/Array] The label names.
+    # @param output_hash [Boolean] The flag indicating whether to output with Ruby Hash.
+    # @return [String/Hash] The summary of classification performance.
+    #   If output_hash is true, it returns the summary with Ruby Hash.
+    def classification_report(y_true, y_pred, target_name: nil, output_hash: false)
+      y_true = Rumale::Validation.check_convert_label_array(y_true)
+      y_pred = Rumale::Validation.check_convert_label_array(y_pred)
+      # calculate each evaluation measure.
+      supports = y_true.bincount
+      precisions = Rumale::EvaluationMeasure::PrecisionRecall.precision_each_class(y_true, y_pred)
+      recalls = Rumale::EvaluationMeasure::PrecisionRecall.recall_each_class(y_true, y_pred)
+      fscores = Rumale::EvaluationMeasure::PrecisionRecall.f_score_each_class(y_true, y_pred)
+      macro_precision = Rumale::EvaluationMeasure::PrecisionRecall.macro_average_precision(y_true, y_pred)
+      macro_recall = Rumale::EvaluationMeasure::PrecisionRecall.macro_average_recall(y_true, y_pred)
+      macro_fscore = Rumale::EvaluationMeasure::PrecisionRecall.macro_average_f_score(y_true, y_pred)
+      accuracy = Rumale::EvaluationMeasure::Accuracy.new.score(y_true, y_pred)
+      sum_supports = supports.sum
+      weights = Numo::DFloat.cast(supports) / sum_supports
+      weighted_precision = (Numo::DFloat.cast(precisions) * weights).sum
+      weighted_recall = (Numo::DFloat.cast(recalls) * weights).sum
+      weighted_fscore = (Numo::DFloat.cast(fscores) * weights).sum
+      # output reults.
+      target_name ||= y_true.to_a.uniq.sort.map(&:to_s)
+      if output_hash
+        res = {}
+        target_name.each_with_index do |label, n|
+          res[label] = {
+            precision: precisions[n],
+            recall: recalls[n],
+            fscore: fscores[n],
+            support: supports[n]
+          }
+        end
+        res[:accuracy] = accuracy
+        res[:macro_avg] = {
+          precision: macro_precision,
+          recall: macro_recall,
+          fscore: macro_fscore,
+          support: sum_supports
+        }
+        res[:weighted_avg] = {
+          precision: weighted_precision,
+          recall: weighted_recall,
+          fscore: weighted_fscore,
+          support: sum_supports
+        }
+        res
+      else
+        width = ['weighted avg'.size, target_name.map(&:size).max].max
+        res = +''
+        res << "#{' ' * width}  precision    recall  f1-score   support\n"
+        res << "\n"
+        target_name.each_with_index do |label, n|
+          label_str = format("%##{width}s", label)
+          precision_str = format('%#10s', format('%.2f', precisions[n]))
+          recall_str = format('%#10s', format('%.2f', recalls[n]))
+          fscore_str = format('%#10s', format('%.2f', fscores[n]))
+          supports_str = format('%#10s', supports[n])
+          res << "#{label_str} #{precision_str}#{recall_str}#{fscore_str}#{supports_str}\n"
+        end
+        res << "\n"
+        supports_str = format('%#10s', sum_supports)
+        accuracy_str = format('%#30s', format('%.2f', accuracy))
+        res << format("%##{width}s ", 'accuracy')
+        res << "#{accuracy_str}#{supports_str}\n"
+        precision_str = format('%#10s', format('%.2f', macro_precision))
+        recall_str = format('%#10s', format('%.2f', macro_recall))
+        fscore_str = format('%#10s', format('%.2f', macro_fscore))
+        res << format("%##{width}s ", 'macro avg')
+        res << "#{precision_str}#{recall_str}#{fscore_str}#{supports_str}\n"
+        precision_str = format('%#10s', format('%.2f', weighted_precision))
+        recall_str = format('%#10s', format('%.2f', weighted_recall))
+        fscore_str = format('%#10s', format('%.2f', weighted_fscore))
+        res << format("%##{width}s ", 'weighted avg')
+        res << "#{precision_str}#{recall_str}#{fscore_str}#{supports_str}\n"
+        res
+      end
+    end
+  end
+end
