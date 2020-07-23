8f06f1681cfce0c30ef4ec90c7d282d6876f3c2f
yoshoku <yoshoku@outlook.com>
2020-07-23 22:46:05 +0900

    :sparkles: Add transformer class with binarization

---
+++ spec/rumale/preprocessing/binarizer_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Rumale::Preprocessing::Binarizer do
+  let(:x) { Numo::DFloat[[-0.5, 15.2], [3.1, -2.4], [4.2, 12.6]] }
+  let(:binarized) { binarizer.fit_transform(x) }
+  let(:n_nonzero_elements) { binarized.gt(0).count }
+
+  context 'when threshold value is not given' do
+    let(:binarizer) { described_class.new }
+
+    it 'binarizes given samples' do
+      expect(binarized).to eq(Numo::DFloat[[0, 1], [1, 0], [1, 1]])
+      expect(n_nonzero_elements).to eq(4)
+    end
+  end
+
+  context 'when threshold value is given' do
+    let(:threshold) { 5.0 }
+    let(:binarizer) { described_class.new(threshold: threshold) }
+
+    it 'binarizes given samples with given threshold value' do
+      expect(binarized).to eq(Numo::DFloat[[0, 1], [0, 0], [0, 1]])
+      expect(n_nonzero_elements).to eq(2)
+    end
+  end
+end
