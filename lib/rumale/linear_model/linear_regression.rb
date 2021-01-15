fe4335304d890f872f0106e037c92762ee7c713f
yoshoku <yoshoku@outlook.com>
2021-01-15 17:22:35 +0900

    :art: Refactor codes and update document on LinearRegression

--- lib/rumale/linear_model/linear_regression.rb
+++ lib/rumale/linear_model/linear_regression.rb
@@ -42,21 +42,21 @@ module Rumale
       #
       # @param learning_rate [Float] The initial value of learning rate.
       #   The learning rate decreases as the iteration proceeds according to the equation: learning_rate / (1 + decay * t).
-      #   If solver = 'svd', this parameter is ignored.
+      #   If solver is not 'sgd', this parameter is ignored.
       # @param decay [Float] The smoothing parameter for decreasing learning rate as the iteration proceeds.
       #   If nil is given, the decay sets to 'learning_rate'.
-      #   If solver = 'svd', this parameter is ignored.
+      #   If solver is not 'sgd', this parameter is ignored.
       # @param momentum [Float] The momentum factor.
-      #   If solver = 'svd', this parameter is ignored.
+      #   If solver is not 'sgd', this parameter is ignored.
       # @param fit_bias [Boolean] The flag indicating whether to fit the bias term.
       # @param bias_scale [Float] The scale of the bias term.
       # @param max_iter [Integer] The maximum number of epochs that indicates
       #   how many times the whole data is given to the training process.
-      #   If solver = 'svd', this parameter is ignored.
+      #   If solver is 'svd', this parameter is ignored.
       # @param batch_size [Integer] The size of the mini batches.
-      #   If solver = 'svd', this parameter is ignored.
+      #   If solver is not 'sgd', this parameter is ignored.
       # @param tol [Float] The tolerance of loss for terminating optimization.
-      #   If solver = 'svd', this parameter is ignored.
+      #   If solver is 'svd', this parameter is ignored.
       # @param solver [String] The algorithm to calculate weights. ('auto', 'sgd', 'svd' or 'lbfgs').
       #   'auto' chooses the 'svd' solver if Numo::Linalg is loaded. Otherwise, it chooses the 'sgd' solver.
       #   'sgd' uses the stochastic gradient descent optimization.
@@ -65,9 +65,9 @@ module Rumale
       # @param n_jobs [Integer] The number of jobs for running the fit method in parallel.
       #   If nil is given, the method does not execute in parallel.
       #   If zero or less is given, it becomes equal to the number of processors.
-      #   This parameter is ignored if the Parallel gem is not loaded.
+      #   This parameter is ignored if the Parallel gem is not loaded or solver is not 'sgd'.
       # @param verbose [Boolean] The flag indicating whether to output loss during iteration.
-      #   If solver = 'svd', this parameter is ignored.
+      #   If solver is 'svd', this parameter is ignored.
       # @param random_seed [Integer] The seed value using to initialize the random generator.
       def initialize(learning_rate: 0.01, decay: nil, momentum: 0.9,
                      fit_bias: true, bias_scale: 1.0, max_iter: 1000, batch_size: 50, tol: 1e-4,
@@ -82,7 +82,7 @@ module Rumale
         super()
         @params.merge!(method(:initialize).parameters.map { |_t, arg| [arg, binding.local_variable_get(arg)] }.to_h)
         @params[:solver] = if solver == 'auto'
-                             load_linalg? ? 'svd' : 'sgd'
+                             enable_linalg?(warning: false) ? 'svd' : 'sgd'
                            else
                              solver.match?(/^svd$|^sgd$|^lbfgs$/) ? solver : 'sgd'
                            end
@@ -104,7 +104,7 @@ module Rumale
         y = check_convert_tvalue_array(y)
         check_sample_tvalue_size(x, y)
 
-        if @params[:solver] == 'svd' && enable_linalg?
+        if @params[:solver] == 'svd' && enable_linalg?(warning: false)
           fit_svd(x, y)
         elsif @params[:solver] == 'lbfgs'
           fit_lbfgs(x, y)
@@ -128,17 +128,8 @@ module Rumale
 
       def fit_svd(x, y)
         x = expand_feature(x) if fit_bias?
-
         w = Numo::Linalg.pinv(x, driver: 'svd').dot(y)
-
-        is_single_target_vals = y.shape[1].nil?
-        if @params[:fit_bias]
-          @weight_vec = is_single_target_vals ? w[0...-1].dup : w[0...-1, true].dup
-          @bias_term = is_single_target_vals ? w[-1] : w[-1, true].dup
-        else
-          @weight_vec = w.dup
-          @bias_term = is_single_target_vals ? 0 : Numo::DFloat.zeros(y.shape[1])
-        end
+        @weight_vec, @bias_term = single_target?(y) ? split_weight(w) : split_weight_mult(w)
       end
 
       def fit_lbfgs(x, y)
@@ -157,30 +148,26 @@ module Rumale
         n_features = x.shape[1]
         n_outputs = single_target?(y) ? 1 : y.shape[1]
 
-        w_init = Rumale::Utils.rand_normal([n_outputs, n_features], @rng.dup).flatten.dup
-
         res = Lbfgsb.minimize(
-          fnc: fnc, jcb: true, x_init: w_init, args: [x, y],
+          fnc: fnc, jcb: true, x_init: init_weight(n_features, n_outputs), args: [x, y],
           maxiter: @params[:max_iter], factr: @params[:tol] / Lbfgsb::DBL_EPSILON,
           verbose: @params[:verbose] ? 1 : -1
         )
 
-        w = single_target?(y) ? res[:x] : res[:x].reshape(n_outputs, n_features).transpose
-
-        if fit_bias?
-          @weight_vec = single_target?(y) ? w[0...-1].dup : w[0...-1, true].dup
-          @bias_term = single_target?(y) ? w[-1] : w[-1, true].dup
-        else
-          @weight_vec = w.dup
-          @bias_term = single_target?(y) ? 0 : Numo::DFloat.zeros(y.shape[1])
-        end
+        @weight_vec, @bias_term =
+          if single_target?(y)
+            split_weight(res[:x])
+          else
+            split_weight_mult(res[:x].reshape(n_outputs, n_features).transpose)
+          end
       end
 
       def fit_sgd(x, y)
-        n_outputs = y.shape[1].nil? ? 1 : y.shape[1]
-        n_features = x.shape[1]
-
-        if n_outputs > 1
+        if single_target?(y)
+          @weight_vec, @bias_term = partial_fit(x, y)
+        else
+          n_outputs = y.shape[1]
+          n_features = x.shape[1]
           @weight_vec = Numo::DFloat.zeros(n_outputs, n_features)
           @bias_term = Numo::DFloat.zeros(n_outputs)
           if enable_parallel?
@@ -189,8 +176,6 @@ module Rumale
           else
             n_outputs.times { |n| @weight_vec[n, true], @bias_term[n] = partial_fit(x, y[true, n]) }
           end
-        else
-          @weight_vec, @bias_term = partial_fit(x, y)
         end
       end
 
@@ -198,11 +183,16 @@ module Rumale
         y.ndim == 1
       end
 
-      def load_linalg?
-        return false if defined?(Numo::Linalg).nil?
-        return false if Numo::Linalg::VERSION < '0.1.4'
+      def init_weight(n_features, n_outputs)
+        Rumale::Utils.rand_normal([n_outputs, n_features], @rng.dup).flatten.dup
+      end
 
-        true
+      def split_weight_mult(w)
+        if fit_bias?
+          [w[0...-1, true].dup, w[-1, true].dup]
+        else
+          [w.dup, Numo::DFloat.zeros(w.shape[1])]
+        end
       end
     end
   end
