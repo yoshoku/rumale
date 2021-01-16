bf804cb5f3d5874757b85b275893c338152dc136
yoshoku <yoshoku@outlook.com>
2021-01-17 00:31:04 +0900

    :wrench: Readd checking code coverage with Coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,17 @@
 # frozen_string_literal: true
 
-require 'pp'
 require 'simplecov'
+require 'simplecov-lcov'
 
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([SimpleCov::Formatter::HTMLFormatter])
+SimpleCov::Formatter::LcovFormatter.config do |config|
+  config.report_with_single_file = true
+  config.single_report_path = 'coverage/lcov.info'
+end
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(
+  [SimpleCov::Formatter::HTMLFormatter, SimpleCov::Formatter::LcovFormatter]
+)
+
 SimpleCov.start
 
 require 'bundler/setup'
