033ffa1eb43560ff56e5714f7d9c121e69bd86aa
yoshoku <yoshoku@outlook.com>
2022-05-16 00:21:47 +0900

    refactor: summarize native extension codes

--- ext/rumale/rumaleext.h
+++ ext/rumale/rumaleext.h
@@ -1,8 +1,12 @@
-#ifndef RUMALE_H
-#define RUMALE_H 1
+#ifndef RUMALEEXT_H
+#define RUMALEEXT_H 1
+
+#include <math.h>
+#include <string.h>
 
 #include <ruby.h>
 
-#include "tree.h"
+#include <numo/narray.h>
+#include <numo/template.h>
 
 #endif /* RUMALEEXT_H */
