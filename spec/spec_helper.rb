38067969e5814e228693537da98e961e5f894bd5
yoshoku <yoshoku@outlook.com>
2022-05-16 21:32:37 +0900

    ci: remove code coverage checking action

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,16 +1,6 @@
 # frozen_string_literal: true
 
 require 'simplecov'
-require 'simplecov-lcov'
-
-SimpleCov::Formatter::LcovFormatter.config do |config|
-  config.report_with_single_file = true
-  config.single_report_path = 'coverage/lcov.info'
-end
-
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(
-  [SimpleCov::Formatter::HTMLFormatter, SimpleCov::Formatter::LcovFormatter]
-)
 
 SimpleCov.start
 
