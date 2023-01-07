94164ae2ef3ee758f35cd5813e001305a141d642
yoshoku <yoshoku@outlook.com>
2023-01-07 15:37:59 +0900

    feat!: remove stochastic gradient descent optimization from LinearRegression

--- rumale-linear_model/lib/rumale/linear_model/linear_regression.rb
+++ rumale-linear_model/lib/rumale/linear_model/linear_regression.rb
@@ -2,21 +2,19 @@
 
 require 'lbfgsb'
 
+require 'rumale/base/estimator'
 require 'rumale/base/regressor'
 require 'rumale/validation'
-require 'rumale/linear_model/base_sgd'
 
 module Rumale
   module LinearModel
     # LinearRegression is a class that implements ordinary least square linear regression
-    # with stochastic gradient descent (SGD) optimization,
-    # singular value decomposition (SVD), or L-BFGS optimization.
+    # with singular value decomposition (SVD) or L-BFGS optimization.
     #
     # @example
     #   require 'rumale/linear_model/linear_regression'
     #
-    #   estimator =
-    #     Rumale::LinearModel::LinearRegression.new(max_iter: 1000, batch_size: 20, random_seed: 1)
+    #   estimator = Rumale::LinearModel::LinearRegression.new
     #   estimator.fit(training_samples, traininig_values)
     #   results = estimator.predict(testing_samples)
     #
@@ -27,11 +25,8 @@ module Rumale
     #   estimator = Rumale::LinearModel::LinearRegression.new(solver: 'svd')
     #   estimator.fit(training_samples, traininig_values)
     #   results = estimator.predict(testing_samples)
-    #
-    # *Reference*
-    # - Bottou, L., "Large-Scale Machine Learning with Stochastic Gradient Descent," Proc. COMPSTAT'10, pp. 177--186, 2010.
-    class LinearRegression < BaseSGD
-      include ::Rumale::Base::Regressor
+    class LinearRegression < Rumale::Base::Estimator
+      include Rumale::Base::Regressor
 
       # Return the weight vector.
       # @return [Numo::DFloat] (shape: [n_outputs, n_features])
@@ -41,56 +36,35 @@ module Rumale
       # @return [Numo::DFloat] (shape: [n_outputs])
       attr_reader :bias_term
 
-      # Return the random generator for random sampling.
-      # @return [Random]
-      attr_reader :rng
-
       # Create a new ordinary least square linear regressor.
       #
-      # @param learning_rate [Float] The initial value of learning rate.
-      #   The learning rate decreases as the iteration proceeds according to the equation: learning_rate / (1 + decay * t).
-      #   If solver is not 'sgd', this parameter is ignored.
-      # @param decay [Float] The smoothing parameter for decreasing learning rate as the iteration proceeds.
-      #   If nil is given, the decay sets to 'learning_rate'.
-      #   If solver is not 'sgd', this parameter is ignored.
-      # @param momentum [Float] The momentum factor.
-      #   If solver is not 'sgd', this parameter is ignored.
       # @param fit_bias [Boolean] The flag indicating whether to fit the bias term.
       # @param bias_scale [Float] The scale of the bias term.
       # @param max_iter [Integer] The maximum number of epochs that indicates
       #   how many times the whole data is given to the training process.
       #   If solver is 'svd', this parameter is ignored.
-      # @param batch_size [Integer] The size of the mini batches.
-      #   If solver is not 'sgd', this parameter is ignored.
       # @param tol [Float] The tolerance of loss for terminating optimization.
       #   If solver is 'svd', this parameter is ignored.
-      # @param solver [String] The algorithm to calculate weights. ('auto', 'sgd', 'svd' or 'lbfgs').
+      # @param solver [String] The algorithm to calculate weights. ('auto', 'svd' or 'lbfgs').
       #   'auto' chooses the 'svd' solver if Numo::Linalg is loaded. Otherwise, it chooses the 'lbfgs' solver.
-      #   'sgd' uses the stochastic gradient descent optimization.
       #   'svd' performs singular value decomposition of samples.
       #   'lbfgs' uses the L-BFGS method for optimization.
-      # @param n_jobs [Integer] The number of jobs for running the fit method in parallel.
-      #   If nil is given, the method does not execute in parallel.
-      #   If zero or less is given, it becomes equal to the number of processors.
-      #   This parameter is ignored if the Parallel gem is not loaded or solver is not 'sgd'.
       # @param verbose [Boolean] The flag indicating whether to output loss during iteration.
       #   If solver is 'svd', this parameter is ignored.
-      # @param random_seed [Integer] The seed value using to initialize the random generator.
-      def initialize(learning_rate: 0.01, decay: nil, momentum: 0.9,
-                     fit_bias: true, bias_scale: 1.0, max_iter: 1000, batch_size: 50, tol: 1e-4,
-                     solver: 'auto',
-                     n_jobs: nil, verbose: false, random_seed: nil)
+      def initialize(fit_bias: true, bias_scale: 1.0, max_iter: 1000, tol: 1e-4, solver: 'auto', verbose: false)
         super()
-        @params.merge!(method(:initialize).parameters.to_h { |_t, arg| [arg, binding.local_variable_get(arg)] })
+        @params = {
+          fit_bias: fit_bias,
+          bias_scale: bias_scale,
+          max_iter: max_iter,
+          tol: tol,
+          verbose: verbose
+        }
         @params[:solver] = if solver == 'auto'
                              enable_linalg?(warning: false) ? 'svd' : 'lbfgs'
                            else
-                             solver.match?(/^svd$|^sgd$|^lbfgs$/) ? solver : 'lbfgs'
+                             solver.match?(/^svd$|^lbfgs$/) ? solver : 'lbfgs'
                            end
-        @params[:decay] ||= @params[:learning_rate]
-        @params[:random_seed] ||= srand
-        @rng = Random.new(@params[:random_seed])
-        @loss_func = ::Rumale::LinearModel::Loss::MeanSquaredError.new
       end
 
       # Fit the model with given training data.
@@ -99,17 +73,15 @@ module Rumale
       # @param y [Numo::DFloat] (shape: [n_samples, n_outputs]) The target values to be used for fitting the model.
       # @return [LinearRegression] The learned regressor itself.
       def fit(x, y)
-        x = ::Rumale::Validation.check_convert_sample_array(x)
-        y = ::Rumale::Validation.check_convert_target_value_array(y)
-        ::Rumale::Validation.check_sample_size(x, y)
+        x = Rumale::Validation.check_convert_sample_array(x)
+        y = Rumale::Validation.check_convert_target_value_array(y)
+        Rumale::Validation.check_sample_size(x, y)
 
-        if @params[:solver] == 'svd' && enable_linalg?(warning: false)
-          fit_svd(x, y)
-        elsif @params[:solver] == 'lbfgs'
-          fit_lbfgs(x, y)
-        else
-          fit_sgd(x, y)
-        end
+        @weight_vec, @bias_term = if @params[:solver] == 'svd' && enable_linalg?(warning: false)
+                                    fit_svd(x, y)
+                                  else
+                                    fit_lbfgs(x, y)
+                                  end
 
         self
       end
@@ -119,7 +91,7 @@ module Rumale
       # @param x [Numo::DFloat] (shape: [n_samples, n_features]) The samples to predict the values.
       # @return [Numo::DFloat] (shape: [n_samples, n_outputs]) Predicted values per sample.
       def predict(x)
-        x = ::Rumale::Validation.check_convert_sample_array(x)
+        x = Rumale::Validation.check_convert_sample_array(x)
 
         x.dot(@weight_vec.transpose) + @bias_term
       end
@@ -129,11 +101,11 @@ module Rumale
       def fit_svd(x, y)
         x = expand_feature(x) if fit_bias?
         w = Numo::Linalg.pinv(x, driver: 'svd').dot(y)
-        @weight_vec, @bias_term = single_target?(y) ? split_weight(w) : split_weight_mult(w)
+        single_target?(y) ? split_weight(w) : split_weight_mult(w)
       end
 
-      def fit_lbfgs(x, y)
-        fnc = proc do |w, x, y| # rubocop:disable Lint/ShadowingOuterLocalVariable
+      def fit_lbfgs(base_x, base_y)
+        fnc = proc do |w, x, y|
           n_samples, n_features = x.shape
           w = w.reshape(y.shape[1], n_features) unless y.shape[1].nil?
           z = x.dot(w.transpose)
@@ -143,39 +115,22 @@ module Rumale
           [loss, gradient.flatten.dup]
         end
 
-        x = expand_feature(x) if fit_bias?
+        base_x = expand_feature(base_x) if fit_bias?
 
-        n_features = x.shape[1]
-        n_outputs = single_target?(y) ? 1 : y.shape[1]
+        n_features = base_x.shape[1]
+        n_outputs = single_target?(base_y) ? 1 : base_y.shape[1]
+        w_init = Numo::DFloat.zeros(n_outputs * n_features)
 
         res = Lbfgsb.minimize(
-          fnc: fnc, jcb: true, x_init: init_weight(n_features, n_outputs), args: [x, y],
+          fnc: fnc, jcb: true, x_init: w_init, args: [base_x, base_y],
           maxiter: @params[:max_iter], factr: @params[:tol] / Lbfgsb::DBL_EPSILON,
           verbose: @params[:verbose] ? 1 : -1
         )
 
-        @weight_vec, @bias_term =
-          if single_target?(y)
-            split_weight(res[:x])
-          else
-            split_weight_mult(res[:x].reshape(n_outputs, n_features).transpose)
-          end
-      end
-
-      def fit_sgd(x, y)
-        if single_target?(y)
-          @weight_vec, @bias_term = partial_fit(x, y)
+        if single_target?(base_y)
+          split_weight(res[:x])
         else
-          n_outputs = y.shape[1]
-          n_features = x.shape[1]
-          @weight_vec = Numo::DFloat.zeros(n_outputs, n_features)
-          @bias_term = Numo::DFloat.zeros(n_outputs)
-          if enable_parallel?
-            models = parallel_map(n_outputs) { |n| partial_fit(x, y[true, n]) }
-            n_outputs.times { |n| @weight_vec[n, true], @bias_term[n] = models[n] }
-          else
-            n_outputs.times { |n| @weight_vec[n, true], @bias_term[n] = partial_fit(x, y[true, n]) }
-          end
+          split_weight_mult(res[:x].reshape(n_outputs, n_features).transpose)
         end
       end
 
@@ -183,8 +138,17 @@ module Rumale
         y.ndim == 1
       end
 
-      def init_weight(n_features, n_outputs)
-        Rumale::Utils.rand_normal([n_outputs, n_features], @rng.dup).flatten.dup
+      def expand_feature(x)
+        n_samples = x.shape[0]
+        Numo::NArray.hstack([x, Numo::DFloat.ones([n_samples, 1]) * @params[:bias_scale]])
+      end
+
+      def split_weight(weight)
+        if fit_bias?
+          [weight[0...-1].dup, weight[-1]]
+        else
+          [weight, 0.0]
+        end
       end
 
       def split_weight_mult(w)
@@ -193,6 +157,10 @@ module Rumale
         else
           [w.dup, Numo::DFloat.zeros(w.shape[1])]
         end
+      end
+
+      def fit_bias?
+        @params[:fit_bias] == true
       end
     end
   end
--- rumale-linear_model/spec/rumale/linear_model/linear_regression_spec.rb
+++ rumale-linear_model/spec/rumale/linear_model/linear_regression_spec.rb
@@ -10,9 +10,8 @@ RSpec.describe Rumale::LinearModel::LinearRegression do
   let(:n_features) { x.shape[1] }
   let(:n_outputs) { multi_target.shape[1] }
   let(:fit_bias) { false }
-  let(:solver) { 'sgd' }
   let(:n_jobs) { nil }
-  let(:estimator) { described_class.new(fit_bias: fit_bias, solver: solver, n_jobs: n_jobs, random_seed: 1).fit(x, y) }
+  let(:estimator) { described_class.new(fit_bias: fit_bias, solver: solver).fit(x, y) }
   let(:predicted) { estimator.predict(x) }
   let(:score) { estimator.score(x, y) }
   let(:copied) { Marshal.load(Marshal.dump(estimator)) }
@@ -38,10 +37,7 @@ RSpec.describe Rumale::LinearModel::LinearRegression do
       expect(copied.params).to eq(estimator.params)
       expect(copied.weight_vec).to eq(estimator.weight_vec)
       expect(copied.bias_term).to eq(estimator.bias_term)
-      expect(copied.rng).to eq(estimator.rng)
       expect(copied.score(x, y)).to eq(score)
-      expect(copied.instance_variable_get(:@penalty_type)).to be_nil
-      expect(copied.instance_variable_get(:@loss_func).class).to eq(Rumale::LinearModel::Loss::MeanSquaredError)
     end
   end
 
@@ -111,16 +107,6 @@ RSpec.describe Rumale::LinearModel::LinearRegression do
       expect(predicted.shape[1]).to eq(n_outputs)
       expect(score).to be_within(0.01).of(1.0)
     end
-  end
-
-  context 'when solver is stochastic gradient descent' do
-    let(:solver) { 'sgd' }
-
-    it_behaves_like 'single regression'
-    it_behaves_like 'single regression with bias'
-    it_behaves_like 'multiple regression'
-    it_behaves_like 'multiple regression with bias'
-    it_behaves_like 'multiple regression with parallel'
   end
 
   context 'when solver is singular value decomposition' do
