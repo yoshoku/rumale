ca9640e647c3b1bff5310be26ff3d3626bd6bc33
yoshoku <yoshoku@outlook.com>
2023-06-18 10:55:29 +0900

    chore: add rubocop script

---
+++ rubocop.sh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+
+BASE_DIR=`pwd`
+GEMS=("rumale-core" "rumale-evaluation_measure" "rumale-preprocessing" "rumale-model_selection" \
+"rumale-pipeline" "rumale-clustering" "rumale-decomposition" "rumale-tree" "rumale-ensemble" \
+"rumale-feature_extraction" "rumale-kernel_approximation" "rumale-kernel_machine" "rumale-linear_model" \
+"rumale-manifold" "rumale-metric_learning" "rumale-naive_bayes" "rumale-nearest_neighbors" \
+"rumale-neural_network" "rumale")
+
+for GEM in "${GEMS[@]}"; do
+  cd "${BASE_DIR}/${GEM}"
+  echo ""
+  echo "${GEM}:"
+  # bundle exec rubocop || exit 1
+  bundle exec rubocop
+done
