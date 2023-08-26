c603134fdf8727995aacb437d2c4ad52fcf3f50d
yoshoku <yoshoku@outlook.com>
2023-08-27 00:02:21 +0900

    chore: add bump up script

---
+++ bump.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+FROM='0.26.0'
+TO='0.27.0'
+
+for FILENAME in `find . -name 'version.rb'`; do
+  gsed -i -e "s/VERSION\s*=\s*'${FROM}'/VERSION = '${TO}'/" ${FILENAME}
+done
+
+for FILENAME in `find . -name '*.gemspec'`; do
+  gsed -i -e "s/\(spec.add_dependency\s\+'rumale\-[a-zA-Z\-_]\+'\),\s\+'~>\s*${FROM}'/\1, '~> ${TO}'/g" ${FILENAME}
+done
