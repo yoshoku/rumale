f6c5862c5fe8f8ce66d5351020ed019cd3c0e1fb
yoshoku <yoshoku@outlook.com>
2024-01-01 21:20:34 +0900

    docs: update copyright year

--- rumale-tree/ext/rumale/tree/ext.hpp
+++ rumale-tree/ext/rumale/tree/ext.hpp
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2022-2023 Atsushi Tatsuma
+ * Copyright (c) 2022-2024 Atsushi Tatsuma
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
