a469e4b33e1a38af49b1a96608a42cbcb8d74716
yoshoku <yoshoku@outlook.com>
2025-11-09 22:45:25 +0900

    chore: remove red-datasets-numo-narray

--- rumale/spec/spec_helper.rb
+++ rumale/spec/spec_helper.rb
@@ -1,34 +1,13 @@
 # frozen_string_literal: true
 
 require 'rumale'
-require 'datasets-numo-narray'
 
 def iris
-  dataset = Datasets::LIBSVM.new('iris').to_narray
-  x = dataset[true, 1..-1]
-  y = Numo::Int32.cast(dataset[true, 0])
-
-  ss = Rumale::ModelSelection::StratifiedShuffleSplit.new(n_splits: 1, test_size: 0.2, random_seed: 38)
-  train_ids, test_ids = ss.split(x, y)[0]
-
-  { x_train: x[train_ids, true].dup,
-    y_train: y[train_ids].dup,
-    x_test: x[test_ids, true].dup,
-    y_test: y[test_ids].dup }
+  Marshal.load(File.binread('spec/iris.dat')) # rubocop:disable Security/MarshalLoad
 end
 
 def housing
-  dataset = Datasets::LIBSVM.new('housing', note: 'scaled to [-1,1]').to_narray
-  x = dataset[true, 1..-1]
-  y = dataset[true, 0]
-
-  ss = Rumale::ModelSelection::ShuffleSplit.new(n_splits: 1, test_size: 0.1, random_seed: 38)
-  train_ids, test_ids = ss.split(x, y)[0]
-
-  { x_train: x[train_ids, true].dup,
-    y_train: y[train_ids].dup,
-    x_test: x[test_ids, true].dup,
-    y_test: y[test_ids].dup }
+  Marshal.load(File.binread('spec/housing.dat')) # rubocop:disable Security/MarshalLoad
 end
 
 RSpec.configure do |config|
